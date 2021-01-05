63d58752efd63cf4a70c107ff2cd223d7fe995a7
paris <silvester.pari@eox.at>
2021-01-05 17:09:23 +0100

    move dependendencies

--- packages/map-basic/.eslintrc.js
+++ packages/map-basic/.eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-underscore-dangle': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
 };
