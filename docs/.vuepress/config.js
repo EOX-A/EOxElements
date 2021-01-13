c445234ed90a10be50faa023649337089069ccd4
paris <silvester.pari@eox.at>
2021-01-13 11:37:42 +0100

    update vuepress config and added fibers

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
+  chainWebpack(config, isServer) {
+    for (const lang of ["sass", "scss"]) {
+      for (const name of ["modules", "normal"]) {
+        const rule = config.module.rule(lang).oneOf(name);
+        rule.uses.delete("sass-loader");
+
+        rule
+          .use("sass-loader")
+          .loader("sass-loader")
+          .options({
+            implementation: require("sass"),
+            sassOptions: {
+              fiber: require("fibers"),
+              indentedSyntax: lang === "sass"
+            }
+          });
+      }
+    }
+  },
   base: '/elements/',
   locales: {
     '/': {
