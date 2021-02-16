543bf8d12b06810906acd3451ff0be45f10a0d6c
silvester-pari <silvester.pari@eox.at>
2021-02-16 15:55:13 +0100

    remove console log

--- packages/charts/src/LineWithLineChart.vue
+++ packages/charts/src/LineWithLineChart.vue
@@ -42,7 +42,6 @@ export default {
     // this.addPlugin(zoom);
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
-    console.log(this.options);
     this.renderChart(this.chartData, this.options);
   },
   methods: {
