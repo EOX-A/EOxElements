af1a859aedb46930bc736c74844097c61fde0fbf
silvester-pari <silvester.pari@eox.at>
2023-04-13 11:24:18 +0200

    chore: replace host to unpkg again to always get latest version

--- elements/chart/src/interface.ts
+++ elements/chart/src/interface.ts
@@ -62,12 +62,12 @@ const createChart = (div: HTMLElement | null) => {
     iframe.setAttribute("id", "EOxChart");
     div?.appendChild(iframe);
     if (!import.meta?.url?.includes("localhost")) {
-      fetch("https://raw.githack.com/EOX-A/elements/chartelement/elements/chart/dist/index.html")
+      fetch("https://www.unpkg.com/@eox/chart/dist/index.html")
       .then((response) => {
         return response.text();
       })
       .then((text) => {
-        const html = text.replace('./assets/', 'https://raw.githack.com/EOX-A/elements/chartelement/elements/chart/dist/assets/');
+        const html = text.replace('./assets/', 'https://www.unpkg.com/@eox/chart/dist/assets/');
         iframe.contentDocument?.open();
         iframe.contentDocument?.write(html);
         iframe.contentDocument?.close();
