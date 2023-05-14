2c30a68380f7f7b0e67d6a74c66f2682b262fbe0
silvester-pari <silvester.pari@eox.at>
2023-05-14 22:14:33 +0200

    chore: formatting

--- elements/map/main.ts
+++ elements/map/main.ts
@@ -2,6 +2,7 @@ import Map from "ol/Map.js";
 import OSM from "ol/source/OSM.js";
 import TileLayer from "ol/layer/Tile.js";
 import View from "ol/View.js";
+import { apply } from "ol-mapbox-style";
 
 import olCss from "ol/ol.css";
 
