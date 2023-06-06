e3b642cfa86a651e4a898607a8a298be83434ce5
RobertOrthofer <orthofer.robert@gmail.com>
2023-06-06 15:58:41 +0200

    fix: vectorTile Test

--- elements/map/test/vectorTilesLayer.cy.ts
+++ elements/map/test/vectorTilesLayer.cy.ts
@@ -3,23 +3,25 @@ import vectorTileLayerStyleJson from "./vectorTilesLayer.json";
 import { VectorTile } from "ol/layer";
 
 describe("layers", () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit("/elements/map/test/general.html");
+    cy.get("eox-map").should(($el) => {
+      const eoxMap = <EOxMap>$el[0];
+      eoxMap.setLayers(vectorTileLayerStyleJson);
+    });
   });
   it("loads a Vector Layer", () => {
     cy.get("eox-map").should(($el) => {
       const eoxMap = <EOxMap>$el[0];
-      eoxMap.setLayers(vectorTileLayerStyleJson);
       const layers = eoxMap.map.getLayers().getArray();
       expect(layers).to.have.length(2);
-
       const layer = layers.find(
         (l) => l.get("mapbox-source") === "countries"
-      ) as VectorTile;
-      const features = layer
+        ) as VectorTile;
+        const features = layer
         .getSource()
         .getFeaturesInExtent(eoxMap.map.getView().calculateExtent());
-      expect(features.length).to.be.greaterThan(1000);
+        expect(features.length).to.be.greaterThan(10);
+      });
     });
-  });
 });
