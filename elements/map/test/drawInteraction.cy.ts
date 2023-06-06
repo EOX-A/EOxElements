f33348da107c9090115eb0b33ecd424cbcdc6579
RobertOrthofer <orthofer.robert@gmail.com>
2023-06-06 09:51:55 +0200

    fix: test for draw interaction create point

--- elements/map/test/drawInteraction.cy.ts
+++ elements/map/test/drawInteraction.cy.ts
@@ -1,23 +1,39 @@
+import VectorLayer from "ol/layer/Vector";
 import { EOxMap } from "../main";
 import vectorLayerStyleJson from "./drawInteraction.json";
+import { simulateEvent } from "./utils/events";
+import VectorSource from "ol/source/Vector";
+import { Point } from "ol/geom";
+
 
 describe("draw interaction", () => {
   beforeEach(() => {
     cy.visit("/elements/map/test/general.html");
   });
-  it("loads a Vector Layer", () => {
+  it("adds a draw interaction", () => {
     cy.get("eox-map").should(($el) => {
-      const eoxMap = <EOxMap>$el[0];
+    const eoxMap = <EOxMap>$el[0];
+  
       eoxMap.setLayers(vectorLayerStyleJson);
-      eoxMap.addDraw('draw_layer', {
+      eoxMap.addDraw('draw_point', {
         id: 'drawInteraction',
-        type: 'LineString',
+        type: 'Point',
       });
 
       // get the interaction via the source key
       const drawInteraction = eoxMap.interactions['drawInteraction']
       expect(drawInteraction).to.exist; 
       expect(drawInteraction.getActive()).to.equal(true)
+      
+
+      simulateEvent(eoxMap.map, 'pointermove', 10, 20);
+      simulateEvent(eoxMap.map, 'pointerdown', 10, 20);
+      simulateEvent(eoxMap.map, 'pointerup', 10, 20);
+      const drawLayer = eoxMap.map.getLayers().getArray()[1] as VectorLayer<VectorSource>;
+      const features = drawLayer.getSource().getFeatures();
+      const geometry = features[0].getGeometry() as Point;
+      expect(geometry.getCoordinates().length).to.be.equal(2);
     });
   });
 });
+
