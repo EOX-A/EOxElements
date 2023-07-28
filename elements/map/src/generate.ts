03cc4df05c714d01cc895270eef24a98cba4be2c
RobertOrthofer <orthofer.robert@gmail.com>
2023-07-28 14:40:21 +0200

    feat: consider flatStyles

--- elements/map/src/generate.ts
+++ elements/map/src/generate.ts
@@ -2,12 +2,15 @@ import * as olLayers from "ol/layer";
 import * as olSources from "ol/source";
 import * as olFormats from "ol/format";
 import { applyStyle } from "ol-mapbox-style";
+import { FlatStyleLike } from "ol/style/flat";
+import mapboxgl from "mapbox-gl";
 
 type EoxLayer = {
   type: olLayers.Layer;
   properties?: Object;
   source?: { type: olSources.Source };
   layers?: Array<EoxLayer>;
+  style?: mapboxgl.Style | FlatStyleLike;
 };
 
 export const generateLayers = (layerArray: Array<EoxLayer>) => {
@@ -26,6 +29,7 @@ export const generateLayers = (layerArray: Array<EoxLayer>) => {
     if (layer.source && !newSource) {
       throw new Error(`Source type ${layer.source.type} not supported!`);
     }
+
     const olLayer = new newLayer({
       ...layer,
       ...(layer.source && {
@@ -38,30 +42,37 @@ export const generateLayers = (layerArray: Array<EoxLayer>) => {
           }),
         }),
       }),
+      style: undefined, // override layer style, apply style after
       // @ts-ignore
       ...(layer.type === "Group" && {
         layers: layer.layers.reverse().map((l) => createLayer(l)),
       }),
     });
-    if (olLayer.get("style")) {
-      // existing layer source will not get overridden by "style" property
-      // to allow vector layers without defined sources, create a dummy-geojson-source
-      // if source does exist
-      const style = olLayer.get("style");
-      if (!style.sources) {
-        style.sources = {};
+
+    if (layer.style) {
+      if ("version" in layer.style) {
+        const mapboxStyle: mapboxgl.Style = layer.style;
+        // existing layer source will not get overridden by "style" property
+        // to allow vector layers without defined sources, create a dummy-geojson-source
+        // if source does exist
+        if (!mapboxStyle.sources) {
+          mapboxStyle.sources = {};
+        }
+        // @ts-ignore
+        const sourceName = layer.properties.id;
+        if (!mapboxStyle.sources[sourceName]) {
+          mapboxStyle.sources[sourceName] = {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: [],
+            },
+          };
+        }
+        applyStyle(olLayer, mapboxStyle, sourceName);
+      } else {
+        olLayer.setStyle(layer.style);
       }
-      const sourceName = style.layers[0].source;
-      if (!style.sources[sourceName]) {
-        style.sources[sourceName] = {
-          type: "geojson",
-          data: {
-            type: "FeatureCollection",
-            features: [],
-          },
-        };
-      }
-      applyStyle(olLayer, style, sourceName);
     }
     return olLayer;
   }
