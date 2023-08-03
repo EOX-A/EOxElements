25ba40ec3e16a49dc2ae9d9970b872ddbe717f0a
silvester-pari <silvester.pari@eox.at>
2023-08-03 17:29:11 +0200

    fix: radio & checkbox styling

--- elements/layercontrol/src/style.eox.ts
+++ elements/layercontrol/src/style.eox.ts
@@ -1,6 +1,7 @@
 import { button } from "../../../utils/styles/button";
 import { slider } from "../../../utils/styles/slider";
 import { radio } from "../../../utils/styles/radio";
+import { checkbox } from "../../../utils/styles/checkbox";
 
 export const styleEOX = `
 * {
@@ -10,6 +11,7 @@ export const styleEOX = `
 ${button}
 ${slider}
 ${radio}
+${checkbox}
 
 summary > .layer {
   height: 36px;
@@ -145,25 +147,6 @@ li .layer .left span {
   display: flex;
   align-items: center;
   margin-left: 6px;
-}
-li .layer input[type=checkbox] {
-  appearance: none;
-  margin: 0;
-  cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-li .layer input[type=checkbox]:after {
-  display: block;
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckbox-blank-outline%3C/title%3E%3Cpath d='M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z' /%3E%3C/svg%3E");
-  width: 20px;
-  height: 20px;
-  margin-right: 4px;
-}
-li .layer input[type=checkbox]:checked:after {
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckbox-marked%3C/title%3E%3Cpath d='M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z' /%3E%3C/svg%3E");
-
 }
 .drag-handle span {
   display: none;
---
+++ utils/styles/checkbox.ts
@@ -0,0 +1,21 @@
+export const checkbox = `
+input[type=checkbox] {
+  appearance: none;
+  margin: 0;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+input[type=checkbox]:after {
+  display: block;
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckbox-blank-outline%3C/title%3E%3Cpath d='M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z' /%3E%3C/svg%3E");
+  width: 20px;
+  height: 20px;
+  margin-right: 4px;
+}
+input[type=checkbox]:checked:after {
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckbox-marked%3C/title%3E%3Cpath d='M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z' /%3E%3C/svg%3E");
+
+}
+`;
