56093ae53f704c5c74d9303a6abd299f3054fb0b
Moritz Riede <moritz@mriede.com>
2023-08-03 11:59:13 +0200

    style: add and modify various styles to implement the figma design

--- elements/layercontrol/src/style.eox.ts
+++ elements/layercontrol/src/style.eox.ts
@@ -11,22 +11,117 @@ ${button}
 ${slider}
 ${radio}
 
+summary > .layer {
+  height: 36px;
+  border-bottom: 1px solid #00417000;
+  display: flex;
+  align-items: center;
+  padding-right: 12px;
+}
+
+[data-type=group] .title {
+  font-style: italic;
+}
+
+
+[data-type] .title::before {
+  width: 24px;
+  height: 24px;
+  margin-right: 6px;
+  margin-left: -1px;
+}
+
+[data-type=raster] .title::before {
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%236889A0' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckerboard%3C/title%3E%3Cpath d='M2 2V22H22V2H2M20 12H16V16H20V20H16V16H12V20H8V16H4V12H8V8H4V4H8V8H12V4H16V8H20V12M16 8V12H12V8H16M12 12V16H8V12H12Z' /%3E%3C/svg%3E");
+}
+
+
+[data-type=group] .title::before {
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%236889A0' viewBox='0 0 24 24'%3E%3Ctitle%3Efolder-outline%3C/title%3E%3Cpath d='M20,18H4V8H20M20,6H12L10,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V8C22,6.89 21.1,6 20,6Z' /%3E%3C/svg%3E");
+}
+
+[data-type]:not([data-type=group]) .title {
+  font-style: normal;
+}
+
+details summary { list-style-type: none; } /* Firefox */
+details summary::-webkit-details-marker { display: none; } /* Chrome */
+details summary::marker { display: none; }
+
+.layers > ul > li {
+  background: #F1F5F9;
+}
+
+summary {
+  position: relative;
+}
+summary details, summary li, summary ul {
+  width: 100%;
+}
+
+input.lc-search {
+  width: 100%;
+  height: 32px;
+  padding-left: 32px;
+  font-size: 15px;
+}
+
+input.lc-search::before {
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Ctitle%3Emagnify%3C/title%3E%3Cpath d='M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z' /%3E%3C/svg%3E");
+}
+
 ul {
-  padding: 0;
+  padding: 0 0 0 20px;
+  background: #D7E2EE;
+  background: #F1F5F9;
 }
 li {
   list-style: none;
+  cursor: pointer;
+  background: #CFDDEB;
+  border-bottom: 1px solid #00417022;
 }
 li summary {
   display: flex;
 }
+
+li[data-type=group] .title::before {
+
+}
+
+.slider-control {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 9px;
+  margin-left: 30px;
+  margin-bottom: 12px;
+}
+
+.slider-control input[type=range] {
+  max-width: 120px;
+}
+
+.slider-control .slider-property {
+  width: 80px;
+  color: #3B6289;
+  font-size: 14px;
+}
+
+.slider-control > * {
+  text-transform: capitalize;
+}
+
+/* Expansible Section Chevron */
 li summary:before {
-  content: '▶';
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echevron-right%3C/title%3E%3Cpath d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' /%3E%3C/svg%3E");
   font-size: 13px;
-  width: 13px;
-  height: 13px;
+  width: 24px;
+  height: 24px;
   margin-right: 6px;
   margin-left: -1px;
+
+  margin-top: 5px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -45,20 +140,23 @@ li .layer .left span {
   cursor: pointer;
   display: flex;
   align-items: center;
+  margin-left: 6px;
 }
 li .layer input[type=checkbox] {
   appearance: none;
   margin: 0;
+  cursor: pointer;
 }
 li .layer input[type=checkbox]:after {
   display: block;
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Ctitle%3Eeye-off-outline%3C/title%3E%3Cpath fill='%23004170' d='M2,5.27L3.28,4L20,20.72L18.73,22L15.65,18.92C14.5,19.3 13.28,19.5 12,19.5C7,19.5 2.73,16.39 1,12C1.69,10.24 2.79,8.69 4.19,7.46L2,5.27M12,9A3,3 0 0,1 15,12C15,12.35 14.94,12.69 14.83,13L11,9.17C11.31,9.06 11.65,9 12,9M12,4.5C17,4.5 21.27,7.61 23,12C22.18,14.08 20.79,15.88 19,17.19L17.58,15.76C18.94,14.82 20.06,13.54 20.82,12C19.17,8.64 15.76,6.5 12,6.5C10.91,6.5 9.84,6.68 8.84,7L7.3,5.47C8.74,4.85 10.33,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C12.69,17.5 13.37,17.43 14,17.29L11.72,15C10.29,14.85 9.15,13.71 9,12.28L5.6,8.87C4.61,9.72 3.78,10.78 3.18,12Z' /%3E%3C/svg%3E");
-  width: 24px;
-  height: 24px;
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckbox-blank-outline%3C/title%3E%3Cpath d='M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z' /%3E%3C/svg%3E");
+  width: 20px;
+  height: 20px;
   margin-right: 4px;
 }
 li .layer input[type=checkbox]:checked:after {
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Ctitle%3Eeye%3C/title%3E%3Cpath fill='%23004170' d='M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z' /%3E%3C/svg%3E");
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Echeckbox-marked%3C/title%3E%3Cpath d='M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z' /%3E%3C/svg%3E");
+
 }
 .drag-handle span {
   display: none;
