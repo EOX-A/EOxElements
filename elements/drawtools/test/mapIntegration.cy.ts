47d74338cf4f58557cda5abbff8a1fa66b41eb08
silvester-pari <silvester.pari@eox.at>
2023-08-09 11:03:46 +0200

    feat(test): introduce mock map for drawtools test

--- elements/drawtools/test/mapIntegration.cy.ts
+++ elements/drawtools/test/mapIntegration.cy.ts
@@ -1,41 +1,47 @@
 import "../src/main";
-import "../../map/main"
+import { EOxDrawTools } from "../src/main";
+import "./_mockMap";
 
 describe("Drawtools", () => {
   beforeEach(() => {
-    // @ts-ignore
-    cy.mount(`<eox-map
-      zoom="3"
-      center="[1000000, 6000000]"
-      layers='[
-        {"type": "Vector","id": "draw","source": {"type": "Vector"}},
-        {"type":"Tile","source":{"type":"OSM"}}
-      ]'
-    ></eox-map>`).as(
-      "eox-map"
-    )
-    // @ts-ignore
-    cy.mount(`
-      <eox-drawtools for="eox-map" layer="draw"></eox-drawtools>`
-    ).as(
-      "eox-drawtools"
-    );
-  })
+    cy.mount("<mock-map></mock-map>");
+    cy.mount(
+      `
+      <eox-drawtools for="mock-map" layer="draw"></eox-drawtools>`
+    ).as("eox-drawtools");
+  });
 
   it("loads the drawtools", () => {
+    cy.get("eox-drawtools").shadow();
+  });
+
+  it("clicks the draw button", () => {
+    cy.get("eox-drawtools")
+      .shadow()
+      .within(() => {
+        cy.get("[data-cy='drawBtn']").contains("draw");
+        cy.get("[data-cy='drawBtn']").should("not.contain", "drawing");
+        cy.get("[data-cy='drawBtn']").click();
+        cy.get("[data-cy='drawBtn']").contains("drawing");
+      });
+    cy.get("eox-drawtools").should(($el) => {
+      const drawTools = <EOxDrawTools>$el[0];
+      expect(drawTools._drawnFeatures).to.have.length(1);
+    });
+  });
+
+  it("clicks the discard button and clears drawn features", () => {
     cy.get("eox-drawtools")
       .shadow()
       .within(() => {
         cy.get("[data-cy='drawBtn']").click();
+        cy.get("[data-cy='drawBtn']").contains("drawing");
+        cy.get("[data-cy='discardBtn']").click();
+        cy.get("[data-cy='drawBtn']").should("not.contain", "drawing");
       });
-    cy.get("eox-map")
-      .shadow()
-      .within(() => {
-        cy.get("canvas").click(20, 20);
-        cy.get("canvas").click(20, 50);
-        cy.get("canvas").click(100, 50);
-        cy.get("canvas").click(100, 20);
-        cy.get("canvas").click(20, 20);
-      })
+    cy.get("eox-drawtools").should(($el) => {
+      const drawTools = <EOxDrawTools>$el[0];
+      expect(drawTools._drawnFeatures).to.have.length(0);
+    });
   });
 });
