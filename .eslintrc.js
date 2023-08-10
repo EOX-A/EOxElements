10a52ce5830aac7613809fd99d261e950128df9d
silvester-pari <silvester.pari@eox.at>
2023-08-10 15:59:38 +0200

    chore: fix eslint config and add workflow

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    "cypress/globals": true,
-  },
-  extends: ["@eox", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["cypress", "@typescript-eslint"],
-  root: true,
-};
