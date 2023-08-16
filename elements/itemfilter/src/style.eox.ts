47f9e887fba6d6f9294b5d83ecf26c088b1ce9fa
silvester-pari <silvester.pari@eox.at>
2023-08-16 10:53:22 +0200

    fix(style): chevron rotation

--- elements/itemfilter/src/style.eox.ts
+++ elements/itemfilter/src/style.eox.ts
@@ -48,12 +48,12 @@ details summary .title {
   margin-bottom: 12px;
 }
 details summary .title::before {
-  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%230009' viewBox='0 0 24 24'%3E%3Ctitle%3Echevron-down%3C/title%3E%3Cpath d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' /%3E%3C/svg%3E");
+  content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%230009' viewBox='0 0 24 24'%3E%3Ctitle%3Echevron-right%3C/title%3E%3Cpath d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' /%3E%3C/svg%3E");
   height: 24px;
   width: 24px;
 }
 details[open] summary .title::before {
-  transform: rotate(-90deg);
+  transform: rotate(90deg);
 }
 details li span,
 details summary {
