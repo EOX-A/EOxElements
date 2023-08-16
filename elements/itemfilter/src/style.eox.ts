0366a33d4198e8e6a9fe8aae2f1ac33f867cacc9
silvester-pari <silvester.pari@eox.at>
2023-08-16 13:21:46 +0200

    fix: display of highlight marks

--- elements/itemfilter/src/style.eox.ts
+++ elements/itemfilter/src/style.eox.ts
@@ -25,7 +25,7 @@ li span {
   text-overflow: ellipsis;
   white-space: nowrap;
 }
-li span, label {
+li label {
   display: flex;
   align-items: center;
 }
@@ -40,31 +40,30 @@ details summary {
 }
 .title {
   font-size: small;
-  text-transform: capitalize;
   align-items: center;
 }
 details summary .title {
   display: flex;
   font-weight: 500;
 }
-details.details-filter summary .title::after,
-details.details-results summary .title::before {
+details.details-filter summary input::after,
+details.details-results summary input::before {
   content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%230009' viewBox='0 0 24 24'%3E%3Ctitle%3Echevron-right%3C/title%3E%3Cpath d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' /%3E%3C/svg%3E");
   height: 24px;
   width: 24px;
 }
-details.details-filter summary .title::after {
+details.details-filter summary input::after {
   position: absolute;
   right: 8px;
   transform: rotate(90deg);
 }
-details[open] summary .title::before {
+details[open] summary input::before {
   transform: rotate(90deg);
 }
-details[open] summary .title::after {
+details[open] summary input::after {
   transform: rotate(270deg);
 }
-details li span,
+eox-itemfilter-expandcontainer .title,
 details summary {
   text-transform: capitalize;
 }
