483e0717ae88ed918cc4c3a326b1dbb891187937
RobertOrthofer <orthofer.robert@gmail.com>
2023-08-31 16:46:10 +0200

    test: add test for style expression as example

--- elements/map/test/vectorLayer.cy.ts
+++ elements/map/test/vectorLayer.cy.ts
@@ -40,7 +40,33 @@ describe("layers", () => {
       // @ts-ignore
       "fill-color": "yellow",
       "stroke-color": "black",
-      "stroke-width": 4,
+      "stroke-width": 2,
+    };
+    cy.mount(
+      `<eox-map layers='${JSON.stringify(vectorLayerStyleJson)}'></eox-map>`
+    ).as("eox-map");
+    cy.get("eox-map").and(($el) => {
+      return new Cypress.Promise((resolve) => {
+        const layers = (<EOxMap>$el[0]).map.getLayers().getArray();
+        // wait for features to load
+        //@ts-ignore
+        layers[0].getSource().on("featuresloadend", () => {
+          //@ts-ignore
+          const feature = layers[0].getSource().getFeatures()[0];
+          //@ts-ignore
+          const styles = layers[0].getStyleFunction()(feature);
+          expect(styles).to.have.length(1);
+          resolve();
+        });
+      });
+    });
+  });
+  it("correctly applies style expression", () => {
+    vectorLayerStyleJson[0].style = {
+      // @ts-ignore
+      "fill-color": ['string', ['get', 'COLOR'], '#eee'],
+      "stroke-color": "black",
+      "stroke-width": 2,
     };
     cy.mount(
       `<eox-map layers='${JSON.stringify(vectorLayerStyleJson)}'></eox-map>`
