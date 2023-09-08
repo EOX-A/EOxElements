500482c0ef1df7a2768da721daf691f35e66c307
Moritz Riede <moritz@mriede.com>
2023-09-08 20:39:00 +0200

    test: tighten test requirements for layer deletion

--- elements/layercontrol/test/general.cy.ts
+++ elements/layercontrol/test/general.cy.ts
@@ -101,7 +101,7 @@ describe("LayerControl", () => {
   });
 
   it("removes layers correctly in control and map", () => {
-    // Ignore this exception so that it does not break the test
+    // Ignore ResizeObserver exceptions so they do not break the test.
     cy.on('uncaught:exception', (err) => {
       if (err.message.includes('ResizeObserver loop')) {
         return false;
@@ -110,90 +110,122 @@ describe("LayerControl", () => {
       }
     });
 
-    cy.mount(`<eox-map
-    zoom="3"
-    center="[1000000, 6000000]"
-    layers='[
-      {
-        "type": "Group",
-        "id": "group1",
-        "title": "Background Layers",
-        "layerControlExpanded": true,
-        "layers": [
+    // We need a real map for this to work. 
+    // Let's use a subset of our standard demo map config:
+    cy
+      .mount(`<eox-map
+        zoom="3"
+        center="[1000000, 6000000]"
+        layers='[
           {
-            "type": "WebGLTile",
-            "id": "s2",
+            "type": "Group",
+            "id": "group1",
+            "title": "Background Layers",
             "layerControlExpanded": true,
-            "title": "s2",
-            "style": {
-              "variables": {
-                "red": 1,
-                "green": 2,
-                "blue": 3,
-                "redMax": 3000,
-                "greenMax": 3000,
-                "blueMax": 3000
-              },
-              "color": [
-                "array",
-                ["/", ["band", ["var", "red"]], ["var", "redMax"]],
-                ["/", ["band", ["var", "green"]], ["var", "greenMax"]],
-                ["/", ["band", ["var", "blue"]], ["var", "blueMax"]],
-                1
-              ],
-              "gamma": 1.1
-            },
-            "source": {
-              "type": "GeoTIFF",
-              "normalize": false,
-              "sources": [
-                {
-                  "url": "https://s2downloads.eox.at/demo/EOxCloudless/2020/rgbnir/s2cloudless2020-16bits_sinlge-file_z0-4.tif"
+            "layers": [
+              {
+                "type": "WebGLTile",
+                "id": "s2",
+                "layerControlExpanded": true,
+                "title": "s2",
+                "style": {
+                  "variables": {
+                    "red": 1,
+                    "green": 2,
+                    "blue": 3,
+                    "redMax": 3000,
+                    "greenMax": 3000,
+                    "blueMax": 3000
+                  },
+                  "color": [
+                    "array",
+                    ["/", ["band", ["var", "red"]], ["var", "redMax"]],
+                    ["/", ["band", ["var", "green"]], ["var", "greenMax"]],
+                    ["/", ["band", ["var", "blue"]], ["var", "blueMax"]],
+                    1
+                  ],
+                  "gamma": 1.1
+                },
+                "source": {
+                  "type": "GeoTIFF",
+                  "normalize": false,
+                  "sources": [
+                    {
+                      "url": "https://s2downloads.eox.at/demo/EOxCloudless/2020/rgbnir/s2cloudless2020-16bits_sinlge-file_z0-4.tif"
+                    }
+                  ]
                 }
-              ]
-            }
-          },
-          {
-            "type": "Tile",
-            "id": "osm",
-            "title": "Open Street Map",
-            "layerControlExpanded": false,
-            "visible": false,
-            "title": "osm",
-            "opacity": 0.5,
-            "source": {
-              "type": "OSM"
-            }
+              },
+              {
+                "type": "Tile",
+                "id": "osm",
+                "title": "Open Street Map",
+                "layerControlExpanded": false,
+                "visible": false,
+                "title": "osm",
+                "opacity": 0.5,
+                "source": {
+                  "type": "OSM"
+                }
+              }
+            ]
           }
         ]
-      }
-    ]
-  '
-  ></eox-map>`).as(
-      "eox-map"
-    );
+      '
+      ></eox-map>`)
+      .as(
+        "eox-map"
+      );
 
-    // Remove the LayerControl which bound to `mock-map`
+    // Remove the LayerControl bound to `mock-map`.
     cy.get('eox-layercontrol').invoke('remove');
 
-    cy.mount(
-      `
-      <eox-layercontrol for="eox-map"></eox-layercontrol>`
-    ).as("eox-layercontrol");
-
-    cy.get("eox-map").and(($el) => {
-      console.log(<MockMap>$el[0]);
-    });
-
+    // Replace it with our LayerControl which uses the actual Map component.
+    cy
+      .mount(
+        `
+        <eox-layercontrol for="eox-map"></eox-layercontrol>`
+      )
+      .as("eox-layercontrol");
+    
+    // Verify the deletion of the menu node.
     cy.get("eox-layercontrol")
       .shadow()
       .within(() => {
-        cy.get("eox-layerconfig")
+        // 1. Press the Delete button to delete the S2 layer.
+        cy
+          .get("[data-layer=\"s2\"] eox-layerconfig")
           .shadow()
-          .first()
           .within(() => {
             cy.get('div button.delete').should('be.visible').click();
           });
+        
+        // 2. Confirm the removal of the layer from the menu tree.
+        cy
+          .get("[data-layer=\"group1\"]")
+          .within(() => {
+            cy.get("[data-layer=\"s2\"]").should('not.exist');
+          });
+      });
+    
+    // Verify the deletion of the map layer itself.
+    cy
+      .get("eox-map")
+      .then($el => {
+        const eoxMap = <EOxMap>$el[0];
+        let layer;
+        try {
+          // TODO / WARNING: This part of the test is broken!
+          //
+          // The following function call does not handle map layers recursively. The test fails 
+          // as expected when presented with `group1`, but succeeds with `osm`!
+          //
+          // This needs to be corrected in `EOxMap` before we can correctly implement this test.
+          layer = eoxMap.getLayerById('s2');
+        } catch (error) {
+          console.error(error);
+        }
+        assert.equal(layer, undefined, 'OSM Layer should not be found');
       });
   });
 });
