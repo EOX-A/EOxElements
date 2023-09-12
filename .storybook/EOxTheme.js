fd29134e3a51555d303c817d9e749e7c0c790710
radupasparuga <radupasparuga25@gmail.com>
2023-09-13 01:41:06 +0300

    chore: update theme and replace js with typescript

---
+++ .storybook/EOxTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'EOX',
+  brandUrl: 'https://eox.at/',
+  brandImage: 'https://eox.at/EOX_Logo.svg',
+  brandTarget: '_self',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import EOxTheme from './EOxTheme';
+
+addons.setConfig({
+  theme: EOxTheme,
+});
