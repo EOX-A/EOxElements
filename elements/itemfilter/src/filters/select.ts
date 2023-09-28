e2e6c5b369da40ee68d9cf6a8f845e47b9fe0a51
silvester-pari <silvester.pari@eox.at>
2023-09-28 09:45:48 +0200

    feat: use new autocomplete and selectionlist elements in filters

--- elements/itemfilter/src/filters/select.ts
+++ elements/itemfilter/src/filters/select.ts
@@ -1,11 +1,16 @@
-import { LitElement, html, nothing } from "lit";
+import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { map } from "lit/directives/map.js";
+import { when } from "lit/directives/when.js";
+import "../autocomplete";
+import "../selectionlist";
 
 @customElement("eox-itemfilter-select")
 export class EOxItemFilterSelect extends LitElement {
   @property()
   filterObject: FilterObject;
+
+  @property({ type: Boolean })
+  unstyled = false;
 
   public reset() {
     this.renderRoot
@@ -29,35 +34,62 @@ export class EOxItemFilterSelect extends LitElement {
 
   render() {
     return html`
-      <ul>
-        ${map(
-          Object.keys(this.filterObject.state).sort((a, b) =>
-            a.localeCompare(b)
-          ),
-          (key) => html`
-            <li class=${this.filterObject.state[key] ? "highlighted" : nothing}>
-              <label>
-                <input
-                  name="selection"
-                  type="radio"
-                  class="select-radio"
-                  id=${key}
-                  checked="${this.filterObject.state[key] || nothing}"
-                  @click=${() => {
-                    for (const el in this.filterObject.state) {
-                      this.filterObject.state[el] = el === key;
-                    }
-                    this.filterObject.dirty = true;
-                    this.dispatchEvent(new CustomEvent("filter"));
-                    this.requestUpdate();
-                  }}
-                />
-                <span class="title">${key}</span>
-              </label>
-            </li>
-          `
-        )}
-      </ul>
+      ${when(
+        Object.keys(this.filterObject.state).length > 10,
+        () => html`
+          <eox-autocomplete
+            .items=${Object.keys(this.filterObject.state)
+              .sort((a, b) => a.localeCompare(b))
+              .map((i) => ({
+                id: i,
+                title: i.replace(/^./, i[0].toUpperCase()),
+              }))}
+            .selectedItems=${Object.keys(this.filterObject.state)
+              .filter((i) => this.filterObject.state[i])
+              .map((i) => ({
+                id: i,
+                title: i.replace(/^./, i[0].toUpperCase()),
+              }))}
+            .unstyled=${this.unstyled}
+            @items-selected=${(evt: CustomEvent) => {
+              Object.keys(this.filterObject.state).forEach((k) => {
+                this.filterObject.state[k] = evt.detail
+                  .map((i) => i.id)
+                  .includes(k);
+              });
+              this.filterObject.dirty = true;
+              this.dispatchEvent(new CustomEvent("filter"));
+            }}
+          >
+          </eox-autocomplete>
+        `,
+        () => html`
+          <eox-selectionlist
+            .items=${Object.keys(this.filterObject.state)
+              .sort((a, b) => a.localeCompare(b))
+              .map((i) => ({
+                id: i,
+                title: i.replace(/^./, i[0].toUpperCase()),
+              }))}
+            .selectedItems=${Object.keys(this.filterObject.state)
+              .filter((i) => this.filterObject.state[i])
+              .map((i) => ({
+                id: i,
+                title: i.replace(/^./, i[0].toUpperCase()),
+              }))}
+            .unstyled=${this.unstyled}
+            @items-selected=${(evt: CustomEvent) => {
+              Object.keys(this.filterObject.state).forEach((k) => {
+                this.filterObject.state[k] = evt.detail
+                  .map((i) => i.id)
+                  .includes(k);
+              });
+              this.filterObject.dirty = true;
+              this.dispatchEvent(new CustomEvent("filter"));
+            }}
+          ></eox-selectionlist>
+        `
+      )}
     `;
   }
 }
