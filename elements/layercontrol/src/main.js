36eb916cfe3886fd664c261f31f74b487f054125
Ahmed Behairi <behairiahmed@gmail.com>
2023-11-08 14:14:25 +0100

    fix: update optional layer list after layer removal (#391)
    * fix: optional list instant update; Close #342
    
    * chore: clean up
    
    * chore: clean up
    
    * revert: clean up

--- elements/layercontrol/src/main.js
+++ elements/layercontrol/src/main.js
@@ -114,7 +114,23 @@ export class EOxLayerControl extends LitElement {
             .titleProperty=${this.titleProperty}
             .tools=${this.tools}
             .unstyled=${this.unstyled}
-            @changed=${() => this.requestUpdate()}
+            @changed=${
+              /**
+               * @param {CustomEvent & {target: Element}} e
+               */
+              (e) => {
+                this.requestUpdate();
+                if (e.target.tagName === "EOX-LAYERCONTROL-LAYER-TOOLS") {
+                  /**
+                   * @type Element & { requestUpdate: function }
+                   */
+                  const optionalListEl = this.renderRoot.querySelector(
+                    "eox-layercontrol-optional-list"
+                  );
+                  optionalListEl?.requestUpdate();
+                }
+              }
+            }
           ></eox-layercontrol-layer-list>
         `
       )}
