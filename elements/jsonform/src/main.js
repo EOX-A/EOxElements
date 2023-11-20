638a53cba1ae0fc11877eb1259cf802b7efad6ca
Srijit S Madhavan <srijitsm123@gmail.com>
2023-11-20 19:26:48 +0530

    feat: initial eox-jsonform implementation (#426)
    * chore: initial jsonform setup
    
    * cleaned form code
    
    * change js type
    
    * logic for dynamic xyz
    
    * fix not a func
    
    * implemented form filter to jsonform
    
    * fix CHANGELOG
    
    * fix test
    
    * change icon
    
    * added style to json form
    
    * removed old example of map and eox-jsonform
    
    * added form to config control
    
    * logic changed from map to layer config
    
    * revert layerconfig changes
    
    * first set of requested changes
    
    * second set of requested changes
    
    ---------
    
    Co-authored-by: silvester-pari <silvester.pari@eox.at>

---
+++ elements/jsonform/src/main.js
@@ -0,0 +1,140 @@
+import { JSONEditor } from "@json-editor/json-editor/dist/jsoneditor.js";
+import { LitElement, html } from "lit";
+import { style } from "./style";
+import { styleEOX } from "./style.eox";
+
+/**
+ * @typedef {JSON & {properties: object}} JsonSchema
+ * @element eox-jsonform
+ */
+
+export class EOxJSONForm extends LitElement {
+  static properties = {
+    schema: { attribute: false, type: Object },
+    startVals: { attribute: false, type: Object },
+    options: { attribute: false, type: Object },
+    unstyled: { type: Boolean },
+  };
+
+  /**
+   * data input by the user
+   * @type {{[key: string]: any}}
+   */
+  #data = {};
+
+  /**
+   * editor instance generated through - JSONEditor
+   * @type {{[key: string]: any}}
+   */
+  #editor = null;
+
+  constructor() {
+    super();
+
+    /**
+     * Schema for the form editor
+     * @type {JsonSchema}
+     */
+    this.schema;
+
+    /**
+     * Default values for the form editor
+     * @type {JsonSchema}
+     */
+    this.startVals;
+
+    /**
+     * Default values for the form editor
+     * @type {object}
+     */
+    this.options = {};
+
+    /**
+     * Render the element without additional styles
+     */
+    this.unstyled = false;
+  }
+
+  /**
+   * Get The JSON schema for rendering the form
+   */
+  getSchema() {
+    return this.schema;
+  }
+  /**
+   * @param {JsonSchema} newSchema
+   */
+  setSchema(newSchema) {
+    this.schema = newSchema;
+  }
+
+  /**
+   * Get default value for rendering the form
+   */
+  getDefaultValues() {
+    return this.startVals;
+  }
+  /**
+   * @param {JsonSchema} newVal
+   */
+  setDefaultValues(newVal) {
+    this.startVals = newVal;
+  }
+
+  /**
+   * Get default value for rendering the form
+   */
+  getValues() {
+    return this.#data;
+  }
+
+  #emitData() {
+    /**
+     * Data object has been changed
+     */
+    this.dispatchEvent(
+      new CustomEvent(`change`, {
+        detail: this.#data,
+        bubbles: true,
+        composed: true,
+      })
+    );
+    this.requestUpdate();
+  }
+
+  firstUpdated() {
+    if (!this.#editor) {
+      const formEle = this.renderRoot.querySelector("form");
+
+      this.#editor = new JSONEditor(formEle, {
+        schema: this.schema,
+        ...(this.startVals ? { startval: this.startVals } : {}),
+        theme: "html",
+        ajax: true,
+        ...this.options,
+      });
+
+      this.#editor.on("ready", () => {
+        this.#data = this.#editor.getValue();
+        this.#emitData();
+      });
+
+      this.#editor.on("change", () => {
+        this.#data = this.#editor.getValue();
+        this.#emitData();
+      });
+    }
+  }
+
+  render() {
+    return html`
+      <style>
+        ${style}
+          ${!this.unstyled && styleEOX}
+      </style>
+      <form></form>
+    `;
+  }
+}
+
+customElements.define("eox-jsonform", EOxJSONForm);
---
+++ elements/jsonform/src/style.eox.js
@@ -0,0 +1,113 @@
+import { checkbox } from "../../../utils/styles/checkbox";
+import { radio } from "../../../utils/styles/radio";
+import { slider } from "../../../utils/styles/slider";
+
+export const styleEOX = `
+  ${checkbox}
+  ${radio}
+  ${slider}
+
+  ul {
+    padding-left: 0;
+    margin-top: 0;
+  }
+  li {
+    list-style: none;
+  }
+  li span {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  li label {
+    display: flex;
+    align-items: center;
+  }
+  li,
+  label,
+  details,
+  input[type="checkbox"],
+  input[type="radio"] {
+    cursor: pointer;
+  }
+  input[type="checkbox"],
+  input[type="radio"] {
+    margin: 0;
+  }
+  input[type="text"], select {
+    box-sizing: border-box !important;
+    width: 100% !important;
+    margin: 0.5rem 0rem !important;
+    padding: 5px 7px !important;
+    border-radius: 4px !important;
+    border: 1px solid #0004 !important;
+  }
+  form[data-theme="html"] .je-indented-panel {
+    border: 0px !important;
+    padding: 12px !important;
+    margin: 0px !important;
+  }
+  section:not(section:last-of-type) {
+    margin-bottom: 1rem;
+  }
+  ul li {
+    padding: 5px 10px;
+  }
+  li.highlighted {
+    background: #00417011;
+  }
+  section {
+    position: relative;
+  }
+  .je-indented-panel {
+    border: 0 !important;
+    padding: 12px !important;
+    margin: 0px !important;
+  }
+  label {
+    font-weight: 500 !important;
+    margin-bottom: 0px !important;
+  }
+  p {
+    margin: 0.5rem 0;
+  }
+  label.je-form-input-label {
+    display: flex;
+    font-weight: 500;
+    font-size: small;
+    align-items: center;
+    align-items: center;
+    border-bottom: 1px solid #0002;
+    padding: 0.5rem 0;
+  }
+  .je-range-control {
+    padding: 0.5rem 0;
+  }
+  .errmsg {
+    font-size: x-small;
+  }
+  .je-header, .je-object__controls {
+    display: none !important;
+  }
+  button.json-editor-btn-collapse {
+    box-shadow: none;
+    display: inline;
+    background: none;
+    width: 24px;
+    height: 24px;
+    font-size: unset;
+    padding: 0;
+    margin: 0;
+    transform: rotate(270deg);
+    border: 0;
+  }
+  button.json-editor-btn-collapse span {
+    text-indent: -999999px;
+    display: flex;
+  }
+  button.json-editor-btn-collapse span:before {
+    position: absolute;
+    text-indent: 0;
+    line-height: initial;
+  }
+`;
