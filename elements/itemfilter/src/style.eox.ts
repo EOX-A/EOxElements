12399bf02d84d68c038d926aad350b6f3e08ccf5
A-Behairi <behairiahmed@gmail.com>
2023-11-21 13:42:07 +0100

    fix: reset icon size

--- elements/itemfilter/src/style.eox.ts
+++ elements/itemfilter/src/style.eox.ts
@@ -166,7 +166,7 @@ eox-itemfilter-range {
 eox-itemfilter-expandcontainer button.reset-icon{
   content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="%23004170" d="M12 4c2.1 0 4.1.8 5.6 2.3c3.1 3.1 3.1 8.2 0 11.3c-1.8 1.9-4.3 2.6-6.7 2.3l.5-2c1.7.2 3.5-.4 4.8-1.7c2.3-2.3 2.3-6.1 0-8.5C15.1 6.6 13.5 6 12 6v4.6l-5-5l5-5V4M6.3 17.6C3.7 15 3.3 11 5.1 7.9l1.5 1.5c-1.1 2.2-.7 5 1.2 6.8c.5.5 1.1.9 1.8 1.2l-.6 2c-1-.4-1.9-1-2.7-1.8Z"%2F%3E%3C%2Fsvg%3E');
   margin-left: 4px;
-  height:16px;
-  width:16px;
+  height:14px;
+  width:14px;
 }
   `;
