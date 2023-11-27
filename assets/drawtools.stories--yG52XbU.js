e698a3db9865bc5eb6952562ec6342026d627bc1
silvester-pari <silvester.pari@eox.at>
2023-11-27 18:29:03 +0000

    Deploying to gh-pages from @ EOX-A/EOxElements@7fd49f36bb3b46e0b7d9113cb4821ac67e4362a0 🚀

---
+++ assets/drawtools.stories--yG52XbU.js
@@ -0,0 +1,251 @@
+var z=Object.defineProperty;var J=(e,r,t)=>r in e?z(e,r,{enumerable:!0,configurable:!0,writable:!0,value:t}):e[r]=t;var n=(e,r,t)=>(J(e,typeof r!="symbol"?r+"":r,t),t),x=(e,r,t)=>{if(!r.has(e))throw TypeError("Cannot "+t)};var o=(e,r,t)=>(x(e,r,"read from private field"),t?t.call(e):r.get(e)),f=(e,r,t)=>{if(r.has(e))throw TypeError("Cannot add the same private member more than once");r instanceof WeakSet?r.add(e):r.set(e,t)},g=(e,r,t,s)=>(x(e,r,"write to private field"),s?s.call(e,t):r.set(e,t),t);import{s as q,x as l,T as y}from"./lit-element-Qm8PRmVu.js";import"./main-oNUOwFZd.js";import{i as N}from"./keyed-GB4NtsQP.js";import{b as W}from"./button-KPw86qfe.js";import"./state-ncEgtE_C.js";import"./index-EySAwWXj.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-YTb-mT7D.js";import"../sb-preview/runtime.js";import"./directive-xgBC_cM0.js";import"./directive-helpers-k6EzVOeb.js";const A=`
+  * {
+    font-family: Roboto, sans-serif;
+  }
+
+  ${W}
+
+  button.discard:before {
+    content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23FF5252' viewBox='0 0 24 24'%3E%3Ctitle%3Etrash-can-outline%3C/title%3E%3Cpath d='M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z' /%3E%3C/svg%3E")
+  }
+
+  button.polygon:before {
+    content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23004170' viewBox='0 0 24 24'%3E%3Ctitle%3Eshape-polygon-plus%3C/title%3E%3Cpath d='M17,15.7V13H19V17L10,21L3,14L7,5H11V7H8.3L5.4,13.6L10.4,18.6L17,15.7M22,5V7H19V10H17V7H14V5H17V2H19V5H22Z' /%3E%3C/svg%3E");
+  }
+  ul {
+    padding: 0;
+  }
+  ul ul {
+    padding-left: 48px;
+  }
+  li:hover {
+    background: #f0f5f9;
+  }
+  li.selected {
+    background: #f0f5f9;
+  }
+  li {
+    list-style: none;
+    padding: 4px;
+  }
+  li {
+    border-bottom: 1px solid #0041703a;
+  }
+  li:first-child {
+    border-top: 1px solid #0041703a;
+  }
+  li.sortable-chosen {
+    background: #eeea;
+  }
+  li.sortable-drag {
+    opacity: 0;
+  }
+  li.sortable-ghost {
+  }
+  button.small.icon, button.small.icon::before {
+    height: 16px;
+    width: 16px;
+    padding: 0px;
+  }
+  button.discard.icon {
+    opacity: .7;
+  }
+  button.discard.icon:hover {
+    opacity: 1;
+  }
+  eox-drawtools-list {
+    width: 100%;
+  }
+  .list {
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    font-size: small;
+    gap: 10px;
+
+  }
+  label {
+    gap: 10px;
+  }
+  label, span {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    font-size: small;
+    flex-grow: 1;
+  }
+`;class P extends q{constructor(){super();n(this,"hoverInteraction");n(this,"clickInteraction");n(this,"hoverId");n(this,"clickId");this.eoxMap=null,this.olMap=null,this.draw=null,this.layer="draw",this.drawLayer=null,this.drawnFeatures=[],this.modify=null,this.unstyled=!1}_handleDelete(t){t.stopPropagation();const s=t.target.getAttribute("index"),i=this.drawnFeatures[s];this.drawLayer.getSource().removeFeature(i),this.drawnFeatures.splice(s,1),this.requestUpdate()}_handleFeatureSelectAndDeselect(t){const s=t.get("id");if(this.clickId===s){const i=this.drawLayer.getSource().getExtent();this.olMap.getView().fit(i,{duration:750}),this.clickInteraction.highlightById([])}else this.clickInteraction.highlightById([s]),this.olMap.getView().fit(t.getGeometry().getExtent(),{duration:750});this.requestUpdate()}firstUpdated(){this.hoverInteraction=this.eoxMap.selectInteractions.selectHover,this.clickInteraction=this.eoxMap.selectInteractions.selectClick,this.hoverInteraction.selectStyleLayer.on("change",()=>this.requestUpdate()),this.clickInteraction.selectStyleLayer.on("change",()=>this.requestUpdate())}render(){var t,s;return this.hoverId=(t=this.hoverInteraction)==null?void 0:t.selectedFids[0],this.clickId=(s=this.clickInteraction)==null?void 0:s.selectedFids[0],l`
+      <style>
+        ${!this.unstyled&&A}
+      </style>
+      <ul>
+        ${this.drawnFeatures.map((i,w)=>{const c=i.get("id"),_=this.hoverId===c||this.clickId===c;return N(w+1,l`
+              <li
+                class="${_?"selected":y}"
+                @mouseover=${()=>{this.clickId!==c&&this.hoverInteraction.highlightById([c])}}
+                @mouseout=${()=>{this.clickId!==c&&this.hoverInteraction.highlightById([])}}
+              >
+                <div
+                  class="list"
+                  @click="${()=>this._handleFeatureSelectAndDeselect(i)}"
+                >
+                  <span class="title">Feature #${w+1}</span>
+                  <button
+                    index=${w}
+                    class="icon small discard"
+                    @click="${this._handleDelete}"
+                  >
+                    ${this.unstyled?"x":y}
+                  </button>
+                </div>
+              </li>
+            `)})}
+      </ul>
+    `}}n(P,"properties",{eoxMap:{attribute:!1,state:!0},olMap:{attribute:!1,state:!0},draw:{attribute:!1,state:!0},layer:{type:String},drawLayer:{attribute:!1,state:!0},drawnFeatures:{attribute:!1,state:!0,type:Array},modify:{attribute:!1,state:!0},unstyled:{type:Boolean}});customElements.define("eox-drawtools-list",P);const Z=`
+:host {
+  display: block;
+}
+`;var a,d;class j extends q{constructor(){super();f(this,a,void 0);f(this,d,void 0);this.allowModify=!1,this.for="eox-map",this.currentlyDrawing=!1,this.draw=null,this.drawLayer=null,this.drawnFeatures=[],this.modify=null,this.multipleFeatures=!1,this.showList=!1,this.unstyled=!1}static get properties(){return{allowModify:{attribute:"allow-modify",type:Boolean},for:{type:String},currentlyDrawing:{attribute:!1,state:!0,type:Boolean},draw:{attribute:!1,state:!0},drawLayer:{attribute:!1,state:!0},drawnFeatures:{attribute:!1,state:!0,type:Array},modify:{attribute:!1,state:!0},multipleFeatures:{attribute:"multiple-features",type:Boolean},showList:{attribute:"show-list",type:Boolean},unstyled:{type:Boolean}}}initDrawLayer(){var s,i;const t=document.querySelector(this.for);g(this,a,t),g(this,d,o(this,a).map),this.drawLayer=o(this,a).addOrUpdateLayer({type:"Vector",properties:{id:"drawLayer",layerControlHide:!0},source:{type:"Vector"},...o(this,a).interactions.drawInteraction?{}:{interactions:[{type:"draw",options:{active:!1,id:"drawInteraction",type:"Polygon",modify:this.allowModify,stopClick:!0}},{type:"select",options:{id:"selectHover",condition:"pointermove",style:{"fill-color":"rgba(51, 153, 204,0.5)","stroke-color":"#3399CC","stroke-width":2.5}}},{type:"select",options:{id:"selectClick",condition:"click",panIn:!0,style:{"fill-color":"rgba(51, 153, 204,0.5)","stroke-color":"#3399CC","stroke-width":2.5}}}]}}),this.draw=o(this,a).interactions.drawInteraction,this.modify=o(this,a).interactions.drawInteractionmodify,(s=this.draw)==null||s.on("drawend",()=>this.onDrawEnd()),(i=this.modify)==null||i.on("modifyend",()=>this.onModifyEnd())}startDrawing(){this.initDrawLayer(),this.draw.setActive(!0),this.currentlyDrawing=!0,this.requestUpdate()}discardDrawing(){this.drawnFeatures=[],this.draw.setActive(!1),this.drawLayer.getSource().clear(),o(this,a).removeInteraction("drawInteraction"),o(this,d).removeLayer(this.drawLayer),this.emitDrawnFeatures(),this.currentlyDrawing=!1,this.requestUpdate()}onDrawEnd(){this.emitDrawnFeatures(),this.draw.setActive(!1),this.currentlyDrawing=!1,this.requestUpdate()}onModifyEnd(){this.emitDrawnFeatures()}emitDrawnFeatures(){setTimeout(()=>{this.drawnFeatures=this.drawLayer.getSource().getFeatures(),this.requestUpdate(),this.dispatchEvent(new CustomEvent("drawupdate",{detail:this.drawnFeatures}))},0)}render(){var t,s,i;return l`
+      <style>
+        ${Z}
+        ${!this.unstyled&&A}
+      </style>
+      <div>
+        <slot></slot>
+        <button
+          data-cy="drawBtn"
+          class="polygon icon"
+          disabled="${!this.multipleFeatures&&((t=this.drawnFeatures)==null?void 0:t.length)>0||this.currentlyDrawing||y}"
+          @click="${()=>this.startDrawing()}"
+        >
+          ${this.currentlyDrawing?"drawing":"draw"}
+        </button>
+        <button
+          data-cy="discardBtn"
+          class="discard icon"
+          disabled="${!((s=this.drawnFeatures)!=null&&s.length)&&!this.currentlyDrawing||y}"
+          @click="${()=>this.discardDrawing()}"
+        >
+          discard
+        </button>
+      </div>
+      ${this.showList&&((i=this.drawnFeatures)!=null&&i.length)?l`<eox-drawtools-list
+            .eoxMap=${o(this,a)}
+            .olMap=${o(this,d)}
+            .draw=${this.draw}
+            .layer=${this.layer}
+            .drawLayer=${this.drawLayer}
+            .drawnFeatures=${this.drawnFeatures}
+            .modify=${this.modify}
+            .unstyled=${this.unstyled}
+            @changed=${()=>this.requestUpdate()}
+          >
+          </eox-drawtools-list>`:y}
+    `}}a=new WeakMap,d=new WeakMap;customElements.define("eox-drawtools",j);const ot={title:"Elements/eox-drawtools",tags:["autodocs"],component:"eox-drawtools"},m={args:{allowModify:!1,multipleFeatures:!1},render:e=>l` <eox-map
+      id="primary"
+      style="width: 400px; height: 300px;"
+      layers='[
+    {"type":"Tile","source":{"type":"OSM"}}
+  ]'
+    ></eox-map>
+    <eox-drawtools
+      for="eox-map#primary"
+      .allowModify=${e.allowModify}
+      .multipleFeatures=${e.multipleFeatures}
+    >
+    </eox-drawtools>`},p={render:()=>l` <eox-map
+      id="multi"
+      style="width: 400px; height: 300px;"
+      layers='[
+      {"type":"Tile","source":{"type":"OSM"}}
+    ]'
+    ></eox-map>
+    <eox-drawtools for="eox-map#multi" multiple-features></eox-drawtools>`},u={render:()=>l` <eox-map
+      id="modify"
+      style="width: 400px; height: 300px;"
+      layers='[
+      {"type":"Tile","source":{"type":"OSM"}}
+    ]'
+    ></eox-map>
+    <eox-drawtools
+      for="eox-map#modify"
+      multiple-features
+      allow-modify
+    ></eox-drawtools>`},h={render:()=>l`
+    <div style="display: flex">
+      <eox-map
+        id="list"
+        style="width: 500px; height: 300px;"
+        layers=${JSON.stringify([{type:"Tile",source:{type:"OSM"}}])}
+      ></eox-map>
+      <eox-drawtools
+        for="eox-map#list"
+        multiple-features
+        show-list
+      ></eox-drawtools>
+    </div>
+  `};var b,M,v;m.parameters={...m.parameters,docs:{...(b=m.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    allowModify: false,
+    multipleFeatures: false
+  },
+  render: args => html\` <eox-map
+      id="primary"
+      style="width: 400px; height: 300px;"
+      layers='[
+    {"type":"Tile","source":{"type":"OSM"}}
+  ]'
+    ></eox-map>
+    <eox-drawtools
+      for="eox-map#primary"
+      .allowModify=\${args.allowModify}
+      .multipleFeatures=\${args.multipleFeatures}
+    >
+    </eox-drawtools>\`
+}`,...(v=(M=m.parameters)==null?void 0:M.docs)==null?void 0:v.source}}};var F,I,L,$,k;p.parameters={...p.parameters,docs:{...(F=p.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => html\` <eox-map
+      id="multi"
+      style="width: 400px; height: 300px;"
+      layers='[
+      {"type":"Tile","source":{"type":"OSM"}}
+    ]'
+    ></eox-map>
+    <eox-drawtools for="eox-map#multi" multiple-features></eox-drawtools>\`
+}`,...(L=(I=p.parameters)==null?void 0:I.docs)==null?void 0:L.source},description:{story:"By setting the `multiple-features` attribute or `multipleFeatures` property to `true`,\nmultiple polygons can be drawn.",...(k=($=p.parameters)==null?void 0:$.docs)==null?void 0:k.description}}};var D,E,S,V,H;u.parameters={...u.parameters,docs:{...(D=u.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => html\` <eox-map
+      id="modify"
+      style="width: 400px; height: 300px;"
+      layers='[
+      {"type":"Tile","source":{"type":"OSM"}}
+    ]'
+    ></eox-map>
+    <eox-drawtools
+      for="eox-map#modify"
+      multiple-features
+      allow-modify
+    ></eox-drawtools>\`
+}`,...(S=(E=u.parameters)==null?void 0:E.docs)==null?void 0:S.source},description:{story:"By setting the `allow-modify` attribute or `allowModify` property,\nthe user can modify features after drawing",...(H=(V=u.parameters)==null?void 0:V.docs)==null?void 0:H.description}}};var C,B,O,T,U;h.parameters={...h.parameters,docs:{...(C=h.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: () => html\`
+    <div style="display: flex">
+      <eox-map
+        id="list"
+        style="width: 500px; height: 300px;"
+        layers=\${JSON.stringify([{
+    type: "Tile",
+    source: {
+      type: "OSM"
+    }
+  }])}
+      ></eox-map>
+      <eox-drawtools
+        for="eox-map#list"
+        multiple-features
+        show-list
+      ></eox-drawtools>
+    </div>
+  \`
+}`,...(O=(B=h.parameters)==null?void 0:B.docs)==null?void 0:O.source},description:{story:"By setting the `show-list` attribute or `showList` property to `true`,\nList of features will be visible",...(U=(T=h.parameters)==null?void 0:T.docs)==null?void 0:U.description}}};const lt=["Primary","MultiPolygon","ModifyFeatures","MultiPolygonWithList"];export{u as ModifyFeatures,p as MultiPolygon,h as MultiPolygonWithList,m as Primary,lt as __namedExportsOrder,ot as default};
+//# sourceMappingURL=drawtools.stories--yG52XbU.js.map
--- assets/preview-2XHe0cQ9.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./iframe-OEoLUvD_.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-l4qGDlph.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-2XHe0cQ9.js.map
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-l4qGDlph.js","./iframe-OEoLUvD_.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./index-Xo9UOPVq.js","./index-F5t8cyY2.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
