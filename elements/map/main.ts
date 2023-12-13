2ad7893e2d914d775095d8799e0a821b619a9cc9
Silvester <silvester.pari@eox.at>
2023-12-13 12:47:24 +0100

    feat!: extract advanced layers & sources into separate bundle (#515)
    * feat: extract advanced layers & sources into separate bundle
    
    * chore(docs): add information about plugin
    
    * chore(lint): update linter rules
    
    * chore(docs): improve eror message
    
    * chore(docs): update layers and sources description

--- elements/map/main.ts
+++ elements/map/main.ts
@@ -18,6 +18,38 @@ import {
 import { buffer } from "ol/extent";
 import "./src/compare";
 
+/**
+ * The `eox-map` is a wrapper for the library [OpenLayers](https://openlayers.org/) with additional features and helper functions.
+ *
+ * Basic usage:
+ *
+ * ```
+ * import "@eox/map"
+ *
+ * <eox-map [...]></eox-map>
+ * ```
+ *
+ * Some basic layers, sources and formats are included in the default bundle, for advanced usage it is
+ * required to import the `advanced-layers-and-sources` plugin.
+ *
+ * Included in the base bundle:
+ * - Formats: `GeoJSON`, `MVT`
+ * - Layers: `Group`, `Image`, `Tile`, `Vector`, `VectorTile`
+ * - Sources: `ImageWMS`, `OSM`, `Tile`, `TileWMS`, `Vector`, `VectorTile`, `WMTS`, `XYZ`
+ *
+ * In order to use the rest of the layers and sources provided by OpenLayers, import the plugin as well:
+ *
+ * ```
+ * import "@eox/map/dist/eox-map-advanced-layers-and-sources.js"
+ * import "@eox/map/dist/eox-map.js"
+ *
+ * <eox-map [...]></eox-map>
+ * ```
+ * Included in the advanced plugin bundle:
+ * - Layers: All OpenLayers layer types, plus [STAC](https://github.com/m-mohr/ol-stac)
+ * - Sources: All OpenLayers source types
+ * - Reprojection through [proj4](https://github.com/proj4js/proj4js)
+ */
 @customElement("eox-map")
 export class EOxMap extends LitElement {
   /**
