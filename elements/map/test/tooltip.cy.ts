e8359d8f86011be111463494f881862fb7d3ddce
silvester-pari <silvester.pari@eox.at>
2024-01-10 17:13:54 +0100

    chore: add test for multiple layers

--- elements/map/test/tooltip.cy.ts
+++ elements/map/test/tooltip.cy.ts
@@ -2,6 +2,7 @@ import { html } from "lit";
 import "../main";
 import vectorLayerStyleJson from "./hoverInteraction.json";
 import { simulateEvent } from "./utils/events";
+import { EoxLayer } from "../src/generate";
 
 describe("tooltip", () => {
   it("displays a tooltip on hover", () => {
@@ -28,6 +29,66 @@ describe("tooltip", () => {
         cy.get("eox-map-tooltip").and(($el) => {
           console.log($el[0]);
         });
+        cy.get("eox-map-tooltip")
+          .shadow()
+          .within(() => {
+            cy.get("ul").should("exist");
+            cy.get("ul").children().should("have.length", 9);
+          });
+      });
+  });
+
+  it.only("displays a tooltip on hover when multiple layers are initialized and only one visible", () => {
+    cy.intercept("https://openlayers.org/data/vector/ecoregions.json", {
+      fixture: "/ecoregions.json",
+    });
+    const multiplelayersJson = [...vectorLayerStyleJson] as EoxLayer[];
+    const secondLayer = structuredClone(vectorLayerStyleJson[0]) as EoxLayer;
+    multiplelayersJson.unshift(secondLayer);
+    secondLayer.properties.id = "regions2";
+    secondLayer.interactions[0].options.id = "selectInteraction2";
+    // @ts-ignore
+    secondLayer.interactions[0].options.layer.properties.id = "selectLayer2";
+    secondLayer.visible = false;
+    console.log(multiplelayersJson);
+    cy.mount(
+      html`<eox-map .layers=${multiplelayersJson}>
+        <eox-map-tooltip></eox-map-tooltip>
+      </eox-map>`
+    ).as("eox-map");
+    cy.get("eox-map").and(($el) => {
+      const eoxMap = <EOxMap>$el[0];
+
+      eoxMap.map.on("loadend", () => {
+        simulateEvent(eoxMap.map, "pointermove", 120, -140);
+      });
+    });
+
+    // first check if the tooltip on the first rendered layer works
+    cy.get("eox-map")
+      .shadow()
+      .within(() => {
+        cy.get("eox-map-tooltip").should("exist");
+        cy.get("eox-map-tooltip")
+          .shadow()
+          .within(() => {
+            cy.get("ul").should("exist");
+            cy.get("ul").children().should("have.length", 9);
+          });
+      });
+
+    // hide first rendered layer and show second layer
+    cy.get("eox-map").and(($el) => {
+      const eoxMap = <EOxMap>$el[0];
+      eoxMap.map.getLayers().getArray()[0].setVisible(false);
+      eoxMap.map.getLayers().getArray()[1].setVisible(true);
+    });
+
+    // check if the tooltip on the second rendered layer works
+    cy.get("eox-map")
+      .shadow()
+      .within(() => {
+        cy.get("eox-map-tooltip").should("exist");
         cy.get("eox-map-tooltip")
           .shadow()
           .within(() => {
