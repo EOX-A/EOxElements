acced905e93ec89f6ee071e871b217b7423f74e9
Silvester <silvester.pari@eox.at>
2024-01-17 07:20:47 +0100

    fix: make sure a fallback layer id is rendered in time in order to support sorting (#588)

--- elements/layercontrol/src/helpers/check-properties.js
+++ elements/layercontrol/src/helpers/check-properties.js
@@ -5,20 +5,37 @@
  * @param {import("ol").Collection<import("ol/layer").Layer | import("ol/layer").Group>} collection - The collection of layers to be checked.
  * @param {string} idProperty - The property used as the ID.
  * @param {string} titleProperty - The property used as the title.
+ * @param {import("lit").LitElement} that - The LitElement instance.
  */
 //
-export default function checkProperties(collection, idProperty, titleProperty) {
+export default function checkProperties(
+  collection,
+  idProperty,
+  titleProperty,
+  that
+) {
   const layerArray = collection.getArray(); // Get an array of layers from the collection.
 
+  let requestUpdateNecessary = false;
   // Loop through each layer in the array to check and assign properties if missing.
   layerArray.forEach((layer) => {
     //@ts-ignore
     const olUID = layer.ol_uid;
 
     // Check and assign an ID property if it's missing.
-    if (!layer.get(idProperty)) layer.set(idProperty, olUID);
+    if (!layer.get(idProperty)) {
+      layer.set(idProperty, olUID);
+      requestUpdateNecessary = true;
+    }
 
     // Check and assign a title property if it's missing.
-    if (!layer.get(titleProperty)) layer.set(titleProperty, `layer ${olUID}`);
+    if (!layer.get(titleProperty)) {
+      layer.set(titleProperty, `layer ${olUID}`);
+      requestUpdateNecessary = true;
+    }
+
+    if (requestUpdateNecessary) {
+      that.requestUpdate();
+    }
   });
 }
