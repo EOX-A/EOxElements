93e03e1f0f5d0d72a365a6cbfc93ac9d3b4f254c
silvester-pari <silvester.pari@eox.at>
2024-01-31 15:57:32 +0000

    Deploying to gh-pages from @ EOX-A/EOxElements@f9eb5fc9a8764cb2b871c2ba0395df003851d513 🚀

--- assets/preview-ZrJXLf9g.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-W89OKkhr.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-NA6-B6-7.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-NA6-B6-7.js","./iframe-W89OKkhr.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./index-uxaVTLnz.js","./index-F5t8cyY2.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
