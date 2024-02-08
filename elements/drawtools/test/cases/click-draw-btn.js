b5dc416fef60c193d8117715f98d07addf4c253f
srijitcoder <srijitsm123@gmail.com>
2024-02-08 18:25:00 +0530

    removed one cypress it function from double level - redundancy issue

--- elements/drawtools/test/cases/click-draw-btn.js
+++ elements/drawtools/test/cases/click-draw-btn.js
@@ -7,25 +7,23 @@ const { drawTools, controller, drawBtn } = TEST_SELECTORS;
  * Clicks the draw button and verifies the 'drawing' state.
  */
 const clickDrawBtnTest = () => {
-  it("clicks the draw button", () => {
-    cy.get(drawTools)
-      .shadow()
-      .within(() => {
-        cy.get(controller)
-          .shadow()
-          .within(() => {
-            // Verify the initial state of the draw button
-            cy.get(drawBtn).contains("draw");
-            cy.get(drawBtn).should("not.contain", "drawing");
+  cy.get(drawTools)
+    .shadow()
+    .within(() => {
+      cy.get(controller)
+        .shadow()
+        .within(() => {
+          // Verify the initial state of the draw button
+          cy.get(drawBtn).contains("draw");
+          cy.get(drawBtn).should("not.contain", "drawing");
 
-            // Click the draw button and verify the 'drawing' state
-            cy.get(drawBtn).click();
-            cy.get(drawBtn).contains("drawing");
-          });
-      });
+          // Click the draw button and verify the 'drawing' state
+          cy.get(drawBtn).click();
+          cy.get(drawBtn).contains("drawing");
+        });
+    });
 
-    // TODO: Simulate drawing and add drawn features (not implemented in the current test)
-  });
+  // TODO: Simulate drawing and add drawn features (not implemented in the current test)
 };
 
 export default clickDrawBtnTest;
