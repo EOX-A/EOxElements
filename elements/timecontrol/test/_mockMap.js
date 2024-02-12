e1cfb96ceb9940f36bb23e0f79db950f26108876
silvester-pari <silvester.pari@eox.at>
2024-02-12 13:09:05 +0100

    chore(test): add initial test cases

---
+++ elements/timecontrol/test/_mockMap.js
@@ -0,0 +1,41 @@
+// Defining a MockMap class extending HTMLElement
+export class MockMap extends HTMLElement {
+  constructor() {
+    super();
+    // Initializing features array
+    this.features = [];
+    // Initializin mock params
+    (this.params = {
+      TIME: "2024-01-01",
+    }),
+      // Simulating a map object with necessary methods
+      (this.map = {
+        // Simulating getLayers method
+        getLayers: () => ({
+          // Simulating getArray method
+          getArray: () => [
+            {
+              // Simulating get method
+              get: () => "TEST_ID",
+              getSource: () => ({
+                // Simulating getParams method to update the source url
+                getParams: () => this.params,
+                // Simulating updateParams method to update the source url
+                updateParams: (params) => {
+                  Object.assign(this.params, params);
+                },
+              }),
+            },
+          ],
+        }),
+        events: {
+          loadend: () => undefined,
+        },
+        // @ts-ignore
+        once: (event, fun) => (this.map.events = { [event]: fun }),
+      });
+  }
+}
+
+// Registering the MockMap custom element
+customElements.define("mock-map", MockMap);
