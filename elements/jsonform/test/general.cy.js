036ec34da80cd7e586d9cf763fd348e25f085032
silvester-pari <silvester.pari@eox.at>
2024-02-13 15:09:53 +0100

    chore: fix linting error

--- elements/jsonform/test/general.cy.js
+++ elements/jsonform/test/general.cy.js
@@ -1,10 +1,6 @@
 // Importing necessary modules, test cases, and enums
 import "../src/main";
 import { loadJsonFormTest } from "./cases";
-import { TEST_SELECTORS } from "../src/enums";
-
-// Destructuring TEST_SELECTORS object
-const { jsonForm } = TEST_SELECTORS;
 
 // Test suite for Drawtools
 describe("Drawtools", () => {
