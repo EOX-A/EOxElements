852c8c65d1cb6d02198c31266f50c5139978b0f0
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-03-05 18:08:02 +0200

    chore: remove vite config from layercontrol

--- elements/layercontrol/vite.config.js
+++ elements/layercontrol/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +10,4 @@ export default defineConfig({
       fileName: "eox-layercontrol",
     },
   },
-  plugins: [nodePolyfills()],
 });
