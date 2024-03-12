81381723f162d0d29c4ab45cb4b28ae7b6b0105e
Silvester <silvester.pari@eox.at>
2024-03-12 07:26:17 +0100

    chore(deps): upgrade storybook to v8 (#743)
    * chore(deps): upgrade storybook to v8
    
    * chore: harmonize CSF formatting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 import { readCsf } from "@storybook/csf-tools";
 
@@ -17,8 +18,8 @@ const config = {
   },
   experimental_indexers: (existingIndexers) =>
     [csfIndexer].concat(existingIndexers || []),
-  addons: ["@storybook/addon-essentials"],
-  framework: "@storybook/web-components-vite",
+  addons: [getAbsolutePath("@storybook/addon-essentials")],
+  framework: getAbsolutePath("@storybook/web-components-vite"),
   docs: {
     autodocs: "tag",
     toc: true,
@@ -26,3 +27,7 @@ const config = {
   staticDirs: ["./public", "../elements/storytelling/stories/public/"],
 };
 export default config;
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
