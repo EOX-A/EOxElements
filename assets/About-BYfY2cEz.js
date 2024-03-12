c887f349032fd787ece40fa36b63ac33da7377f9
silvester-pari <silvester.pari@eox.at>
2024-03-12 06:27:23 +0000

    Deploying to gh-pages from @ EOX-A/EOxElements@81381723f162d0d29c4ab45cb4b28ae7b6b0105e 🚀

---
+++ assets/About-BYfY2cEz.js
@@ -0,0 +1,13 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{u as r}from"./index-ZIu_CKYn.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";function o(n){const t={a:"a",h1:"h1",h2:"h2",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{id:"eoxelements-beta",children:"EOxElements [beta]"}),`
+`,`
+`,e.jsxs("a",{href:"https://github.com/EOX-A/EOxElements",target:"_blank",style:{display:"flex",alignItems:"center",margin:"20px 0"},children:[e.jsx("img",{src:"https://eox-a.github.io/EOxElements/github-mark.svg",alt:"github logo",width:"25",style:{marginRight:"6px"}}),"GitHub repository"]}),`
+`,e.jsx(t.h2,{id:"introduction",children:"Introduction"}),`
+`,e.jsx(t.p,{children:`EOxElements are a set of reusable, modular and highly configurable components for building geospatial applications.
+The goal is to reduce code duplication during development, and offer ready-made solutions for quick bootstrapping and
+increased long-term stabilty.`}),`
+`,e.jsx(t.h2,{id:"technology",children:"Technology"}),`
+`,e.jsxs(t.p,{children:["The EOxElements are a suite of custom elements (a.k.a. ",e.jsx(t.a,{href:"https://www.webcomponents.org/introduction",rel:"nofollow",children:"web components"}),`), which are framework-agnostic.
+Thus, they can be used in applications built with Vanilla JavaScript, Vue.js, React, Angular etc.`]}),`
+`,e.jsx(t.h2,{id:"current-state",children:"Current state"}),`
+`,e.jsxs(t.p,{children:[`All of the elements are currently functional, but not yet ready for production-use because of frequent breaking changes.
+Please refer to the repository's `,e.jsx(t.a,{href:"https://github.com/EOX-A/EOxElements/milestones",rel:"nofollow",children:"Milestones"})," to get a feeling of when to expect a producion-ready v1.0.0."]})]})}function c(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{c as default};
---
+++ assets/index-jYi0rGEw.js
@@ -0,0 +1,1 @@
+import{M as p,u as s}from"./index-ZIu_CKYn.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";export{p as MDXProvider,s as useMDXComponents};
