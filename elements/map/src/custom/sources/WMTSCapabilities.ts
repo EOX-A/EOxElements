016e2d04ffb87ca6cc1f83777dc507d19bee68aa
RobertOrthofer <orthofer.robert@gmail.com>
2024-03-19 16:01:01 +0100

    update types, fix inputs

--- elements/map/src/custom/sources/WMTSCapabilities.ts
+++ elements/map/src/custom/sources/WMTSCapabilities.ts
@@ -1,24 +1,28 @@
 import { TileImage } from "ol/source";
-import { RequestEncoding, optionsFromCapabilities } from "ol/source/WMTS";
+import { optionsFromCapabilities } from "ol/source/WMTS";
 import WMTSCapabilitiesFormat from "ol/format/WMTSCapabilities.js";
 import { createFromTileUrlFunctions } from "ol/tileurlfunction.js";
 import { appendParams } from "ol/uri.js";
-
-type optionalWmtsConfig = {
-  matrixSet: string; // The matrix set identifier, required if there is more than one matrix set in the layer capabilities.
-  projection: string; // The desired CRS when no matrixSet is specified. eg: "EPSG:3857". If the desired projection is not available, an error is thrown.
-  requestEncoding: RequestEncoding; // url encoding format for the layer. Default is the first tile url format found in the GetCapabilities response.
-  style: string; // The name of the style
-  format: string; // Image format for the layer. Default is the first format returned in the GetCapabilities response.
-  crossOrigin: string | null | undefined; // Cross origin. Default is `undefined`.
-  dimensions: string; // TO DO
-  version: string; // TO DO
-};
+import { AttributionLike } from "ol/source/Source";
+import { Projection, ProjectionLike } from "ol/proj";
 
 type WMTSCapabilitiesOptions = {
   url: string;
   layer: string;
-  config: optionalWmtsConfig;
+  attributions?: AttributionLike;
+  attributionsCollapsible?: boolean;
+  cacheSize?: number;
+  crossOrigin?: null|string;
+  interpolate?: boolean;
+  projection?: ProjectionLike;
+  transition?: number;
+  opaque?: boolean;
+  key?: string;
+  tilePixelRatio?: number;
+  zDirection?: number;
+  wrapX?: boolean;
+  dimensions: object;
+  version: string;
 };
 
 class WMTSCapabilities extends TileImage {
@@ -30,58 +34,19 @@ class WMTSCapabilities extends TileImage {
       attributions: options.attributions,
       cacheSize: options.cacheSize,
       opaque: options.opaque,
-      projection: options.projection,
-      state: options.state,
-      tileGrid: options.tileGrid,
       tilePixelRatio: options.tilePixelRatio,
       transition: options.transition,
-      interpolate: options.interpolate,
-      //key: options.key,
+      interpolate:
+        options.interpolate !== undefined ? options.interpolate : true,
+      key: options.key,
       attributionsCollapsible: options.attributionsCollapsible,
       zDirection: options.zDirection,
-      /*tileLoadFunction: function (imageTile, src) {
-        console.log("tileLoad");
-        console.log(src);
-        imageTile.getImage().src = src;
-      },*/
+      wrapX: options.wrapX
     });
 
-    /**
-     * @private
-     * @type {string}
-     */
-    this.version_ = options.config?.version !== undefined ? options.config?.version : "1.0.0";
-
-    /**
-     * @private
-     * @type {string}
-     */
-    this.format_ = options.config?.format !== undefined ? options.config?.format : "image/jpeg";
-
-    /**
-     * @private
-     * @type {!Object}
-     */
-    this.dimensions_ =
-      options.config?.dimensions !== undefined ? options.config?.dimensions : {};
-
-    /**
-     * @private
-     * @type {string}
-     */
+    this.version_ = options.version !== undefined ? options?.version : "1.0.0";
+    this.dimensions_ = options.dimensions !== undefined ? options.dimensions : {};
     this.layer_ = options.layer;
-
-    /**
-     * @private
-     * @type {string}
-     */
-    this.matrixSet_ = options.config?.matrixSet;
-
-    /**
-     * @private
-     * @type {string}
-     */
-    this.style_ = options.config?.style || 'default';
 
     fetch(options.url)
       .then((response) => response.text())
@@ -93,9 +58,9 @@ class WMTSCapabilities extends TileImage {
       });
   }
 
-  version_: string = "1.0.0";
-  format_: string = "image/jpeg";
-  dimensions_: object = {};
+  version_: string;
+  dimensions_: object;
+  format_: string;
   layer_: string;
   matrixSet_: string;
   style_: string;
@@ -113,14 +78,14 @@ class WMTSCapabilities extends TileImage {
       options
     );
 
-    this.projection = capabilitiesOptions.projection;
+    this.crossOrigin = capabilitiesOptions.crossOrigin;
+    this.projection = capabilitiesOptions.projection as Projection;
     this.tileGrid = capabilitiesOptions.tileGrid;
     this.requestEncoding_ = capabilitiesOptions.requestEncoding;
     this.matrixSet_ = capabilitiesOptions.matrixSet;
     this.style_ = capabilitiesOptions.style;
     this.format_ = capabilitiesOptions.format;
     this.dimensions_ = capabilitiesOptions.dimensions;
-    console.log(this.tileGrid);
     this.setUrls(capabilitiesOptions.urls);
     if (this.urls && this.urls.length > 0) {
       this.tileUrlFunction = createFromTileUrlFunctions(
