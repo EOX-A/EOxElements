d5fbeacd29dcd8f416977fffce5bac9f63692b5d
RobertOrthofer <orthofer.robert@gmail.com>
2024-03-20 16:32:28 +0100

    remove geolocation test because of https issues

--- elements/map/test/controls.cy.ts
+++ elements/map/test/controls.cy.ts
@@ -86,33 +86,4 @@ describe("webcomponent property parsing", () => {
       ).to.be.equal("AWS_NO2-VISUALISATION");
     });
   });
-
-  it("Geolocation Control", () => {
-    cy.mount(
-      html`<eox-map
-        .zoom=${0}
-        .center=${[0, 0]}
-        .layers=${[
-          {
-            type: "Tile",
-            properties: {
-              id: "customId",
-            },
-            source: {
-              type: "OSM",
-            },
-          },
-        ]}
-        .controls=${{
-          Geolocation: {},
-        }}
-      ></eox-map>`
-    ).as("eox-map");
-    cy.get("eox-map").and(async ($el) => {
-      const eoxMap = <EOxMap>$el[0];
-      expect(
-        eoxMap.map.getControls().getLength(),
-      ).to.be.equal(1);
-    });
-  });
 });
