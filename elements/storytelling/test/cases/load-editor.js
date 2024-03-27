c8616bffc0ac4459bb84ea972ff3a8c6bdbd0bf7
srijitcoder <srijitsm123@gmail.com>
2024-03-27 13:25:22 +0530

    added test

---
+++ elements/storytelling/test/cases/load-editor.js
@@ -0,0 +1,32 @@
+import { TEST_SELECTORS } from "../../src/enums";
+
+// Destructure TEST_SELECTORS object
+const { storyTelling } = TEST_SELECTORS;
+
+/**
+ * Load markdown editor
+ */
+const loadMarkdownEditorTest = () => {
+  const testText = "# Foo";
+  const editorSelector = "eox-storytelling-editor";
+  const jsonFormSelector = `${editorSelector} eox-jsonform`;
+
+  cy.mount(
+    `<eox-storytelling show-editor markdown="${testText}"></eox-storytelling>`
+  ).as(storyTelling);
+
+  cy.get(storyTelling)
+    .shadow()
+    .within(() => {
+      cy.get(editorSelector).should("exist");
+      cy.get(jsonFormSelector).should("exist");
+      cy.get(jsonFormSelector).then(($jsonform) => {
+        const newMardown = "# Bar";
+        $jsonform[0].value = { Story: newMardown };
+        expect($jsonform[0].value.Story).to.eq(newMardown);
+      });
+      cy.get("h1").should("have.text", "Bar");
+    });
+};
+
+export default loadMarkdownEditorTest;
