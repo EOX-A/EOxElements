ed2687ffad056b846f12dac4a0a8dd23fa835b74
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-04-02 16:37:35 +0300

    style: run prettier

--- cypress.config.ts
+++ cypress.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
     },
     fixturesFolder: "./elements", // changes fixture folder location from ./cypress/fixtures for component tests
     specPattern: specPatternComponentTests,
-    indexHtmlFile: "cypress/support/component-index.html",
-    assetsInclude: ['**/*.xml', '**/*.png'],
+    indexHtmlFile: "cypress/support/component-index.html"
   },
 });
