c3b238e15e4df845de0f0ba0ec6378e3c95c27f1
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-04-22 17:26:42 +0300

    add: dynamic individual tests for each story

--- cypress/e2e/itemfilter.cy.js
+++ cypress/e2e/itemfilter.cy.js
@@ -1,19 +1,14 @@
 import "cypress-network-idle";
-
+import stories from "../../docs/index.json";
 describe("Stories test", () => {
-  it("Should go through every story", () => {
-    cy.request("/index.json").as("stories");
-    cy.get("@stories").then((response) => {
-      console.log(response.body);
-      let count = 0;
-      Object.values(response.body.entries).forEach((element) => {
-        if (element.type && element.type == "story") {
-          count += 1;
-          cy.visit(`/iframe.html?id=${element.id}`);
-          cy.waitForNetworkIdle(1000);
-        }
+  console.log(stories);
+  Object.values(stories.entries).forEach((story) => {
+    console.log(story.id);
+    if (story.type && story.type == "story") {
+      it(`${story.id}`, () => {
+        cy.visit(`/iframe.html?id=${story.id}`);
+        cy.waitForNetworkIdle(1000);
       });
-      console.log(count);
-    });
+    }
   });
 });
