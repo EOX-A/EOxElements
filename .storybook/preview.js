d9c0ec0ce7e35113c41f3b8d463696b7c7c49a5c
Moritz Riede <moritz@mriede.com>
2024-05-08 12:17:39 +0200

    chore: add new geosearch element to storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,6 +13,7 @@ import "../elements/map/src/plugins/advancedLayersAndSources/index.ts";
 import "../elements/stacinfo/src/main.ts";
 import "../elements/storytelling/src/main.js";
 import "../elements/timecontrol/src/main.ts";
+import "../elements/geosearch/main.js";
 
 /**
  * A custom wrapper for the default setCustomElementsManifest function.
