81fde0dea05159cc2d85140de89e3e5da4ce133f
Silvester <silvester.pari@eox.at>
2024-05-08 15:53:13 +0200

    chore: reduce number of stories, init demo story (WIP) (#931)
    * chore: reduce number of stories, init demo story (WIP)
    
    * chore: add more storytelling docs text
    
    * chore: add description for as attribute
    
    * chore: comment out basic sample elements for now
    
    * chore: undo last commit

--- elements/storytelling/stories/index.js
+++ elements/storytelling/stories/index.js
@@ -3,15 +3,4 @@
 export { default as PrimaryStory } from "./primary"; // StoryTelling with markdown string
 export { default as MarkdownAsURLStory } from "./markdown-url"; // StoryTelling with markdown URL
 export { default as MarkdownSlotStory } from "./markdown-slot"; // StoryTelling with markdown from the slot
-export { default as MarkdownAttrCommentStory } from "./markdown-attr-comment"; // StoryTelling using attribute as comments in markdown
-export { default as NavigationStory } from "./navigation"; // StoryTelling with Navigation
-export { default as MarkdownBasicConfigStory } from "./markdown-basic-config"; // StoryTelling with basic config
-export { default as CustomElementStory } from "./custom-element"; // StoryTelling with custom element
-export { default as MarkdownSectionsStory } from "./markdown-sections"; // StoryTelling with sections
-export { default as MarkdownMapSectionStory } from "./markdown-map-section"; // StoryTelling with eox-map sections
-export { default as MarkdownMapTourStory } from "./markdown-map-tour"; // StoryTelling with map tour
 export { default as MarkdownEditorStory } from "./markdown-editor"; // StoryTelling with editor
-export { default as MarkdownHeroStory } from "./markdown-hero"; // StoryTelling with Hero Section
-export { default as MarkdownHeroWithNavStory } from "./markdown-hero-with-nav"; // StoryTelling with Hero Section with Nav
-export { default as MarkdownFallbackModeStory } from "./markdown-fallback"; // StoryTelling with hidden fallback image
-export { default as MarkdownLightBoxStory } from "./markdown-lightbox"; // StoryTelling with lightbox
