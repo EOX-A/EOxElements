de21667860d85e944087819cfad96d4b7984ceba
Moritz Riede <moritz@mriede.com>
2024-05-13 11:39:55 +0200

    feat: working state of button mode and directions

--- elements/geosearch/main.js
+++ elements/geosearch/main.js
@@ -1,12 +1,20 @@
 import { LitElement, html, css } from 'lit';
 
+import { button } from "../../utils/styles/button";
+
 class EOxGeoSearch extends LitElement {
     static get properties() {
         return {
-          _data: {type: Array},
+          _data: {attribute: false},
           _isListVisible: {attribute: false},
-          _searchString: {type: String},
+          _isInputVisible: {attribute: false},
+          _inputValue: {attribute: false},
           onSelect: {type: Function},
+          // Enable button mode? as in <eox-geosearch button></eox-geosearch>
+          button: {type: Boolean},
+          label: {type: String},
+          // The position of the input field, as 'left', 'top', 'right' or 'bottom'.
+          direction: {type: String},
         };
       }
 
@@ -14,18 +22,31 @@ class EOxGeoSearch extends LitElement {
         super();
 
         this._data = [];
+        // Whether the result list is visible or not
         this._isListVisible = false;
-        this._searchString = '';
+        // Whether the input is visible or not
+        this._isInputVisible = false;
+        this._inputValue = '';
     }
 
     static styles = css`
+        .hidden {
+            display: none;
+        }
+
+        .geosearch {
+            display: flex;
+            flex-direction: row;
+            align-items: start;
+        }
+
         .search-container {
             display: flex;
             flex-direction: column;
             align-items: start;
         }
-        .hidden {
-            display: none;
+        .search-container.hidden {
+            opacity: 0;
         }
         .results-container {
             min-height: 100px;
@@ -50,7 +71,6 @@ class EOxGeoSearch extends LitElement {
         }
     `;
 
-
     // TODO: Find a solution to avoid storing key in the code
     async performSearch (q) {
         let url = `/opencage-mock-data.json`;
@@ -60,24 +80,29 @@ class EOxGeoSearch extends LitElement {
         this._data = json.results;
     } 
 
-    emit(_searchString) {
+    emit(_inputValue) {
         let event = new CustomEvent('input', {
             bubbles: true,
             cancelable: true,
-            value: _searchString,
+            value: _inputValue,
         });
 
         this.dispatchEvent(event);
     }
 
     onInput(e) {
-        this._searchString = e.target.value;
-        if (this._searchString.length > 1) {
-            this.performSearch(this._searchString);
+        this._inputValue = e.target.value;
+        if (this._inputValue.length > 1) {
+            this.performSearch(this._inputValue);
             this._isListVisible = true;
         } else {
             this._isListVisible = false;
         }
+    }
+
+    onButtonClick() {
+        this._isInputVisible = !this._isInputVisible;
+        console.log(`switched input visibility ${this._isInputVisible ? 'on' : 'off'}`);
     }
 
     handleSelect(item) {
@@ -87,15 +112,57 @@ class EOxGeoSearch extends LitElement {
 
     render() {
         return html`
-            <div class="search-container">
-                <input id="gazetteer" type="text" placeholder="Type to search" .value="${this._searchString}" @input="${this.onInput}">
-                <div class="results-container ${this._isListVisible ? '' : 'hidden'}">
-                    ${this._data.map(item => html`
-                        <eox-geosearch-item
-                            .item="${item}"
-                            .onClick="${this.handleSelect}"
-                        />
-                    `)}
+            <style>
+                ${button}
+            </style>
+            <div class="geosearch" style="flex-direction: ${
+                this.direction === 'top' ? 'column-reverse' :
+                this.direction === 'left' ? 'row-reverse' :
+                this.direction === 'bottom' ? 'column' :
+                'row'
+            }">
+                <button
+                    class="${this.button ? '' : 'hidden'}"
+                    style="
+                        margin-${this.direction ?? 'right'}: 12px;
+                        background: ${this._isInputVisible ? '#0078CE' : '#004170'};
+                    "
+                    @click="${this.onButtonClick}"
+                >
+                    ${this.label ?? 'Search'}
+                </button>
+                <div
+                    class="search-container ${
+                        this.button
+                            ? this._isInputVisible ? '' : 'hidden'
+                            : ''
+                    }"
+                    style="flex-direction: ${
+                        this.direction === 'top'
+                            ? 'column-reverse'
+                            : 'column'
+                    }"
+                >
+                    <input
+                        id="gazetteer"
+                        type="text"
+                        placeholder="Type to search"
+                        .value="${this._inputValue}"
+                        style="margin-top: ${
+                            this.direction === 'top'
+                                ? 12
+                                : 0
+                        }px"
+                        @input="${this.onInput}"
+                    >
+                    <div class="results-container ${this._isListVisible ? '' : 'hidden'}">
+                        ${this._data.map(item => html`
+                            <eox-geosearch-item
+                                .item="${item}"
+                                .onClick="${this.handleSelect}"
+                            />
+                        `)}
+                    </div>
                 </div>
             </div>
         `;
