fc52954ade6f9cf46eb3434c082e2c1d9967fdec
Moritz Riede <moritz@mriede.com>
2024-05-13 11:38:41 +0200

    chore: add stories for button mode and directions

--- elements/geosearch/geosearch.stories.js
+++ elements/geosearch/geosearch.stories.js
@@ -15,3 +15,43 @@ export default {
 export const Primary = {
   render: () => html`<eox-geosearch></eox-geosearch>`,
 };
+
+export const ButtonMode = {
+  render: () => html`
+    <eox-geosearch
+      label="Search"
+      button
+    ></eox-geosearch>
+  `,
+};
+
+export const CustomDirection = {
+  render: () => html`
+    <h1>Open to left</h1>
+    <eox-geosearch
+      label="Geosearch"
+      direction="left"
+      button
+    ></eox-geosearch>
+
+    <h1>Open to top</h1>
+    <eox-geosearch
+      label="Geosearch"
+      direction="top"
+      button
+    ></eox-geosearch>
+
+    <h1>Open to right (default)</h1>
+    <eox-geosearch
+      label="Geosearch"
+      button
+    ></eox-geosearch>
+
+    <h1>Open to bottom</h1>
+    <eox-geosearch
+      label="Geosearch"
+      direction="bottom"
+      button
+    ></eox-geosearch>
+  `,
+};
