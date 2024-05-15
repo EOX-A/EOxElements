95e9e008323859c2dfae4a1a795c999d7d71821b
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-05-15 17:11:37 +0300

    fix: handle case when cypress folder is changed in git diff

--- cypress/e2e/storybook.cy.js
+++ cypress/e2e/storybook.cy.js
@@ -1,30 +1,31 @@
 import "cypress-network-idle";
 import stories from "../../docs/index.json";
 
-// const pathsChanged = `elements/drawtools/src/components/controller.js
-// elements/chart/src/main.ts
-// elements/jsonform/stories/public/basicSchema.json`;
-const pathsChanged = Cypress.env("CI_PATHS_CHANGED");
-let uniqueElementFolders = [];
 describe("Stories test", () => {
+  const pathsChanged = Cypress.env("CI_PATHS_CHANGED");
+  let uniqueElementFolders = [];
+  let cypressChanged = false;
   // only run stories from files that have been changed
   if (pathsChanged) {
     const changed = pathsChanged.split("\n");
-    console.log(changed);
+    // if cypress folder changed => run all tests
     if (!changed.some((item) => item.startsWith("cypress"))) {
-      console.log("no cypress folder");
       const filteredElementsFolders = changed
         .filter((filePath) => filePath.startsWith("elements/"))
         .map((filePath) => filePath.split("/")[1]);
       uniqueElementFolders = [...new Set(filteredElementsFolders)];
       console.log(uniqueElementFolders);
+    } else {
+      cypressChanged = true;
     }
   }
-  const filteredStories = Object.values(stories.entries).filter((obj) => {
-    return uniqueElementFolders.some((folder) =>
-      obj.importPath.includes(folder)
-    );
-  });
+  const filteredStories = cypressChanged
+    ? Object.values(stories.entries)
+    : Object.values(stories.entries).filter((obj) => {
+        return uniqueElementFolders.some((folder) =>
+          obj.importPath.includes(folder)
+        );
+      });
   filteredStories.forEach((story) => {
     if (story.type && story.type == "story") {
       it(`${story.id}`, () => {
