c3d68e1e9a91cf364732d9cfb6dd1b0bc3296fa9
silvester-pari <silvester.pari@eox.at>
2024-05-15 17:15:01 +0200

    Revert "feat: improved vanilla ol map config export (#944)"
    This reverts commit ba20a5fbe41550023f002df96c8a7f919527b597.

--- elements/map/main.ts
+++ elements/map/main.ts
@@ -314,7 +314,7 @@ export class EOxMap extends LitElement {
   extractLayerConfig = (layerArray: Array<Layer>) => {
     const layers: Array<EoxLayer> = [];
     layerArray.map((l) => {
-      if (["Group", "_LayerGroup"].includes(l.constructor.name)) {
+      if (l.constructor.name.includes("LayerGroup")) {
         layers.push({
           type: "Group",
           properties: {
@@ -325,44 +325,17 @@ export class EOxMap extends LitElement {
       } else if (l.constructor.name.includes("STACLayer")) {
         layers.push(l.get("_jsonDefinition"));
       } else {
-        const layerConfig: any = {
+        layers.push({
           type: <EoxLayer["type"]>l.constructor.name.replace("Layer", ""),
           properties: {
             id: l.get("id") ? l.get("id") : getUid(l),
           },
-        };
-        // Evaluate what other information we need to extract for different source types
-        const olsource: any = l.getSource();
-        // only export visible layers
-        if (olsource && l.isVisible()) {
-          // Extract source config
-          const source: any = {
+          source: {
             type: <sourceType>(
               l.getSource().constructor.name.replace("Source", "")
             ),
-          };
-          if (["XYZ", "TileWMS", "WMS"].includes(olsource.constructor.name)) {
-            if ("url" in olsource) {
-              source.url = olsource.url;
-            } else if ("urls" in olsource) {
-              source.urls = olsource.urls;
-            }
-          } else if (olsource.constructor.name === "VectorSource") {
-            source.url = olsource.getUrl();
-            source.format = olsource.getFormat()?.constructor.name;
-          }
-          // Extract possible other configuration options
-          if (["TileWMS", "WMS"].includes(olsource.constructor.name)) {
-            source.params = olsource.getParams();
-            source.serverType = olsource.serverType_;
-          }
-          if (olsource.constructor.name === "VectorSource") {
-            // TODO: the getStyle function does not return the applied style as described in OL docs
-            layerConfig.style = ""; // l.getStyle();
-          }
-          layerConfig.source = source;
-          layers.push(layerConfig);
-        }
+          },
+        });
       }
     });
     return layers;
@@ -390,7 +363,7 @@ export class EOxMap extends LitElement {
         layers,
         view,
         controls,
-        preventScroll: true,
+        preventScroll: false,
       };
     }
   }
