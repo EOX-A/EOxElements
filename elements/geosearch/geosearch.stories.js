f87c0035d9078c1229e945d3043d963c3a7a1e24
Moritz Riede <moritz@mriede.com>
2024-05-16 15:28:18 +0200

    feat: switch to using zoomExtent, run linter

--- elements/geosearch/geosearch.stories.js
+++ elements/geosearch/geosearch.stories.js
@@ -1,10 +1,10 @@
-import { EOxGeoSearch } from './main';
-import { html } from 'lit';
+import { EOxGeoSearch } from "./main";
+import { html } from "lit";
 
 export default {
-    title: "Elements/eox-geosearch",
-    tags: ["autodocs"],
-    component: "eox-geosearch",
+  title: "Elements/eox-geosearch",
+  tags: ["autodocs"],
+  component: "eox-geosearch",
 };
 
 /*
@@ -17,50 +17,31 @@ export const Primary = {
 };
 
 export const ButtonMode = {
-  render: () => html`
-    <eox-geosearch
-      label="Search"
-      button
-    ></eox-geosearch>
-  `,
+  render: () => html` <eox-geosearch label="Search" button></eox-geosearch> `,
 };
 
 export const CustomDirection = {
   render: () => html`
     <h1>Open to left</h1>
-    <eox-geosearch
-      label="Geosearch"
-      direction="left"
-      button
-    ></eox-geosearch>
+    <eox-geosearch label="Geosearch" direction="left" button></eox-geosearch>
 
     <h1>Open to top</h1>
-    <eox-geosearch
-      label="Geosearch"
-      direction="top"
-      button
-    ></eox-geosearch>
+    <eox-geosearch label="Geosearch" direction="top" button></eox-geosearch>
 
     <h1>Open to right (default)</h1>
-    <eox-geosearch
-      label="Geosearch"
-      button
-    ></eox-geosearch>
+    <eox-geosearch label="Geosearch" button></eox-geosearch>
 
     <h1>Open to bottom</h1>
-    <eox-geosearch
-      label="Geosearch"
-      direction="bottom"
-      button
-    ></eox-geosearch>
+    <eox-geosearch label="Geosearch" direction="bottom" button></eox-geosearch>
   `,
 };
 
 export const MapSearch = {
   args: {
     center: [15.0, 48.0],
-    endpoint: 'https://api.opencagedata.com/geocode/v1/json?=&q={{query}}&key={{key}}&limit={{limit}}',
-    key: '',
+    endpoint:
+      "https://api.opencagedata.com/geocode/v1/json?=&q={{query}}&key={{key}}&limit={{limit}}",
+    key: "",
   },
   render: (args) => {
     return html`
@@ -71,18 +52,30 @@ export const MapSearch = {
         small
         direction="left"
         limit="8"
-        .onSelect="${(item) => document.querySelector("eox-map#geosearch-map").center = [item.geometry.lng, item.geometry.lat]}"
+        .onSelect="${(item) => {
+          console.log(item.zoomExtent);
+          document.querySelector("eox-map#geosearch-map").zoomExtent =
+            item.zoomExtent;
+        }}"
         .endpoint="${args.endpoint}"
         .key="${args.key}"
       ></eox-geosearch>
 
       <eox-map
         id="geosearch-map"
-        .center='${args.center}'
-        .layers='${[{"type":"Tile","source":{"type":"OSM"}}]}'
-        .zoom='${7}'
-        style="width: 100%; height: 500px;">
+        .animationOptions=${{
+          duration: 400,
+          padding: [10, 10, 10, 10],
+        }}
+        .zoomExtent=${[
+          -8172569.397164129, -7410537.976763416, -5969880.614083453,
+          -2491403.9138794523,
+        ]}
+        .layers="${[{ type: "Tile", source: { type: "OSM" } }]}"
+        .zoom="${0}"
+        style="width: 100%; height: 500px;"
+      >
       </eox-map>
-    `
+    `;
   },
 };
