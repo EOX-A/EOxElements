cd65f06963273cdbe7c916f584d0f2fe8b692298
Moritz Riede <moritz@mriede.com>
2024-05-17 01:57:30 +0200

    feat: add some proper stories to showcase component in map environment

--- elements/geosearch/geosearch.stories.js
+++ elements/geosearch/geosearch.stories.js
@@ -12,31 +12,7 @@ export default {
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
-export const Primary = {
-  render: () => html`<eox-geosearch></eox-geosearch>`,
-};
-
-export const ButtonMode = {
-  render: () => html` <eox-geosearch label="Search" button></eox-geosearch> `,
-};
-
-export const CustomDirection = {
-  render: () => html`
-    <h1>Open to left</h1>
-    <eox-geosearch label="Geosearch" direction="left" button></eox-geosearch>
-
-    <h1>Open to top</h1>
-    <eox-geosearch label="Geosearch" direction="top" button></eox-geosearch>
-
-    <h1>Open to right (default)</h1>
-    <eox-geosearch label="Geosearch" button></eox-geosearch>
-
-    <h1>Open to bottom</h1>
-    <eox-geosearch label="Geosearch" direction="bottom" button></eox-geosearch>
-  `,
-};
-
-export const MapSearch = {
+ export const Primary = {
   args: {
     center: [15.0, 48.0],
     endpoint:
@@ -47,14 +23,12 @@ export const MapSearch = {
     return html`
       <eox-geosearch
         label="Search"
-        style="position: absolute; top: 36px; right: 24px; z-index: 12;"
-        button
-        small
+        style="position: absolute; top: 36px; right: 32px; z-index: 12;"
         direction="left"
         limit="8"
         .onSelect="${(item) => {
           console.log(item.zoomExtent);
-          document.querySelector("eox-map#geosearch-map").zoomExtent =
+          document.querySelector("eox-map#geosearch-map-primary").zoomExtent =
             item.zoomExtent;
         }}"
         .endpoint="${args.endpoint}"
@@ -62,7 +36,7 @@ export const MapSearch = {
       ></eox-geosearch>
 
       <eox-map
-        id="geosearch-map"
+        id="geosearch-map-primary"
         .animationOptions=${{
           duration: 400,
           padding: [10, 10, 10, 10],
@@ -76,6 +50,154 @@ export const MapSearch = {
         style="width: 100%; height: 500px;"
       >
       </eox-map>
+
+      <p>
+        Set both the <code>endpoint</code> and <code>key</code> attributes to switch to actual real-time API access.
+      </p>
+    `;
+  },
+};
+
+export const ButtonMode = {
+  args: {
+    center: [15.0, 48.0],
+    endpoint:
+      "https://api.opencagedata.com/geocode/v1/json?=&q={{query}}&key={{key}}&limit={{limit}}",
+    key: "",
+  },
+  render: (args) => {
+    return html`
+      <eox-geosearch
+        label="Search"
+        style="position: absolute; top: 36px; right: 32px; z-index: 12;"
+        small
+        button
+        direction="left"
+        limit="8"
+        .onSelect="${(item) => {
+          console.log(item.zoomExtent);
+          document.querySelector("eox-map#geosearch-map-primary").zoomExtent =
+            item.zoomExtent;
+        }}"
+        .endpoint="${args.endpoint}"
+        .key="${args.key}"
+      ></eox-geosearch>
+
+      <eox-map
+        id="geosearch-map-primary"
+        .animationOptions=${{
+          duration: 400,
+          padding: [10, 10, 10, 10],
+        }}
+        .zoomExtent=${[
+          -8172569.397164129, -7410537.976763416, -5969880.614083453,
+          -2491403.9138794523,
+        ]}
+        .layers="${[{ type: "Tile", source: { type: "OSM" } }]}"
+        .zoom="${0}"
+        style="width: 100%; height: 500px;"
+      >
+      </eox-map>
+
+      <p>
+        Set both the <code>endpoint</code> and <code>key</code> attributes to switch to actual real-time API access.
+      </p>
+    `;
+  },
+};
+
+export const CustomAlignments = {
+  args: {
+    center: [15.0, 48.0],
+    endpoint:
+      "https://api.opencagedata.com/geocode/v1/json?=&q={{query}}&key={{key}}&limit={{limit}}",
+    key: "",
+  },
+  render: (args) => {
+    return html`
+      <eox-geosearch
+        label="Search"
+        style="position: absolute; top: 36px; right: 32px; z-index: 12;"
+        small
+        button
+        direction="left"
+        limit="8"
+        .onSelect="${(item) => {
+          console.log(item.zoomExtent);
+          document.querySelector("eox-map#geosearch-map-primary").zoomExtent =
+            item.zoomExtent;
+        }}"
+        .endpoint="${args.endpoint}"
+        .key="${args.key}"
+      ></eox-geosearch>
+
+      <eox-geosearch
+        label="Search"
+        style="position: absolute; top: 36px; left: 32px; z-index: 12;"
+        small
+        button
+        direction="right"
+        limit="8"
+        .onSelect="${(item) => {
+          console.log(item.zoomExtent);
+          document.querySelector("eox-map#geosearch-map-primary").zoomExtent =
+            item.zoomExtent;
+        }}"
+        .endpoint="${args.endpoint}"
+        .key="${args.key}"
+      ></eox-geosearch>
+
+      <eox-geosearch
+        label="Search"
+        style="position: absolute; bottom: 96px; right: 32px; z-index: 12;"
+        small
+        button
+        direction="top"
+        limit="8"
+        .onSelect="${(item) => {
+          console.log(item.zoomExtent);
+          document.querySelector("eox-map#geosearch-map-primary").zoomExtent =
+            item.zoomExtent;
+        }}"
+        .endpoint="${args.endpoint}"
+        .key="${args.key}"
+      ></eox-geosearch>
+
+      <eox-geosearch
+        label="Search"
+        style="position: absolute; bottom: 96px; left: 32px; z-index: 12;"
+        small
+        button
+        direction="right"
+        limit="8"
+        .onSelect="${(item) => {
+          console.log(item.zoomExtent);
+          document.querySelector("eox-map#geosearch-map-primary").zoomExtent =
+            item.zoomExtent;
+        }}"
+        .endpoint="${args.endpoint}"
+        .key="${args.key}"
+      ></eox-geosearch>
+
+      <eox-map
+        id="geosearch-map-primary"
+        .animationOptions=${{
+          duration: 400,
+          padding: [10, 10, 10, 10],
+        }}
+        .zoomExtent=${[
+          -8172569.397164129, -7410537.976763416, -5969880.614083453,
+          -2491403.9138794523,
+        ]}
+        .layers="${[{ type: "Tile", source: { type: "OSM" } }]}"
+        .zoom="${0}"
+        style="width: 100%; height: 700px;"
+      >
+      </eox-map>
+
+      <p>
+        Set both the <code>endpoint</code> and <code>key</code> attributes to switch to actual real-time API access.
+      </p>
     `;
   },
 };
