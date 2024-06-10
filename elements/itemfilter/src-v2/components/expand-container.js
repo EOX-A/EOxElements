af5c13c3e71be038dc860f4de1a86a375a2cee97
srijitcoder <srijitsm123@gmail.com>
2024-06-10 17:17:27 +0530

    added comment to extand container

--- elements/itemfilter/src-v2/components/expand-container.js
+++ elements/itemfilter/src-v2/components/expand-container.js
@@ -2,7 +2,17 @@ import { LitElement, html, nothing } from "lit";
 import { styleEOX } from "../style.eox";
 import { when } from "lit/directives/when.js";
 
+/**
+ * EOxItemFilterExpandContainer is a custom web component that provides an expandable container for item filters.
+ * The component supports a featured view and an expandable details view.
+ *
+ * @module EOxItemFilterExpandContainer
+ * @extends LitElement
+ * @property {Object} filterObject - The filter object containing state and properties such as title and expanded state.
+ * @property {Boolean} unstyled - Flag to determine if default styles should be applied.
+ */
 export class EOxItemFilterExpandContainer extends LitElement {
+  // Define properties with defaults and types
   static get properties() {
     return {
       filterObject: { attribute: false, type: Object },
@@ -12,10 +22,25 @@ export class EOxItemFilterExpandContainer extends LitElement {
 
   constructor() {
     super();
+
+    /**
+     * @type Object
+     */
     this.filterObject = {};
+
+    /**
+     * @type Boolean
+     */
     this.unstyled = false;
   }
 
+  /**
+   * Handles the toggle event for the details element.
+   * Dispatches a custom event "details-toggled" with the event details.
+   *
+   * @param {Event} event - The toggle event.
+   * @private
+   */
   #handleDetailsToggle(event) {
     this.dispatchEvent(
       new CustomEvent("details-toggled", {
@@ -26,6 +51,9 @@ export class EOxItemFilterExpandContainer extends LitElement {
     );
   }
 
+  /**
+   * Renders the HTML template for the component.
+   */
   render() {
     return html`
       <style>
