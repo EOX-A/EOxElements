362ee2fb73fd25482ede8f89c5cb903a0587eff1
RobertOrthofer <orthofer.robert@gmail.com>
2024-06-21 17:11:16 +0200

    add test for wmts tile grid

---
+++ elements/map/test/wmts.cy.ts
@@ -0,0 +1,42 @@
+import { html } from "lit";
+import "../main";
+
+describe("layers", () => {
+  it("create wmts with tile grid", () => {
+    cy.intercept("*Request=GetTile*", {
+      fixture: "./map/test/fixtures/tiles/wms/eox_cloudless.jpeg",
+    });
+
+    const layer = {
+      type: "Tile",
+      properties: {
+        id: "customId",
+      },
+      source: {
+        type: "WMTS",
+        url: "https://tiles.maps.eox.at/wmts",
+        layer: "s2cloudless-2017_3857",
+        style: "default",
+        matrixSet: "GoogleMapsCompatible",
+        tileGrid: {
+          tileSize: [128, 128],
+        },
+      },
+    };
+
+    cy.mount(html`<eox-map .layers=${[layer]}></eox-map>`).as("eox-map");
+    cy.get("eox-map").and(($el) => {
+      const layer = (<EOxMap>$el[0]).map
+        .getLayers()
+        .getArray()[0] as import("../src/generate").AnyLayerWithSource;
+      expect(layer).to.exist;
+      expect(layer.get("id")).to.be.equal("customId");
+
+      const source = layer.getSource() as import("ol/source/WMTS").default;
+      expect(
+        source.getTileGrid().getTileSize(0),
+        "use tileGrid options"
+      ).to.be.deep.equal([128, 128]);
+    });
+  });
+});
