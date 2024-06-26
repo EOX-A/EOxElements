0976797b10c1d32aa922258bf8570251a47d1750
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-06-26 15:46:48 +0300

    build: update output structure to reflect src structure

--- elements/drawtools/vite.config.js
+++ elements/drawtools/vite.config.js
@@ -1,18 +1,29 @@
 import { defineConfig } from "vite";
+import { globSync } from "glob";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: "esnext",
     minify: false,
-    terserOptions: {
-      mangle: false,
-      keep_classnames: true,
-      keep_fnames: true,
-    },
     rollupOptions: {
+      input: Object.fromEntries(
+        globSync("src/**/*.js").map((file) => [
+          path.relative(
+            "src",
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
       output: {
         compact: false,
+        format: "es",
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
       },
       treeshake: false,
     },
@@ -21,6 +32,7 @@ export default defineConfig({
       name: "eox-drawtools",
       // the proper extensions will be added
       fileName: "eox-drawtools",
+      formats: ["es"],
     },
   },
 });
