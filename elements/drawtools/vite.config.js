6fa9ecfaa7c8ef4fb2e9209b2e99687632609a57
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-06-26 14:13:10 +0300

    build: remove minify and optimization for drawtools and geosearch

--- elements/drawtools/vite.config.js
+++ elements/drawtools/vite.config.js
@@ -3,6 +3,19 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "esnext",
+    minify: false,
+    terserOptions: {
+      mangle: false,
+      keep_classnames: true,
+      keep_fnames: true,
+    },
+    rollupOptions: {
+      output: {
+        compact: false,
+      },
+      treeshake: false,
+    },
     lib: {
       entry: "./src/main.js",
       name: "eox-drawtools",
