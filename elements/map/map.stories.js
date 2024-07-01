a421ffaaf3e514dd47be6e6d4640a626506b99c0
RobertOrthofer <orthofer.robert@gmail.com>
2024-07-01 10:18:04 +0200

    add a tilegrid story

--- elements/map/map.stories.js
+++ elements/map/map.stories.js
@@ -121,6 +121,35 @@ export const WMTSCapabilitiesLayer = {
   },
 };
 
+/**
+ * `WMTS` data can also be accessed directly without the need of fetching the capabilities.
+ * A TileGrid can be defined via the `tileGrid`-property of the Source
+ */
+export const WMTSTileGrid = {
+  args: {
+    center: [20, 40],
+    layers: [
+      {
+        type: "Tile",
+        properties: {
+          id: "customId",
+        },
+        source: {
+          type: "WMTS",
+          url: "https://tiles.maps.eox.at/wmts",
+          layer: "s2cloudless-2017_3857",
+          style: "default",
+          matrixSet: "GoogleMapsCompatible",
+          tileGrid: {
+            tileSize: [128, 128],
+          },
+        },
+      },
+    ],
+    zoom: 5,
+  },
+};
+
 export const STACLayer = {
   args: {
     center: [-122.38, 46.1],
