be75396f9cf90f31a4a872794c02789a598b1086
Robert Orthofer <orthofer.robert@gmail.com>
2024-07-04 10:05:39 +0200

    Fix: unmanaged Styles (#1078)
    * adapt layergroup change listener to only setActive
    
    * cleanly remove interactions when layer is removed for good
    
    * allow number IDs
    
    * add test for swapping selection layer

--- elements/map/main.ts
+++ elements/map/main.ts
@@ -213,6 +213,16 @@ export class EOxMap extends LitElement {
           )
         ) {
           const layerToBeRemoved = getLayerById(this, l.properties?.id);
+          const jsonDefinition = layerToBeRemoved.get("_jsonDefinition");
+          jsonDefinition.interactions?.forEach(
+            (interaction: import("./src/generate").EOxInteraction) => {
+              if (interaction.type === "select") {
+                this.removeSelect(interaction.options.id);
+              } else {
+                this.removeInteraction(interaction.options.id);
+              }
+            }
+          );
           this.map.removeLayer(layerToBeRemoved);
         }
       });
@@ -488,7 +498,7 @@ export class EOxMap extends LitElement {
    * Removes a given EOxSelectInteraction from the map.
    * @param id id of the interaction
    */
-  removeSelect = (id: string) => {
+  removeSelect = (id: string | number) => {
     this.selectInteractions[id].remove();
     delete this.selectInteractions[id];
   };
