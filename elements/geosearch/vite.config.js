32618a460d287ebaf3042b2fb5d4486da2cc1096
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-07-08 15:55:37 +0300

    fix: revert geosearch vite config

--- elements/geosearch/vite.config.js
+++ elements/geosearch/vite.config.js
@@ -1,38 +1,13 @@
 import { defineConfig } from "vite";
-import { globSync } from "glob";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: "esnext",
-    minify: false,
-    rollupOptions: {
-      input: Object.fromEntries(
-        globSync("src/**/*.js").map((file) => [
-          path.relative(
-            "src",
-            file.slice(0, file.length - path.extname(file).length)
-          ),
-          fileURLToPath(new URL(file, import.meta.url)),
-        ])
-      ),
-      output: {
-        compact: false,
-        format: "es",
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-      treeshake: false,
-    },
     lib: {
       entry: "./src/main.js",
       name: "eox-geosearch",
       // the proper extensions will be added
       fileName: "eox-geosearch",
-      formats: ["es"],
     },
   },
 });
