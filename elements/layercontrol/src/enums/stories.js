53dcb2adfac9664bc4ce6ef908238ff91900b8ef
A-Behairi <behairiahmed@gmail.com>
2024-07-09 14:44:58 +0200

    chore: format

--- elements/layercontrol/src/enums/stories.js
+++ elements/layercontrol/src/enums/stories.js
@@ -301,86 +301,102 @@ export const STORIES_LAYER_CROPOMHUSC2 = {
   },
 };
 
-
-
 const LAYERCONFIG_LAYER_SEE = {
   type: "style",
-  "schema": {
-    "type": "object",
-    "title": "Data configuration",
-    "properties": {
-      "settlementDistance": {
-        "type": "number",
-        "minimum": 0,
-        "maximum": 5000,
-        "format": "range",
-        "default": 0
+  schema: {
+    type: "object",
+    title: "Data configuration",
+    properties: {
+      settlementDistance: {
+        type: "number",
+        minimum: 0,
+        maximum: 5000,
+        format: "range",
+        default: 0,
       },
-      "vminmax": {
-        "title": "Global horizontal irradiation",
-        "description": "[kWh/m²/day]",
-        "type": "object",
-        "properties": {
-          "vmin": {
-            "type": "number",
-            "minimum": 0,
-            "maximum": 5,
-            "format": "range",
-            "default": 2
+      vminmax: {
+        title: "Global horizontal irradiation",
+        description: "[kWh/m²/day]",
+        type: "object",
+        properties: {
+          vmin: {
+            type: "number",
+            minimum: 0,
+            maximum: 5,
+            format: "range",
+            default: 2,
           },
-          "vmax": {
-            "type": "number",
-            "minimum": 0,
-            "maximum": 5,
-            "format": "range",
-            "default": 5
-          }
+          vmax: {
+            type: "number",
+            minimum: 0,
+            maximum: 5,
+            format: "range",
+            default: 5,
+          },
         },
-        "format": "minmax"
-      }
-    }
-  }
-}
+        format: "minmax",
+      },
+    },
+  },
+};
 
 const STYLES_LAYER_SEE = {
-  "variables": {
-    "vmin": 2,
-    "vmax": 5,
-    "settlementDistance": 0
+  variables: {
+    vmin: 2,
+    vmax: 5,
+    settlementDistance: 0,
   },
-  "color": [
+  color: [
     "case",
     [
       "all",
       [">", ["band", 1], 1],
-      [">=", ["band", 2], ["var", "settlementDistance"]]
+      [">=", ["band", 2], ["var", "settlementDistance"]],
     ],
     [
       "interpolate",
       ["linear"],
-      ["/", ["-", ["band", 1], ["var", "vmin"]], ["-", ["var", "vmax"], ["var", "vmin"]]],
-      0, [68, 1, 84, 1],
-      0.067, [70, 23, 103, 1],
-      0.133, [71, 44, 122, 1],
-      0.2, [65, 63, 131, 1],
-      0.266, [59, 81, 139, 1],
-      0.333, [52, 97, 141, 1],
-      0.4, [44, 113, 142, 1],
-      0.467, [39, 129, 142, 1],
-      0.533, [33, 144, 141, 1],
-      0.6, [39, 173, 129, 1],
-      0.666, [66, 187, 114, 1],
-      0.733, [92, 200, 99, 1],
-      0.8, [131, 210, 75, 1],
-      0.867, [170, 220, 50, 1],
-      0.933, [212, 226, 44, 1],
-      1, [253, 231, 37, 1]
+      [
+        "/",
+        ["-", ["band", 1], ["var", "vmin"]],
+        ["-", ["var", "vmax"], ["var", "vmin"]],
+      ],
+      0,
+      [68, 1, 84, 1],
+      0.067,
+      [70, 23, 103, 1],
+      0.133,
+      [71, 44, 122, 1],
+      0.2,
+      [65, 63, 131, 1],
+      0.266,
+      [59, 81, 139, 1],
+      0.333,
+      [52, 97, 141, 1],
+      0.4,
+      [44, 113, 142, 1],
+      0.467,
+      [39, 129, 142, 1],
+      0.533,
+      [33, 144, 141, 1],
+      0.6,
+      [39, 173, 129, 1],
+      0.666,
+      [66, 187, 114, 1],
+      0.733,
+      [92, 200, 99, 1],
+      0.8,
+      [131, 210, 75, 1],
+      0.867,
+      [170, 220, 50, 1],
+      0.933,
+      [212, 226, 44, 1],
+      1,
+      [253, 231, 37, 1],
     ],
-    [
-      "color", 0, 0, 0, 0
-    ]
-  ]
-}
+    ["color", 0, 0, 0, 0],
+  ],
+};
 
 export const STORIES_LAYER_SEE = {
   type: "WebGLTile",
@@ -388,14 +404,18 @@ export const STORIES_LAYER_SEE = {
   properties: {
     id: Symbol(),
     title: "Solar Energy COG Example",
-    layerConfig: LAYERCONFIG_LAYER_SEE
+    layerConfig: LAYERCONFIG_LAYER_SEE,
   },
   source: {
     type: "GeoTIFF",
     normalize: false,
     sources: [
-      { url: 'https://eox-gtif-public.s3.eu-central-1.amazonaws.com/DHI/v2/SolarPowerPotential_Annual_COG_clipped_3857_fixed.tif' },
-      { url: 'https://eox-gtif-public.s3.eu-central-1.amazonaws.com/DHI/WSF_EucDist_Austria_3857_COG_fix.tif' },
-    ]
-  }
-}
+      {
+        url: "https://eox-gtif-public.s3.eu-central-1.amazonaws.com/DHI/v2/SolarPowerPotential_Annual_COG_clipped_3857_fixed.tif",
+      },
+      {
+        url: "https://eox-gtif-public.s3.eu-central-1.amazonaws.com/DHI/WSF_EucDist_Austria_3857_COG_fix.tif",
+      },
+    ],
+  },
+};
--- elements/layercontrol/stories/layer-config-styles.js
+++ elements/layercontrol/stories/layer-config-styles.js
@@ -9,8 +9,10 @@ import {
 // registering the projection of CROPOMHUSC2_VECTOR_CONFIG_STYLE_LAYER
 setTimeout(async () => {
   //@ts-expect-error EOX Map API
-  await document.querySelector("eox-map#config").registerProjectionFromCode(3035)
-})
+  await document
+    .querySelector("eox-map#config")
+    .registerProjectionFromCode(3035);
+});
 
 export default {
   args: {},
@@ -25,7 +27,11 @@ export default {
       .zoom=${5.5}
       id="config"
       .style=${STORIES_MAP_STYLE}
-      .layers=${[STORIES_LAYER_SEE, STORIES_LAYER_CROPOMHUSC2, STORIES_LAYER_TERRAIN_LIGHT]}
+      .layers=${[
+        STORIES_LAYER_SEE,
+        STORIES_LAYER_CROPOMHUSC2,
+        STORIES_LAYER_TERRAIN_LIGHT,
+      ]}
     >
     </eox-map>
   `,
