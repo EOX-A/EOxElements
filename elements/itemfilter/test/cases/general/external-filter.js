ea932ad530b7c47df7bcc9b56c2fdc993fe540a2
srijitcoder <srijitsm123@gmail.com>
2024-07-17 10:31:48 +0200

    updated external text with click

--- elements/itemfilter/test/cases/general/external-filter.js
+++ elements/itemfilter/test/cases/general/external-filter.js
@@ -4,13 +4,14 @@ import testItems from "../../testItems.json";
  * Tests the external filtering functionality in the eox-itemfilter component.
  */
 const externalFilterTest = () => {
-  cy.intercept("https://test.com", [{ ...testItems[5] }]);
+  cy.intercept("https://external-api-url.com", [{ ...testItems[5] }]);
 
   cy.get("eox-itemfilter").then(($el) => {
     $el[0].config = {
       ...$el[0].config,
-      externalFilter: () => {
-        return `https://test.com`;
+      externalFilter: (items, filter) => {
+        expect(filter.themes.dirty).to.be.equal($el[0].filters.themes.dirty);
+        return "https://external-api-url.com";
       },
     };
   });
@@ -18,12 +19,19 @@ const externalFilterTest = () => {
   cy.get("eox-itemfilter")
     .shadow()
     .within(() => {
-      cy.get("eox-itemfilter-results summary .title").contains(
-        testItems[5].themes[0]
-      );
-      cy.get("eox-itemfilter-results label .title").contains(
-        testItems[5].title
-      );
+      cy.get("[data-details=themes]").within(() => {
+        cy.get("eox-itemfilter-select")
+          .shadow()
+          .within(() => {
+            cy.get("[data-title=agriculture]").click();
+          });
+      });
+      cy.get("eox-itemfilter-results summary .title")
+        .first()
+        .contains(testItems[5].themes[0]);
+      cy.get("eox-itemfilter-results label .title")
+        .first()
+        .contains(testItems[5].title);
     });
 };
 
