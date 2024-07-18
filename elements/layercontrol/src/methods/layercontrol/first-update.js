be7ff491d9ab3eab0a6118ac64d8c379d74e14ce
Moritz Riede <moritz@mriede.com>
2024-07-18 12:44:47 +0200

    chore: retrieve the map or copy it directly

--- elements/layercontrol/src/methods/layercontrol/first-update.js
+++ elements/layercontrol/src/methods/layercontrol/first-update.js
@@ -6,11 +6,36 @@
  * @returns {import("@eox/map/main").EOxMap | null} The EOxMap element or null if not found.
  */
 const firstUpdatedMethod = (EOxLayerControl) => {
-  const mapSelector = EOxLayerControl.for;
+  const map = EOxLayerControl.for;
+  console.log(map);
+
+  if (map instanceof HTMLElement) {
+    // The provided `for` property is an `eox-map` element.
+    console.log('got a html element');
+    console.log(map.map);
+    EOxLayerControl.map = map.map;
+
+    return map;
+  } else {
+    // The `for` property is a string selector targeting an `eox-map` element.
+    console.log('got a selector');
+
+    // Find the element specified in EOxLayerControl.for
+    /** * @type {Element & { map: import("ol").Map }} */
+    const foundElement = document.querySelector(map);
+
+    // Check if the element exists and its map property differs from EOxLayerControl.map
+    if (foundElement && foundElement.map !== EOxLayerControl.map) {
+      // Update the EOxLayerControl's map property
+      EOxLayerControl.map = foundElement.map;
+    }
+
+    return foundElement;
+  }
 
   // Find the element specified in EOxLayerControl.for
   /** * @type {Element & { map: import("ol").Map }} */
-  const foundElement = document.querySelector(mapSelector);
+  const foundElement = document.querySelector(map);
 
   // Check if the element exists and its map property differs from EOxLayerControl.map
   if (foundElement && foundElement.map !== EOxLayerControl.map) {
