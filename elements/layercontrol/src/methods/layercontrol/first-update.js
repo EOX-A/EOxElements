ddb25d8ec72368f89501a23eb65f9721102f9bd0
Moritz Riede <moritz@mriede.com>
2024-07-18 13:04:17 +0200

    refactor: simplify element-selector duality by using silvester's new `getElement` function

--- elements/layercontrol/src/methods/layercontrol/first-update.js
+++ elements/layercontrol/src/methods/layercontrol/first-update.js
@@ -1,3 +1,5 @@
+import { getElement } from '../../../../../utils/getElement.js';
+
 /**
  * Retrieves the EOxMap element based on the provided selector in EOxLayerControl's 'for' property.
  * Updates the EOxLayerControl's map property based on the target element's map.
@@ -6,29 +8,15 @@
  * @returns {import("@eox/map/main").EOxMap | null} The EOxMap element or null if not found.
  */
 const firstUpdatedMethod = (EOxLayerControl) => {
-  const map = EOxLayerControl.for;
-  console.log(map);
+  const mapElement = getElement(EOxLayerControl.for);
 
-  if (map instanceof HTMLElement) {
-    // The provided `for` property is an `eox-map` element.
-    EOxLayerControl.map = map.map;
-
-    return map;
-  } else {
-    // The `for` property is a string selector targeting an `eox-map` element.
-
-    // Find the element specified in EOxLayerControl.for
-    /** * @type {Element & { map: import("ol").Map }} */
-    const foundElement = document.querySelector(map);
-
-    // Check if the element exists and its map property differs from EOxLayerControl.map
-    if (foundElement && foundElement.map !== EOxLayerControl.map) {
-      // Update the EOxLayerControl's map property
-      EOxLayerControl.map = foundElement.map;
-    }
-
-    return foundElement;
+  // Check if the element exists and its map property differs from EOxLayerControl.map
+  if (mapElement && mapElement.map !== EOxLayerControl.map) {
+    // Update the EOxLayerControl's map property
+    EOxLayerControl.map = mapElement.map;
   }
+
+  return mapElement.map;
 };
 
 export default firstUpdatedMethod;
