1bdf749b5428130c867dd9bcecbc0f37098616d5
RobertOrthofer <orthofer.robert@gmail.com>
2024-08-02 16:55:35 +0200

    add test for programmatic vector layer selection

--- elements/map/test/selectInteraction.cy.ts
+++ elements/map/test/selectInteraction.cy.ts
@@ -88,6 +88,52 @@ describe("select interaction on click", () => {
     });
   });
 
+  it("programmatically highlight by IDs", () => {
+    return new Cypress.Promise((resolve) => {
+      cy.intercept(
+        "https://openlayers.org/data/vector/ecoregions.json",
+        (req) => {
+          req.reply(ecoRegionsFixture);
+        }
+      );
+      const styleJson = JSON.parse(
+        JSON.stringify(vectorLayerJson)
+      ) as Array<EoxLayer>;
+      styleJson[0].interactions = [
+        {
+          type: "select",
+          options: {
+            id: "selectInteraction",
+            condition: "click",
+            style: {
+              "stroke-color": "white",
+              "stroke-width": 3,
+            },
+          } as import("../src/select").SelectOptions,
+        },
+      ];
+      cy.mount(html`<eox-map .center=${[0, 0]} .layers=${styleJson}></eox-map>`).as("eox-map");
+
+      cy.get("eox-map").and(($el) => {
+        const eoxMap = <EOxMap>$el[0];
+        eoxMap.map.on('loadend', ()=> {
+          //on loadend, programmatically select a few features...
+          eoxMap.selectInteractions.selectInteraction.highlightById([710, 717, 828], {
+            duration: 400,
+            padding: [50, 50, 50, 50]
+          });
+          // ..and expect the map to animate to them
+          setTimeout(() => {
+            const center = eoxMap.map.getView().getCenter();
+            console.log(center);
+            expect(center, 'animates to selected features').to.not.deep.equal([0, 0]);
+            resolve();
+          }, 200)
+        })
+      });
+    });
+  });
+
   it("remove interaction", () => {
     const styleJson = JSON.parse(
       JSON.stringify(vectorTileLayerJson)
