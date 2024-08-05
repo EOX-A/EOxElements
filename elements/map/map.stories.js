fb727d3fb585216656b9201602fd8de322cc460d
Robert Orthofer <orthofer.robert@gmail.com>
2024-08-05 17:35:16 +0200

    feat: loading indicator map control (#1173)
    * implement loading indicator
    
    * custom spinner icons
    
    * add loading indicator stories
    
    * remove unused svg file

--- elements/map/map.stories.js
+++ elements/map/map.stories.js
@@ -319,6 +319,64 @@ export const Geolocation = {
   },
 };
 
+/**
+ * A simple, unobtrusive loading indicator in the bottom left that appears any time when the map is loading.
+ */
+export const StandardLoadingIndicator = {
+  args: {
+    zoom: 9,
+    center: [0, 51.5],
+    controls: {
+      LoadingIndicator: {},
+      Zoom: {},
+    },
+    layers: [
+      {
+        type: "Tile",
+        properties: {
+          id: "customId",
+        },
+        source: {
+          type: "OSM",
+        },
+      },
+    ],
+  },
+};
+
+/**
+ * Loading Indicators can also be centered over the entire map by setting the option `type` to `'fullscreen'`, adapting the opacity is adviced when doing so.
+ *
+ * Custom rotating SVG-Icons can be used by setting the svg data as the `spinnerSvg`-option.
+ */
+export const CustomFullScreenLoadingIndicator = {
+  args: {
+    zoom: 9,
+    center: [0, 51.5],
+    controls: {
+      LoadingIndicator: {
+        type: "fullscreen",
+        opacity: 0.2,
+        spinnerSvg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="50" height="50" style="shape-rendering: auto; display: block; background: transparent;" xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle stroke-dasharray="120 50" r="30" stroke-width="22" stroke="#cd4609" fill="none" cy="50" cx="50">
+        <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="0.4s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+            </circle><g></g></g></svg>`,
+      },
+      Zoom: {},
+    },
+    layers: [
+      {
+        type: "Tile",
+        properties: {
+          id: "customId",
+        },
+        source: {
+          type: "OSM",
+        },
+      },
+    ],
+  },
+};
+
 export const HoverSelect = {
   args: {
     layers: [
---
+++ elements/map/src/controls/LoadingIndicator.ts
@@ -0,0 +1,83 @@
+import { Control } from "ol/control.js";
+
+type LoadingIndicatorType = "small" | "fullscreen";
+
+export type LoadingIndicatorOptions = import("ol/control/Control").Options & {
+  /**
+   * @property {string} spinnerSvg svg to be used as spinner icon
+   */
+  spinnerSvg?: string;
+  /**
+   * @property {Opacity} opacity opacity, defaults to 1
+   */
+  opacity?: number;
+  /**
+   * @property {LoadingIndicatorType} type type of appearance. Small button style of fullscreen style. Defaults to button style.
+   */
+  type?: LoadingIndicatorType;
+};
+
+export default class LoadingIndicatorControl extends Control {
+  /**
+   * @param {LoadingIndicatorOptions} [opt_options] Control options.
+   */
+  constructor(opt_options: LoadingIndicatorOptions) {
+    const options = (opt_options || {}) as LoadingIndicatorOptions;
+    if (options.opacity === undefined) {
+      options.opacity = 1;
+    }
+    if (options.type === undefined) {
+      options.type = "small";
+    }
+
+    const element = document.createElement("div");
+    element.className = "LoadingIndicator ol-unselectable ol-control";
+    element.classList.add("loading-indicator");
+    element.style.opacity = String(options.opacity);
+
+    if (options.spinnerSvg) {
+      element.innerHTML = options.spinnerSvg;
+    } else {
+      // fallback icon
+      element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" width="50" height="50" style="shape-rendering: auto; display: block; background: transparent;" xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle stroke-dasharray="164.93361431346415 56.97787143782138" r="35" stroke-width="12" stroke="#1a467c" fill="none" cy="50" cx="50">
+  <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="1.2222222222222223s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+  </circle><g></g></g></svg>`;
+    }
+    if (options.type === "fullscreen") {
+      element.classList.add("fullscreen");
+    } else {
+      element.classList.add("small");
+    }
+    super({
+      element: element,
+    });
+  }
+
+  /**
+   * Remove the control from its current map and attach it to the new map.
+   * Pass `null` to just remove the control from the current map.
+   * Subclasses may set up event handlers to get notified about changes to
+   * the map here.
+   * @param {import("ol/Map").default|null} map Map.
+   * @api
+   */
+  setMap(map: import("ol/Map.js").default | null) {
+    super.setMap(map);
+    if (map) {
+      map.on("loadstart", () => {
+        this.getElement().style.visibility = "visible";
+      });
+      map.on("loadend", () => {
+        this.getElement().style.visibility = "hidden";
+      });
+    }
+  }
+
+  /**
+   * returns the geolocation control button
+   * @returns
+   */
+  getElement() {
+    return this.element;
+  }
+}
