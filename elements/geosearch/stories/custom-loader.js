c883cfaf7fd8c221d8977fba6e789186ca6752bf
Moritz Riede <moritz@mriede.com>
2024-08-07 14:18:36 +0200

    chore: add new story for custom loader functionality

---
+++ elements/geosearch/stories/custom-loader.js
@@ -0,0 +1,105 @@
+import { html } from "lit";
+
+const CustomLoader = {
+  args: {
+    endpoint: "/opencage-mock-data.json",
+    customLoader: html`
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48px"
+        height="48px"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="12" cy="12" r="0" fill="currentColor">
+          <animate
+            id="svgSpinnersPulseMultiple0"
+            fill="freeze"
+            attributeName="r"
+            begin="0;svgSpinnersPulseMultiple2.end"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="0;11"
+          />
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="0;svgSpinnersPulseMultiple2.end"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="1;0"
+          />
+        </circle>
+        <circle cx="12" cy="12" r="0" fill="currentColor">
+          <animate
+            id="svgSpinnersPulseMultiple1"
+            fill="freeze"
+            attributeName="r"
+            begin="svgSpinnersPulseMultiple0.begin+0.2s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="0;11"
+          />
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="svgSpinnersPulseMultiple0.begin+0.2s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="1;0"
+          />
+        </circle>
+        <circle cx="12" cy="12" r="0" fill="currentColor">
+          <animate
+            id="svgSpinnersPulseMultiple2"
+            fill="freeze"
+            attributeName="r"
+            begin="svgSpinnersPulseMultiple0.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="0;11"
+          />
+          <animate
+            fill="freeze"
+            attributeName="opacity"
+            begin="svgSpinnersPulseMultiple0.begin+0.4s"
+            calcMode="spline"
+            dur="1.2s"
+            keySplines=".52,.6,.25,.99"
+            values="1;0"
+          />
+        </circle>
+      </svg>
+    `,
+  },
+  render: (args) => {
+    return html`
+      <eox-geosearch
+        label="Search"
+        style="position: absolute; top: 36px; left: 32px; z-index: 12;"
+        .endpoint="${args.endpoint}"
+        .loaderSvg="${args.customLoader}"
+      ></eox-geosearch>
+
+      <eox-map
+        id="geosearch-map-primary"
+        .animationOptions=${{
+          duration: 400,
+          padding: [10, 10, 10, 10],
+        }}
+        .config=${{
+          layers: [{ type: "Tile", source: { type: "OSM" } }],
+          view: { center: [15, 48], zoom: 3 },
+        }}
+        style="width: 100%; height: 500px;"
+      >
+      </eox-map>
+    `;
+  },
+};
+
+export default CustomLoader;
