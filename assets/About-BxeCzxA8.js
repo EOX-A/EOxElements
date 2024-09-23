a667286d87cda2fe033eab3b182463967b130dbc
silvester-pari <silvester.pari@eox.at>
2024-09-23 07:28:27 +0000

    Deploying to gh-pages from @ EOX-A/EOxElements@0dac7fd1a660ba9544804334dff8e8cf03abeafa 🚀

--- assets/About-BxeCzxA8.js
+++ assets/About-BxeCzxA8.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime-DEdD30eg.js";import{useMDXComponents as r}from"./index-CcnH5Kt0.js";import"./index-RYns6xqu.js";function o(n){const t={a:"a",h1:"h1",h2:"h2",p:"p",...r(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{id:"eoxelements-beta",children:"EOxElements [beta]"}),`
+import{j as e}from"./jsx-runtime-CjOlxkLm.js";import{useMDXComponents as s}from"./index-DhQFcGa7.js";function o(n){const t={a:"a",h1:"h1",h2:"h2",p:"p",...s(),...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{id:"eoxelements-beta",children:"EOxElements [beta]"}),`
 `,`
 `,e.jsxs("a",{href:"https://github.com/EOX-A/EOxElements",target:"_blank",style:{display:"flex",alignItems:"center",margin:"20px 0"},children:[e.jsx("img",{src:"https://eox-a.github.io/EOxElements/github-mark.svg",alt:"github logo",width:"25",style:{marginRight:"6px"}}),"GitHub repository"]}),`
 `,e.jsx(t.h2,{id:"introduction",children:"Introduction"}),`
@@ -10,4 +10,4 @@ increased long-term stabilty.`}),`
 Thus, they can be used in applications built with Vanilla JavaScript, Vue.js, React, Angular etc.`]}),`
 `,e.jsx(t.h2,{id:"current-state",children:"Current state"}),`
 `,e.jsxs(t.p,{children:[`All of the elements are currently functional, but not yet ready for production-use because of frequent breaking changes.
-Please refer to the repository's `,e.jsx(t.a,{href:"https://github.com/EOX-A/EOxElements/milestones",rel:"nofollow",children:"Milestones"})," to get a feeling of when to expect a producion-ready v1.0.0."]})]})}function l(n={}){const{wrapper:t}={...r(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{l as default};
+Please refer to the repository's `,e.jsx(t.a,{href:"https://github.com/EOX-A/EOxElements/milestones",rel:"nofollow",children:"Milestones"})," to get a feeling of when to expect a producion-ready v1.0.0."]})]})}function a(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{a as default};
