d8a97c913a1c1cedd027a1f58c3510073e1bb2c3
Viktor Farkas <fv1ktor09@gmail.com>
2024-09-30 16:36:15 +0200

    chore: formatting

--- elements/itemfilter/test/cases/general/validation.js
+++ elements/itemfilter/test/cases/general/validation.js
@@ -2,7 +2,6 @@
  * Tests the validation option of the text input.
  */
 const validationTest = () => {
-  
   cy.get("eox-itemfilter")
     .shadow()
     .within(() => {
@@ -10,7 +9,10 @@ const validationTest = () => {
         const el = $elList[0];
         cy.get(".error-validation").should("not.have.text");
         cy.get("[data-cy='search']").type("abcdefghijk");
-        cy.get(".error-validation").should("have.text", el.filterObject.validation.message);
+        cy.get(".error-validation").should(
+          "have.text",
+          el.filterObject.validation.message
+        );
         cy.get("[data-cy='search']").clear();
         cy.get(".error-validation").should("not.have.text");
       });
