732dc6aa0b496c1a2376e1dd6eca2e2ab65868cf
A-Behairi <behairiahmed@gmail.com>
2024-10-08 12:04:12 +0200

    fix: added more legend tests

--- elements/layercontrol/test/cases/general/check-layer-legend.js
+++ elements/layercontrol/test/cases/general/check-layer-legend.js
@@ -1,7 +1,19 @@
 import "color-legend-element";
 
 export const checkLayerLegend = () => {
-  const legendTitle = "legend title";
+  const legendLayer = {
+    properties: {
+      title: "Legend test",
+      layerLegend: {
+        title: "Legend title",
+        range: ["green", "red"],
+        domain: [0, 1],
+        ticks: 4,
+        tickValues: [0, 0.2, 0.4, 0.8],
+        width: 460,
+      },
+    },
+  };
 
   cy.get("eox-layercontrol").then(($el) => {
     const layerControlEl = $el[0];
@@ -12,20 +24,7 @@ export const checkLayerLegend = () => {
     // Accessing the mock map element
     const mockMap = $el[0];
     // Setting layers
-    mockMap.setLayers([
-      {
-        properties: {
-          title: "Legend test",
-          layerLegend: {
-            title: legendTitle,
-            range: ["green", "red"],
-            domain: [0, 1],
-            ticks: 3,
-            width: 460,
-          },
-        },
-      },
-    ]);
+    mockMap.setLayers([legendLayer]);
   });
 
   cy.get("eox-layercontrol")
@@ -36,9 +35,30 @@ export const checkLayerLegend = () => {
         .should("exist")
         .shadow()
         .within(() => {
+          // check if rendered legend title is equal to the configured title
           cy.get("p.legend-title").then(($el) => {
-            // check if rendered legend title is equal to the configured title
-            expect($el[0].textContent).equal(legendTitle);
+            expect($el[0].textContent).equal(
+              legendLayer.properties.layerLegend.title
+            );
+          });
+
+          // check if the image (grandient) is rendered
+          cy.get("image").should("exist");
+
+          // check if the numbers of ticks rendered are equal to the configured ticks
+          cy.get("line").then(($el) => {
+            expect($el.length).equals(legendLayer.properties.layerLegend.ticks);
+          });
+
+          // check if the rendered tick values are the same as the values configured
+          const stringTickValues =
+            legendLayer.properties.layerLegend.tickValues.map((val) =>
+              val.toFixed(1).toString()
+            );
+          cy.get("text").then(($el) => {
+            $el.each((_idx, el) => {
+              expect(stringTickValues).include(el.textContent);
+            });
           });
         });
     });
