e696031d6255c2d2dfc60db682524f97e36fafa5
silvester-pari <silvester.pari@eox.at>
2024-10-10 14:07:02 +0200

    chore: adapt typescript file pattern and fix some ts error

--- elements/chart/src/methods/render/render-chart.js
+++ elements/chart/src/methods/render/render-chart.js
@@ -5,7 +5,7 @@ import { DEFAULT_SPEC } from "../../enums";
 /**
  * Render the Vega-Lite chart using vega-embed with a spec and (optionally) dataValues
  *
- * @param {import("../../main").EOxChart} The eox-chart instance
+ * @param {import("../../main").EOxChart} EOxChart The eox-chart instance
  * @param {import("vega-embed").VisualizationSpec} spec
  * @param {Object=} dataValues
  *
@@ -14,7 +14,7 @@ const renderChartMethod = (EOxChart, spec, dataValues) => {
   const mergedSpec = deepmerge(DEFAULT_SPEC, spec);
   vegaEmbed(
     /** @type {HTMLElement} */ (EOxChart.renderRoot.querySelector("#vis")),
-    mergedSpec,
+    mergedSpec
   ).then((res) => {
     if (dataValues) {
       Object.keys(dataValues).forEach((dataSourceName) => {
