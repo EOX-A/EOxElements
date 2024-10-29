9f84de3c098ea5df798ff42adc0fe27ff3e901b6
Silvester <silvester.pari@eox.at>
2024-10-29 11:27:40 +0100

    chore: remove timeout according to fixed bug in eox-map (#1335)
    * chore: remove timeout according to fixed bug in eox-map
    
    * chore: remove second timeout
    
    * chore: add select btn test
    
    ---------
    
    Co-authored-by: A-Behairi <behairiahmed@gmail.com>

--- elements/drawtools/src/methods/draw/discard-drawing.js
+++ elements/drawtools/src/methods/draw/discard-drawing.js
@@ -9,10 +9,8 @@ const discardDrawingMethod = (EoxDrawTool) => {
   const discardDrawingActions = () => {
     // Reset drawnFeatures, deactivate drawing, and clear drawLayer's source
     EoxDrawTool.drawnFeatures = [];
-    setTimeout(() => {
-      EoxDrawTool.draw.setActive(false);
-      EoxDrawTool.selectionEvents.removeSelectionEvent();
-    }, 200);
+    EoxDrawTool.draw.setActive(false);
+    EoxDrawTool.selectionEvents.removeSelectionEvent();
     EoxDrawTool.drawLayer.getSource().clear();
     //@ts-expect-error TODO
     EoxDrawTool.geoJSON = null;
