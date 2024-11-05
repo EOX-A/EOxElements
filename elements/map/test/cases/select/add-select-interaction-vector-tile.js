d0d8fae7dc4d336dba4fdb6dd6548ac3d4ad46f8
RobertOrthofer <orthofer.robert@gmail.com>
2024-11-05 11:43:45 +0100

    fix formatting

--- elements/map/test/cases/select/add-select-interaction-vector-tile.js
+++ elements/map/test/cases/select/add-select-interaction-vector-tile.js
@@ -18,7 +18,10 @@ const addSelectInteractionVectorTile = (vectorTileInteraction) => {
     cy.get("eox-map").and(($el) => {
       const eoxMap = $el[0];
       eoxMap.addEventListener("select", (evt) => {
-        expect(eoxMap.map.getTargetElement().style.cursor, 'changes cursor to pointer').to.be.equal('pointer');
+        expect(
+          eoxMap.map.getTargetElement().style.cursor,
+          "changes cursor to pointer",
+        ).to.be.equal("pointer");
         expect(evt.detail.feature).to.exist;
         resolve();
       });
