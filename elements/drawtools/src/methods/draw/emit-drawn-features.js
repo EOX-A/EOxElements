103a0933351cbbdfe385977181284df37ee9236a
A-Behairi <behairiahmed@gmail.com>
2024-11-06 12:07:07 +0100

    feat: emitted feature projection

--- elements/drawtools/src/methods/draw/emit-drawn-features.js
+++ elements/drawtools/src/methods/draw/emit-drawn-features.js
@@ -8,7 +8,21 @@ const emitDrawnFeaturesMethod = (EoxDrawTool, drawUpdateEvent) => {
   // Function to emit features after a timeout (ensures update)
   const emitFeatures = () => {
     // Update drawnFeatures with features from drawLayer's source
-    EoxDrawTool.drawnFeatures = EoxDrawTool.drawLayer.getSource().getFeatures();
+    const drawnFeatures = EoxDrawTool.drawLayer.getSource().getFeatures();
+    // Transform drawn features to the desired projection
+    const source = EoxDrawTool.eoxMap.projection || "EPSG:3857";
+    // has a default value of EPSG:4326
+    const destination = EoxDrawTool.projection;
+    // Update drawnFeatures with transformed features if the destination is defined
+    EoxDrawTool.drawnFeatures = destination
+      ? drawnFeatures.map((feat) => {
+          feat = feat.clone();
+          const transformed = feat.getGeometry().transform(source, destination);
+          feat.setGeometry(transformed);
+          return feat;
+        })
+      : drawnFeatures;
+
     EoxDrawTool.updateGeoJSON();
     EoxDrawTool.requestUpdate();
 
