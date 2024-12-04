58ca335cbd9681769cd42398b27d5508a1df199b
Moritz Riede <moritz@mriede.com>
2024-12-04 10:59:17 +0100

    chore: remove play function

--- elements/drawtools/stories/distance-measurement.js
+++ elements/drawtools/stories/distance-measurement.js
@@ -1,5 +1,4 @@
 import { html } from "lit";
-// import { expect, userEvent, waitFor } from "@storybook/test";
 
 import { STORIES_MAP_STYLE, STORIES_LAYERS_ARRAY } from "../src/enums";
 
@@ -21,28 +20,6 @@ export const DistanceMeasurement = {
       measure
     />
   `,
-  play: async ({ _canvasElement, _step }) => {
-    console.log("hello from play function");
-    throw new Error("Simulated error!");
-    /*await waitFor(() => {
-      const itemFilterComponent =
-        canvasElement.querySelector("eox-itemfilter");
-      expect(itemFilterComponent).toBeTruthy();
-      expect(itemFilterComponent.shadowRoot).toBeTruthy();
-    });
-
-    // Get the item filter component and its shadow root
-    const itemFilterComponent = canvasElement.querySelector("eox-itemfilter");
-    const shadowRoot = itemFilterComponent.shadowRoot;
-
-    // Simulate user interaction: Searching for "Asparagus"
-    await step("Searching for Asparagus", async () => {
-      const inputElement = shadowRoot.querySelector(
-        'input[placeholder="Type Something..."]',
-      );
-      await userEvent.type(inputElement, "Asparagus", { delay: 100 });
-    });*/
-  },
 };
 
 export default DistanceMeasurement;
