0285b725744d8cd7d57b05f8787ca30c6aa997da
Daniel Santillan <daniel.santillan@eox.at>
2024-12-12 19:59:44 +0100

    feat: format handler for multiple geometry types and formats (#1276)
    * feat: added initial bounding box format handler for jsonform
    
    * feat: add polygons and editors
    
    * chore: cleanup
    
    * feat: selection input + adjusted returned values accordingly
    
    * fix: feature selection story & renamed drawtool to bbox
    
    * chore: format
    
    * fix: lint & tests
    
    * fix: spread features
    
    * feat: type spatial custom validator
    
    * chore: update collection schema
    
    * test: render drawtools on type spatial
    
    * feat: add projection option
    
    * test: projection
    
    * fix: auto start drawing
    
    * fix: remove `*-editor` formats
    
    * fix: update editors types & adapt the validator and stories
    
    * feat: support format point/points
    
    * feat: support wkt and geojson types and handle their validations
    
    * chore: update stories and clean up
    
    * test: adjust tests
    
    * fix: return a feature for single formats and feature collection for plural
    
    * feat: support line/lines format
    
    * fix: discard drawing on destroy
    
    * fix: adjust collection schema and format
    
    ---------
    
    Co-authored-by: A-Behairi <behairiahmed@gmail.com>

--- elements/jsonform/stories/index.js
+++ elements/jsonform/stories/index.js
@@ -4,3 +4,10 @@ export { default as CollectionStory } from "./collection"; // Input form based o
 export { default as ExternalStory } from "./external"; // Input form based on External URL
 export { default as MarkdownStory } from "./markdown"; // Input form based on Markdown Editor config
 export { default as UnStyledStory } from "./unstyled"; // Unstyled input form
+export { default as BoundingBoxStory } from "./bounding-box"; // Input form based on drawtools - Box
+export { default as PolygonStory } from "./polygons"; // Input form based on drawtools - Polygon
+export { default as PointStory } from "./points"; // Input form based on drawtools - Point
+export { default as FeatureSelectionStory } from "./feature-selection"; // Input form based on drawtools - Feature Selection
+export { default as LineStory } from "./line"; // Input form based on Drawtools - LineString
+export { default as WKTStory } from "./wkt"; // Input form based on Drawtools that returns WKT string
+export { default as GeoJSONStory } from "./geojson"; // Input form based on Drawtools that returns GeoJSON
---
+++ elements/jsonform/stories/polygons.js
@@ -0,0 +1,12 @@
+/**
+ * Drawtools component demonstrating the configuration options for eox-jsonform.
+ * Allows the user to draw polygons on the map as an input
+ */
+import polygonsScheme from "./public/polygonSchema.json";
+
+const Polygons = {
+  args: {
+    schema: polygonsScheme,
+  },
+};
+export default Polygons;
