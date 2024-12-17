c9348465b6c1c3cd2b8a1775bff0a69863fc696a
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2024-12-17 13:42:11 +0200

    chore: update bg colors for jsonform

--- elements/jsonform/src/style.js
+++ elements/jsonform/src/style.js
@@ -2,4 +2,14 @@ export const style = `
   * {
     font-family: Roboto, sans-serif;
   }
+  :host, :root {
+    --background-color: var(--eox-background-color, transparent);
+    background-color: var(--background-color, transparent);
+  }
+  form {
+    background-color: var(--background-color, transparent);
+  }
+  .CodeMirror {
+    background-color: var(--background-color, transparent)
+  }
 `;
