85f63b92b5701e2c1e077898d0991253839ec03b
silvester-pari <silvester.pari@eox.at>
2025-01-31 08:45:28 +0100

    feat: add pointermove event

--- elements/chart/src/main.js
+++ elements/chart/src/main.js
@@ -52,6 +52,19 @@ export class EOxChart extends LitElement {
     this.unstyled = false;
   }
 
+  _dispatchItemPointerMoveEvent(detail) {
+    /**
+     * Chart item is hovered. Event detail includes
+     * `event` (click event) and `item` (the clicked item on the chart)
+     *
+     */
+    this.dispatchEvent(
+      new CustomEvent("pointermove.item", {
+        detail,
+      }),
+    );
+  }
+
   _dispatchItemClickEvent(detail) {
     /**
      * Chart item is clicked. Event detail includes
