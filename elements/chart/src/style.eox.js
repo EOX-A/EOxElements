0fdba165472c4b75025b7b00d300411042ec3c5c
silvester-pari <silvester.pari@eox.at>
2025-03-03 21:53:41 +0100

    fix: important css override

--- elements/chart/src/style.eox.js
+++ elements/chart/src/style.eox.js
@@ -3,7 +3,7 @@ import eoxStyle from "@eox/ui/style.css?inline";
 export const styleEOX = `
   ${eoxStyle}
   .vega-embed .vega-actions a {
-    font-family: inherit;
-    font-weight: normal;
+    font-family: inherit !important;
+    font-weight: normal !important;
   }
 `;
