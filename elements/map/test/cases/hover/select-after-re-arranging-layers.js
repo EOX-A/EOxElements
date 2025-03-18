5dbc59214128a53f36b32bce87d885ca9d35b9f7
RobertOrthofer <orthofer.robert@gmail.com>
2025-03-18 10:52:51 +0100

    re-activate layer ordering

--- elements/map/test/cases/hover/select-after-re-arranging-layers.js
+++ elements/map/test/cases/hover/select-after-re-arranging-layers.js
@@ -31,38 +31,38 @@ const selectAfterReArrangingLayers = () => {
 
   let selectCounter = 0;
   let featureSelectCounter = 0;
-  const myPromise = new Promise(resolve => {
+  const featureSelectPromise = new Promise((resolve) => {
     cy.get("eox-map").and(($el) => {
       const eoxMap = $el[0];
 
-    eoxMap.addEventListener("select", (evt) => {
-      selectCounter++;
-      // @ts-expect-error TODO
-      if (evt.detail.feature) {
-        featureSelectCounter++;
-      }
-      if (selectCounter === 3) {
-        // moving the cursor to a feature, moving it off the feature, and onto the feature again
-        resolve(featureSelectCounter);
-      }
-    });
-    eoxMap.map.on("loadend", () => {
-      /*const layers = eoxMap.map.getLayers();
-      let first = layers.getArray()[0];
-      layers.removeAt(0);
-      layers.insertAt(1, first);
-      first = layers.getArray()[0];
-      layers.removeAt(0);
-      layers.insertAt(1, first);*/
-      simulateEvent(eoxMap.map, "pointermove", 120, -140); // a feature here
-      simulateEvent(eoxMap.map, "pointermove", 0, -140); // no feature here
-      simulateEvent(eoxMap.map, "pointermove", 120, -140); // a feature here
-    });
+      eoxMap.addEventListener("select", (evt) => {
+        selectCounter++;
+        // @ts-expect-error TODO
+        if (evt.detail.feature) {
+          featureSelectCounter++;
+        }
+        if (selectCounter === 3) {
+          // moving the cursor to a feature, moving it off the feature, and onto the feature again
+          resolve(featureSelectCounter);
+        }
+      });
+      eoxMap.map.on("loadend", () => {
+        const layers = eoxMap.map.getLayers();
+        let first = layers.getArray()[0];
+        layers.removeAt(0);
+        layers.insertAt(1, first);
+        first = layers.getArray()[0];
+        layers.removeAt(0);
+        layers.insertAt(1, first);
 
-    })
+        simulateEvent(eoxMap.map, "pointermove", 120, -140); // a feature here
+        simulateEvent(eoxMap.map, "pointermove", 0, -140); // no feature here
+        simulateEvent(eoxMap.map, "pointermove", 120, -140); // a feature here
+      });
+    });
   });
 
-  cy.wrap(myPromise).should('eq', 2);
+  cy.wrap(featureSelectPromise).should("eq", 2);
 };
 
 export default selectAfterReArrangingLayers;
