f588ad8450993bdb59cabcd3e19bdde0cee72887
RobertOrthofer <orthofer.robert@gmail.com>
2025-03-18 09:52:49 +0100

    properly handle async test use `cy.wrap` with `should` to yield to an async promise

--- elements/map/test/cases/hover/select-after-re-arranging-layers.js
+++ elements/map/test/cases/hover/select-after-re-arranging-layers.js
@@ -28,10 +28,13 @@ const selectAfterReArrangingLayers = () => {
       <eox-map-tooltip></eox-map-tooltip>
     </eox-map>`,
   ).as("eox-map");
-  cy.get("eox-map").and(($el) => {
-    const eoxMap = $el[0];
-    let selectCounter = 0;
-    let featureSelectCounter = 0;
+
+  let selectCounter = 0;
+  let featureSelectCounter = 0;
+  const myPromise = new Promise(resolve => {
+    cy.get("eox-map").and(($el) => {
+      const eoxMap = $el[0];
+
     eoxMap.addEventListener("select", (evt) => {
       selectCounter++;
       // @ts-expect-error TODO
@@ -40,23 +43,26 @@ const selectAfterReArrangingLayers = () => {
       }
       if (selectCounter === 3) {
         // moving the cursor to a feature, moving it off the feature, and onto the feature again
-        expect(featureSelectCounter).to.be.equal(2);
+        resolve(featureSelectCounter);
       }
     });
-
     eoxMap.map.on("loadend", () => {
-      const layers = eoxMap.map.getLayers();
+      /*const layers = eoxMap.map.getLayers();
       let first = layers.getArray()[0];
       layers.removeAt(0);
       layers.insertAt(1, first);
       first = layers.getArray()[0];
       layers.removeAt(0);
-      layers.insertAt(1, first);
+      layers.insertAt(1, first);*/
       simulateEvent(eoxMap.map, "pointermove", 120, -140); // a feature here
       simulateEvent(eoxMap.map, "pointermove", 0, -140); // no feature here
       simulateEvent(eoxMap.map, "pointermove", 120, -140); // a feature here
     });
+
+    })
   });
+
+  cy.wrap(myPromise).should('eq', 2);
 };
 
 export default selectAfterReArrangingLayers;
