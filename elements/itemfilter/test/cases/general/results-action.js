27bcc727419fa0b65ce9a313c71df10dd4c34096
Radu-Mihai Pașparugă <radupasparuga25@gmail.com>
2025-03-18 16:47:47 +0200

    chore: add result action event test

---
+++ elements/itemfilter/test/cases/general/results-action.js
@@ -0,0 +1,14 @@
+const resultsActionTest = () => {
+  cy.window().then((win) => {
+    cy.spy(win, "alert").as("alertSpy");
+  });
+  cy.get("eox-itemfilter")
+    .shadow()
+    .within(() => {
+      cy.get(".result-action").first().click()
+    });
+  cy.get("@alertSpy").should("have.been.calledWith", "White Asparagus area [%] clicked!");
+
+};
+
+export default resultsActionTest;
