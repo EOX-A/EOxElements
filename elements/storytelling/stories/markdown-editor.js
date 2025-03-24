8b3f15db771f66e642ac41a6768dc67e557da7bd
silvester-pari <silvester.pari@eox.at>
2025-03-24 08:04:15 +0100

    feat: add md footnote support

--- elements/storytelling/stories/markdown-editor.js
+++ elements/storytelling/stories/markdown-editor.js
@@ -14,6 +14,10 @@ export const MarkdownEditor = {
 
 Storytelling is crucial in the realm of science because scientific data, while rich in information, can often be complex and challenging to communicate. By framing data within narratives, storytelling makes scientific concepts accessible, engaging, and memorable to a wider audience. It bridges the gap between the technical details of research and the public's understanding, fostering appreciation, curiosity, and ultimately, a deeper connection to the wonders of science.
 
+Specifically, **scrolly**telling[^1] adds another layer of engagement, which allows the user to interact with the website by simply scrolling through it.
+
+[^1]: Blend of *scroll* + *storytelling*. [Storytelling enriched with multimedial elements triggered while scrolling down a webpage](https://en.wiktionary.org/wiki/scrollytelling).
+
 ## How do I get started?
 ### Story editor and live preview
 The main view of this page shows the live preview of the finished story. Floating above that you can see the story editor, which you can show and hide using the toggle:
