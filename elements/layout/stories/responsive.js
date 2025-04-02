e3003ddbb2a018cf63c012c877b2dbba8fcb8c10
silvester-pari <silvester.pari@eox.at>
2025-04-02 18:33:34 +0200

    chore: add gap to better illustrate story

--- elements/layout/stories/responsive.js
+++ elements/layout/stories/responsive.js
@@ -5,11 +5,11 @@ export const Responsive = {
   args: {},
   render: () => html`
     <!-- Render eox-layout component -->
-    <eox-layout style="${STORIES_LAYOUT_STYLE}">
+    <eox-layout style="${STORIES_LAYOUT_STYLE}" gap="8">
       <eox-layout-item x="0" y="0" w="12/1/1" h="1/11/12">
         x="0" y="0" w="12/1/1" h="1/11/12"
       </eox-layout-item>
-      <eox-layout-item x="0/1/1" y="1/0/0" w="12/11/11" h="11/11/12">
+      <eox-layout-item x="0/1/1" y="1/0/0" w="12/11/10" h="10/11/12">
         x="0/1/1" y="1/0/0" w="12/11/11" h="11/11/12"
       </eox-layout-item>
       <eox-layout-item x="0/0/11" y="11/11/0" w="12/12/1" h="1/1/12">
