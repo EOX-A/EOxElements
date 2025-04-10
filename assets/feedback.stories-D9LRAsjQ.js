02729f5de8fa543e7c779d69538667bc7157bddf
silvester-pari <silvester.pari@eox.at>
2025-04-10 18:05:41 +0000

    Deploying to gh-pages from @ EOX-A/EOxElements@787f80d6018ab9ea85a2276c79f5648dcdb8cee2 🚀

---
+++ assets/feedback.stories-D9LRAsjQ.js
@@ -0,0 +1,13 @@
+import{x as u}from"./lit-element-L04JKUcP.js";const b={args:{},render:()=>u`
+    <div style="height: 250px;">
+      <eox-feedback
+        endpoint="/fake/endpoint"
+        style="position: relative !important;"
+      ></eox-feedback>
+    </div>
+  `},f={args:{},render:()=>u`
+    <eox-feedback-button
+      endpoint="https://git-issue-creator.hub-int.eox.at/create-issue?repo=1038"
+      position="bottom-left"
+    ></eox-feedback-button>
+  `},l={title:"Elements/eox-feedback",tags:["autodocs"],component:"eox-feedback"},e=b,t=f;var o,r,s,a,i;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:"PrimaryStory",...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source},description:{story:"Primary story showcasing basic usage.",...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.description}}};var n,c,d,p,m;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:"ButtonStory",...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source},description:{story:"Triggering of eox-feeback modal via button. The button can be positioned using the `position` attribute.\nCan be `top-left`, `top-right`, `bottom-left`, or `bottom-right`.",...(m=(p=t.parameters)==null?void 0:p.docs)==null?void 0:m.description}}};const x=["Primary","Button"];export{t as Button,e as Primary,x as __namedExportsOrder,l as default};
