60a41094ac0a70155877467a2c15da40e026598a
srijitcoder <srijitsm123@gmail.com>
2025-09-04 16:18:17 +0530

    feat: made values dynamic and attached with map layer

--- elements/timeslider/src/main.js
+++ elements/timeslider/src/main.js
@@ -1,14 +1,18 @@
 import { LitElement, html } from "lit";
 import { style } from "./style.js";
 import { styleEOX } from "./style.eox.js";
+import Group from "ol/layer/Group";
+import { getElement } from "@eox/elements-utils";
 import { Timeline, DataSet } from "vis-timeline/standalone";
 import visTimelineCSS from "vis-timeline/styles/vis-timeline-graph2d.css?inline";
 import dayjs from "dayjs";
 import dayOfYear from "dayjs/plugin/dayOfYear";
 import isoWeek from "dayjs/plugin/isoWeek";
+import minMax from "dayjs/plugin/minMax";
 
 dayjs.extend(dayOfYear);
 dayjs.extend(isoWeek);
+dayjs.extend(minMax);
 
 /**
  * @element eox-timeslider
@@ -18,8 +22,13 @@ export class EOxTimeSlider extends LitElement {
     return {
       for: { type: String },
       unstyled: { type: Boolean },
+      sliderValues: { type: Array, attribute: "slider-values" },
     };
   }
+
+  #visTimeline = null;
+  #layerSources = null;
+  #eoxMap = null;
 
   constructor() {
     super();
@@ -33,124 +42,48 @@ export class EOxTimeSlider extends LitElement {
      * @type {String|HTMLElement}
      */
     this.for = "eox-map";
+
+    this.sliderValues = [];
   }
 
   initVisTimeline() {
-    const groups = new DataSet([
-      { id: 1, content: "Sentinel-2 L2A" },
-      { id: 2, content: "Sentinel-1 GRD" },
-      { id: 3, content: "Sentinel-1 SLC" },
-    ]);
+    if (this.sliderValues.length === 0) return;
 
-    const items = new DataSet([
-      {
-        id: 101,
-        group: 1,
-        className: "task",
-        start: "2024-10-05",
-        end: "2024-11-25",
-      },
-      {
-        id: 102,
-        group: 1,
-        className: "milestone",
-        start: "2024-11-29",
-        type: "point",
-      },
-      {
-        id: 103,
-        group: 1,
-        className: "milestone",
-        start: "2024-12-01",
-        type: "point",
-      },
-      {
-        id: 104,
-        group: 1,
-        className: "task",
-        start: "2024-12-03",
-        end: "2024-12-20",
-      },
-      {
-        id: 105,
-        group: 1,
-        className: "milestone",
-        start: "2025-01-05",
-        type: "point",
-      },
-      {
-        id: 106,
-        group: 1,
-        className: "task",
-        start: "2025-01-07",
-        end: "2025-01-18",
-      },
-      {
-        id: 107,
-        group: 1,
-        className: "milestone",
-        start: "2025-01-23",
-        type: "point",
-      },
-      {
-        id: 108,
-        group: 1,
-        className: "task",
-        start: "2025-02-01",
-        end: "2025-04-15",
-      },
+    const groups = new DataSet([]);
+    const items = new DataSet([]);
 
-      {
-        id: 201,
-        group: 2,
-        className: "task",
-        start: "2024-10-02",
-        end: "2024-12-02",
-      },
-      {
-        id: 202,
-        group: 2,
-        className: "milestone",
-        start: "2024-12-10",
-        type: "point",
-      },
-      // {
-      //   id: 203,
-      //   group: 2,
-      //   className: "task",
-      //   start: "2024-12-14",
-      //   end: "2025-04-20",
-      // },
+    for (const slider of this.sliderValues) {
+      groups.add({
+        id: slider.layer,
+        content: slider.name,
+      });
+      for (const value of slider.values) {
+        items.add({
+          group: slider.layer,
+          className: "milestone",
+          start: value.date,
+          type: "point",
+          property: slider.property,
+        });
+      }
+    }
 
-      // {
-      //   id: 301,
-      //   group: 3,
-      //   className: "task",
-      //   start: "2024-10-03",
-      //   end: "2024-11-28",
-      // },
-      // {
-      //   id: 302,
-      //   group: 3,
-      //   className: "task",
-      //   start: "2024-12-05",
-      //   end: "2025-04-20",
-      // },
-    ]);
-
+    const dates = items.map((item) => dayjs(item.start));
+    const min = dayjs.min(dates).subtract(30, "day").format("YYYY-MM-DD");
+    const max = dayjs.max(dates).add(30, "day").format("YYYY-MM-DD");
     const container = this.renderRoot.querySelector("#timeslider");
 
     const options = {
-      groupOrder: (a, b) => a.id - b.id,
+      // groupOrder: (a, b) => a.id - b.id,
       stack: false,
       selectable: true,
       zoomable: true,
       moveable: true,
       margin: { item: 40, axis: 20 },
-      start: "2024-09-20",
-      end: "2025-04-30",
-      min: "2024-07-01",
-      max: "2025-07-01",
+      start: min,
+      end: max,
+      min: min,
+      max: max,
       showCurrentTime: true,
       timeAxis: {
         scale: "day",
@@ -183,73 +116,157 @@ export class EOxTimeSlider extends LitElement {
       },
     };
 
-    const timeline = new Timeline(container, items, groups, options);
-    timeline.setCurrentTime(new Date("2025-01-03T12:00:00"));
+    if (!this.#visTimeline) {
+      this.#visTimeline = new Timeline(container, items, groups, options);
 
-    function formatDate(d) {
-      return d.toLocaleDateString(undefined, {
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-      });
-    }
-
-    function setSelected(date) {
-      const d = new Date(date);
-      if (Number.isNaN(d.getTime())) return;
-      try {
-        timeline.addCustomTime(d, "selected");
-      } catch (_) {
-        /* exists */
+      function formatDate(d) {
+        return d.toLocaleDateString(undefined, {
+          year: "numeric",
+          month: "short",
+          day: "2-digit",
+        });
       }
-      console.log(timeline);
-      timeline.setCustomTime(d, "selected");
-      timeline.setCustomTimeTitle(formatDate(d), "selected");
-      const el = container.querySelector(
-        '.vis-custom-time[data-id="selected"]',
-      );
-      if (el) el.classList.add("vis-custom-time-selected");
 
-      const selectedEle = container.querySelector(".vis-custom-time");
-      if (selectedEle) {
-        const labelEle = selectedEle.children[0];
-        labelEle.classList.add("vis-custom-time-selected-label");
-        labelEle.innerText = dayjs(date).format("MMM DD' YYYY");
+      function setSelected(
+        date,
+        layerSources,
+        visTimeline,
+        eoxMap,
+        EOxTimeSlide,
+      ) {
+        const d = new Date(date);
+        if (Number.isNaN(d.getTime())) return;
+        try {
+          visTimeline.addCustomTime(d, "selected");
+        } catch (_) {
+          /* exists */
+        }
+
+        visTimeline.setCustomTime(d, "selected");
+        visTimeline.setCustomTimeTitle(formatDate(d), "selected");
+        const el = container.querySelector(
+          '.vis-custom-time[data-id="selected"]',
+        );
+        if (el) el.classList.add("vis-custom-time-selected");
+
+        const selectedEle = container.querySelector(".vis-custom-time");
+        if (selectedEle) {
+          const labelEle = selectedEle.children[0];
+          labelEle.classList.add("vis-custom-time-selected-label");
+          labelEle.innerText = dayjs(date).format("MMM DD' YYYY");
+        }
+
+        const flatLayers = EOxTimeSlide.getFlatLayersArray(
+          /** @type {import('ol/layer/Base').default[]} */ (
+            eoxMap.map.getLayers().getArray()
+          ),
+        );
+
+        const selectedItems = items.get({
+          filter: function (item) {
+            return item.start == dayjs(date).format("YYYY-MM-DD");
+          },
+        });
+
+        selectedItems.forEach((item) => {
+          if (item.group && eoxMap) {
+            const layer = flatLayers.find((l) => l.get("id") === item.group);
+            const source = layer.getSource();
+            source.updateParams({
+              [item.property]: item.start,
+            });
+          }
+        });
+        EOxTimeSlide.requestUpdate();
       }
-    }
 
-    timeline.on("rangechange", (props) => {
-      const range = timeline.getWindow();
-      const rangeWidth = range.end - range.start; // milliseconds
-      const containerWidth = container.offsetWidth;
+      this.#visTimeline.on("rangechange", (props) => {
+        const range = this.#visTimeline.getWindow();
+        const rangeWidth = range.end - range.start; // milliseconds
+        const containerWidth = container.offsetWidth;
 
-      // Calculate width per millisecond
-      const msPerPixel = rangeWidth / containerWidth;
+        // Calculate width per millisecond
+        const msPerPixel = rangeWidth / containerWidth;
 
-      // Set milestone width based on visible date range
-      // Adjust the multiplier as needed for desired milestone size
-      const milestoneWidth = Math.max(
-        2,
-        Math.min(20, containerWidth / (rangeWidth / (1000 * 60 * 60 * 24 * 7))),
-      ); // 1 week base
+        // Set milestone width based on visible date range
+        // Adjust the multiplier as needed for desired milestone size
+        const milestoneWidth = Math.max(
+          2,
+          Math.min(
+            20,
+            containerWidth / (rangeWidth / (1000 * 60 * 60 * 24 * 7)),
+          ),
+        ); // 1 week base
 
-      const milestoneElements = container.querySelectorAll(
-        ".vis-item.milestone",
-      );
-      milestoneElements.forEach((milestone) => {
-        milestone.style.width = `${milestoneWidth}px`;
+        const milestoneElements = container.querySelectorAll(
+          ".vis-item.milestone",
+        );
+        milestoneElements.forEach((milestone) => {
+          milestone.style.width = `${milestoneWidth}px`;
+        });
       });
-    });
 
-    timeline.on("click", (props) => {
-      if (props && props.time) setSelected(props.time);
-    });
+      this.#visTimeline.on("click", (props) => {
+        if (props && props.time)
+          setSelected(
+            props.time,
+            this.#layerSources,
+            this.#visTimeline,
+            this.#eoxMap,
+            this,
+          );
+      });
 
-    timeline.addCustomTime(new Date(), "selected");
+      this.#visTimeline.addCustomTime(new Date(), "selected");
+    }
   }
 
   firstUpdated() {
     this.initVisTimeline();
+    this.updateMap();
+  }
+
+  /**
+   * TEMP / TO-DO, this is a copy of the function defined in the eox-map:
+   * https://github.com/EOX-A/EOxElements/blob/main/elements/map/src/layer.ts#L25
+   * Consider a way to properly export that function and use it here
+   * See also:
+   * https://github.com/EOX-A/EOxElements/issues/974
+   * @param {import('ol/layer/Base').default[]} layers
+   * @returns {import('ol/layer/Base').default[]}
+   */
+  getFlatLayersArray(layers) {
+    const flatLayers = [];
+    flatLayers.push(...layers);
+
+    /** @type {Array<Group>} */
+    let groupLayers =
+      /** @type {Array<Group>} */
+      (flatLayers.filter((l) => l instanceof Group));
+    while (groupLayers.length) {
+      /** @type {Array<Group>} */
+      const newGroupLayers = [];
+      for (let i = 0, ii = groupLayers.length; i < ii; i++) {
+        const layersInsideGroup = groupLayers[i].getLayers().getArray();
+        flatLayers.push(...layersInsideGroup);
+        /** @type {Array<Group>} */
+        const filteredGroups = /** @type {Array<Group>} */ (
+          layersInsideGroup.filter((l) => l instanceof Group)
+        );
+        newGroupLayers.push(...filteredGroups);
+      }
+      groupLayers = newGroupLayers;
+    }
+    return flatLayers;
+  }
+
+  updateMap() {
+    const foundElement = getElement(this.for);
+
+    if (foundElement) {
+      const EoxMap = /** @type {import("@eox/map").EOxMap} */ (foundElement);
+      this.#eoxMap = EoxMap;
+    }
   }
 
   render() {
--- elements/timeslider/stories/primary.js
+++ elements/timeslider/stories/primary.js
@@ -1,5 +1,4 @@
 import { html } from "lit";
-import { DEFAULT_ARGS } from "../src/enums/stories";
 
 /**
  * Generates a story configuration for the Primary TimeSlider.
@@ -8,18 +7,221 @@ import { DEFAULT_ARGS } from "../src/enums/stories";
  */
 export const Primary = {
   args: {
-    ...DEFAULT_ARGS,
+    center: [1000000, 6000000],
+    zoom: 5,
+    layers: [
+      {
+        type: "Tile",
+        properties: {
+          id: "AWS_NO2-VISUALISATION",
+        },
+        source: {
+          type: "TileWMS",
+          url: "https://services.sentinel-hub.com/ogc/wms/0635c213-17a1-48ee-aef7-9d1731695a54",
+          params: {
+            LAYERS: "AWS_NO2-VISUALISATION",
+            TIME: "2022-12-05",
+          },
+        },
+      },
+      {
+        type: "Tile",
+        properties: {
+          id: "AWS_VIS_WIND_V_10M",
+        },
+        source: {
+          type: "TileWMS",
+          url: "https://services.sentinel-hub.com/ogc/wms/0635c213-17a1-48ee-aef7-9d1731695a54",
+          params: {
+            LAYERS: "AWS_VIS_WIND_V_10M",
+            TIME: "2022-12-05",
+          },
+        },
+      },
+      {
+        type: "Tile",
+        properties: {
+          id: "OSM",
+        },
+        source: {
+          type: "OSM",
+        },
+      },
+    ],
     for: "eox-map",
+    sliderValues: [
+      {
+        layer: "AWS_VIS_WIND_V_10M",
+        name: "Wind Visualisation 10M",
+        property: "TIME",
+        values: [
+          {
+            date: "2022-12-05",
+            cloudCoverage: 20,
+          },
+          {
+            date: "2022-12-12",
+            cloudCoverage: 15,
+          },
+          {
+            date: "2022-12-19",
+            cloudCoverage: 35,
+          },
+          {
+            date: "2022-12-26",
+            cloudCoverage: 8,
+          },
+          {
+            date: "2023-01-16",
+            cloudCoverage: 42,
+          },
+          {
+            date: "2023-01-23",
+            cloudCoverage: 28,
+          },
+          {
+            date: "2023-01-30",
+            cloudCoverage: 12,
+          },
+          {
+            date: "2023-02-06",
+            cloudCoverage: 55,
+          },
+          {
+            date: "2023-02-13",
+            cloudCoverage: 33,
+          },
+          {
+            date: "2023-02-27",
+            cloudCoverage: 18,
+          },
+          {
+            date: "2023-03-06",
+            cloudCoverage: 47,
+          },
+          {
+            date: "2023-03-13",
+            cloudCoverage: 25,
+          },
+          {
+            date: "2023-03-20",
+            cloudCoverage: 9,
+          },
+          {
+            date: "2023-03-27",
+            cloudCoverage: 38,
+          },
+          {
+            date: "2023-04-03",
+            cloudCoverage: 22,
+          },
+          {
+            date: "2023-04-10",
+            cloudCoverage: 51,
+          },
+          {
+            date: "2023-04-17",
+            cloudCoverage: 14,
+          },
+          {
+            date: "2023-04-24",
+            cloudCoverage: 29,
+          },
+        ],
+      },
+      {
+        layer: "AWS_NO2-VISUALISATION",
+        name: "NO2 Visualisation",
+        property: "TIME",
+        values: [
+          {
+            date: "2022-12-05",
+            cloudCoverage: 73,
+          },
+          {
+            date: "2022-12-12",
+            cloudCoverage: 41,
+          },
+          {
+            date: "2022-12-19",
+            cloudCoverage: 87,
+          },
+          {
+            date: "2022-12-26",
+            cloudCoverage: 12,
+          },
+          {
+            date: "2023-01-16",
+            cloudCoverage: 95,
+          },
+          {
+            date: "2023-01-23",
+            cloudCoverage: 64,
+          },
+          {
+            date: "2023-01-30",
+            cloudCoverage: 28,
+          },
+          {
+            date: "2023-02-06",
+            cloudCoverage: 91,
+          },
+          {
+            date: "2023-02-13",
+            cloudCoverage: 56,
+          },
+          {
+            date: "2023-02-27",
+            cloudCoverage: 7,
+          },
+          {
+            date: "2023-03-06",
+            cloudCoverage: 83,
+          },
+          {
+            date: "2023-03-13",
+            cloudCoverage: 39,
+          },
+          {
+            date: "2023-03-20",
+            cloudCoverage: 15,
+          },
+          {
+            date: "2023-03-27",
+            cloudCoverage: 76,
+          },
+          {
+            date: "2023-04-03",
+            cloudCoverage: 52,
+          },
+          {
+            date: "2023-04-10",
+            cloudCoverage: 98,
+          },
+          {
+            date: "2023-04-17",
+            cloudCoverage: 23,
+          },
+          {
+            date: "2023-04-24",
+            cloudCoverage: 67,
+          },
+        ],
+      },
+    ],
   },
   render: (args) => html`
     <eox-map
       id="primary"
-      style="width: 100%; height: 300px;"
+      style="width: 100%; height: 500px;"
       .zoom=${args.zoom}
       .center=${args.center}
       .layers=${args.layers}
     ></eox-map>
-    <eox-timeslider .for=${args.for}></eox-timeslider>
+    <eox-timeslider
+      .sliderValues=${args.sliderValues}
+      .for=${args.for}
+    ></eox-timeslider>
   `,
 };
 
