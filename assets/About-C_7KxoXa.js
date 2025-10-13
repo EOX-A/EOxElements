51e1c668f621c848eab2e8005f58b6c56b30521a
silvester-pari <silvester.pari@eox.at>
2025-10-13 14:16:11 +0000

    Deploying to gh-pages from @ EOX-A/EOxElements@ee5024710073f0c6a674f6410a14318ff11e4359 🚀

--- assets/About-C_7KxoXa.js
+++ assets/About-C_7KxoXa.js
@@ -7,7 +7,7 @@ The goal is to reduce code duplication during development, and offer ready-made 
 increased long-term stabilty.`}),`
 `,e.jsx(t.h2,{id:"technology",children:"Technology"}),`
 `,e.jsxs(t.p,{children:["The EOxElements are a suite of custom elements (a.k.a. ",e.jsx(t.a,{href:"https://www.webcomponents.org/introduction",rel:"nofollow",children:"web components"}),`), which are framework-agnostic.
-Thus, they can be used in applications built with Vanilla JavaScript, Vue.js, React, Angular etc.`]}),`
+Thus, they can be used in applications built with Vanilla JavaScript, Vue.js, React, Angular etc. For usage examples please have a look at `,e.jsx(t.a,{href:"/docs/usage--docs",children:"the provided examples"}),"."]}),`
 `,e.jsx(t.h2,{id:"current-state",children:"Current state"}),`
 `,e.jsxs(t.p,{children:[`All of the elements are currently functional, but not yet ready for production-use because of frequent breaking changes.
-Please refer to the repository's `,e.jsx(t.a,{href:"https://github.com/EOX-A/EOxElements/milestones",rel:"nofollow",children:"Milestones"})," to get a feeling of when to expect a producion-ready v1.0.0."]})]})}function a(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{a as default};
+Please refer to the repository's `,e.jsx(t.a,{href:"https://github.com/EOX-A/EOxElements/milestones",rel:"nofollow",children:"Milestones"})," to get a feeling of when to expect a producion-ready v1.0.0."]})]})}function i(n={}){const{wrapper:t}={...s(),...n.components};return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}export{i as default};
