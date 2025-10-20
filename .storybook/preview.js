6d61d73d0c78ba75baf38a0a0f8f55340d15c66d
silvester-pari <silvester.pari@eox.at>
2025-10-20 23:53:07 +0200

    chore(docs): link type definitions in args table

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { setCustomElementsManifest } from "@storybook/web-components-vite";
 import customElements from "../custom-elements.json";
+import typedocJson from "../typedoc.json";
 import DocumentationTemplate from "./DocumentationTemplate.mdx";
 
 import "@eox/chart";
@@ -49,6 +50,60 @@ const preview = {
     docs: {
       toc: true,
       page: DocumentationTemplate,
+      extractArgTypes: (component) => {
+        const {
+          attributes,
+          events,
+          members: properties,
+        } = customElements.modules.find(
+          (m) => m.declarations[0].tagName === component,
+        ).declarations[0];
+        return [
+          ...properties.map((m) => ({
+            category: m.kind === "method" ? "methods" : "properties",
+            ...m,
+          })),
+          ...attributes.map((a) => ({ category: "attributes", ...a })),
+          ...events.map((a) => ({ category: "events", ...a })),
+        ].reduce((acc, curr) => {
+          const {
+            category,
+            default: defaultValue,
+            description,
+            name,
+            parameters,
+            type,
+          } = curr;
+          const typeText =
+            parameters?.[0].type?.text ||
+            parameters?.[0].type ||
+            type?.text ||
+            type;
+          let matchingType;
+
+          typedocJson.children.forEach((c) => {
+            const child = c.children?.find((cc) => typeText?.includes(cc.name));
+            if (child) matchingType = child;
+          });
+          acc[name] = {
+            description: matchingType
+              ? `${description} <br /> <code>${typeText.replace(matchingType.name, `<a target="_blank" href="${matchingType?.sources?.[0]?.url}">${matchingType.name}</a>`)}</code>`
+              : ``,
+            table: {
+              defaultValue: { summary: defaultValue },
+              category,
+              type: {
+                ...(!matchingType
+                  ? {
+                      summary: typeText,
+                    }
+                  : {}),
+              },
+            },
+          };
+          return acc;
+        }, {});
+      },
       source: {
         transform: (code, storyContext) => {
           /**
