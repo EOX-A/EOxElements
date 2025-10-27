1cc49d8d3eecff41ec6d564002ce45c3290c38c7
silvester-pari <silvester.pari@eox.at>
2025-10-27 14:33:59 +0100

    chore: rewrite example to account for map movement

--- elements/timeslider/stories/ExternalMapRendering.js
+++ elements/timeslider/stories/ExternalMapRendering.js
@@ -6,8 +6,8 @@ import { html } from "lit";
  */
 export const ExternalMapRendering = {
   args: {
-    center: [12.5, 42],
-    zoom: 9,
+    center: [12, 42],
+    zoom: 10,
     for: "eox-map",
     layerIdKey: "id",
     titleKey: "name",
@@ -46,130 +46,107 @@ export const ExternalMapRendering = {
       @update=${args.update}
     ></eox-timeslider>
     <script>
-      const init = async () => {
-        const eoxMap = document.querySelector("eox-map");
-        // Fetch collection
+      const eoxMap = document.querySelector("eox-map");
+      let items = [];
+      const createLayerGroup = (
+        items,
+        displayFilter = (i) =>
+          i.properties.datetime === items[items.length - 1].properties.datetime
+      ) => ({
+        type: "Group",
+        layers: [
+          ...items
+            .filter((i) => displayFilter(i))
+            .map((i) => ({
+              type: "Tile",
+              source: {
+                type: "TileJSON",
+                url: i.assets.tilejson.href,
+                crossOrigin: "anonymous",
+              },
+              properties: {
+                id: i.id,
+                title: "test",
+              },
+            })),
+        ],
+        properties: {
+          id: "sentinel-2",
+          title: items.length ? items[0].properties.datetime : "dummy title",
+          timeControlValues:
+            items?.map((f) => ({
+              date: f.properties.datetime,
+              itemId: f.id,
+              cloudCoverage: f.properties["eo:cloud_cover"],
+            })) || [],
+          timeControlProperty: "dummy",
+        },
+      });
+
+      const fetchItems = async (extent = eoxMap.lonLatExtent) => {
         const url =
           "https://planetarycomputer.microsoft.com/api/stac/v1/search?collections=sentinel-2-l2a&bbox=" +
           eoxMap.lonLatExtent +
           "&limit=200&datetime=2025-01-01T00:00:00Z/2026-01-01T00:00:00Z&sortby=+datetime";
         const searchResponse = await fetch(url);
         const { features } = await searchResponse.json();
-        const items = features;
-        const itemsByDate = features.reduce((acc, item, index) => {
-          //if (index) {
-          const date = item.properties.datetime.split("T")[0];
-          if (!acc[date]) {
-            acc[date] = [];
-            acc[date].push(item);
-            items.push(item);
-          }
-          //}
-          return acc;
-        }, {});
-        // Layer creation function
-        const createXyzLayer = (item) => {
-          return {
-            type: "Tile",
-            source: {
-              type: "TileJSON",
-              url: item?.assets.tilejson.href,
-              crossOrigin: "anonymous",
-            },
-            properties: {
-              id: item.id,
-              title: "test",
-            },
-          };
-        };
-        // Listen for timeslider updates and update map layers accordingly
-        document
-          .querySelector("eox-timeslider")
-          .addEventListener("update", (e) => {
-            const eoxMap = document.querySelector("eox-map");
-
-            eoxMap.layers = [
-              {
-                type: "Group",
-                layers: [
-                  ...items
-                    .filter(
-                      (i) =>
-                        i.properties.datetime ===
-                        e.detail.selectedItems["sentinel-2"][0].originalDate,
-                    )
-                    .map((i) => createXyzLayer(i)),
-                ],
-                properties: {
-                  id: "sentinel-2",
-                  title: items[0].properties.datetime,
-                  timeControlValues: items.map((f, index) => ({
-                    date: f.properties.datetime,
-                    itemId: f.id,
-                    cloudCoverage: f.properties["eo:cloud_cover"],
-                  })),
-                  timeControlProperty: "dummy",
-                },
-              },
-              eoxMap.layers[0],
-            ];
-          });
-        document
-          .querySelector("eox-timeslider")
-          .addEventListener("export", (e) => {
-            const eoxMap = document.querySelector("eox-map");
-            const mapLayers = [];
-            for (const dateKey in e.detail.selectedRangeItems) {
-              const date = e.detail.selectedRangeItems[dateKey];
-              const layers = [];
-              let center = null;
-              for (const itemKey in date) {
-                const item = items.find(
-                  (item) => item.id === date[itemKey].itemId,
-                );
-                center = item.properties["proj:centroid"];
-                layers.push(createXyzLayer(item));
-              }
-              layers.push(eoxMap.layers[0]);
-              mapLayers.push({
-                layers,
-                center: [center.lon, center.lat],
-                zoom: 9,
-                date: dateKey,
-              });
-            }
-            e.detail.generate({
-              mapLayers,
-            });
-          });
-        // Initial rendering
-        document.querySelector("eox-map").layers = [
-          {
-            type: "Group",
-            layers: [...items.map((i) => createXyzLayer(i))],
-            properties: {
-              id: "sentinel-2",
-              title: items[0].properties.datetime,
-              timeControlValues: items.map((f, index) => ({
-                date: f.properties.datetime,
-                itemId: f.id,
-                cloudCoverage: f.properties["eo:cloud_cover"],
-              })),
-              timeControlProperty: "dummy",
-            },
-          },
-          {
-            type: "Tile",
-            properties: {
-              id: "OSM",
-            },
-            source: {
-              type: "OSM",
-            },
-          },
-        ];
+        return features;
       };
-      init();
+
+      // Listen for timeslider updates and update map layers accordingly
+      document.querySelector("eox-timeslider").addEventListener("update", (e) => {
+        if (!items.length) return;
+        eoxMap.layers = [
+          createLayerGroup(
+            items,
+            (i) =>
+              i.properties.datetime ===
+              e.detail.selectedItems["sentinel-2"]?.[0].originalDate
+          ),
+          eoxMap.layers[0],
+        ];
+      });
+      document.querySelector("eox-timeslider").addEventListener("export", (e) => {
+        const mapLayers = [];
+        for (const dateKey in e.detail.selectedRangeItems) {
+          const date = e.detail.selectedRangeItems[dateKey];
+          const layers = [];
+          let center = null;
+          for (const itemKey in date) {
+            const item = items.find((item) => item.id === date[itemKey].itemId);
+            center = item.properties["proj:centroid"];
+            layers.push(createXyzLayer(item));
+          }
+          layers.push(eoxMap.layers[0]);
+          mapLayers.push({
+            layers,
+            center: [center.lon, center.lat],
+            zoom: 9,
+            date: dateKey,
+          });
+        }
+        e.detail.generate({
+          mapLayers,
+        });
+      });
+
+      // Initial rendering
+      document.querySelector("eox-map").layers = [
+        {
+          type: "Tile",
+          properties: {
+            id: "OSM",
+          },
+          source: {
+            type: "OSM",
+          },
+        },
+      ];
+
+      document.querySelector("eox-map").map.on("moveend", async () => {
+        items = await fetchItems(eoxMap.currentLonLatExtent);
+        eoxMap.layers = [createLayerGroup(items), eoxMap.layers[0]];
+      });
     </script>
   `,
 };
