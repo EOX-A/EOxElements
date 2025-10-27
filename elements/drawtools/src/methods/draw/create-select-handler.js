149c76b07e84d87d05c3ad0717562217676e66da
Daniel Santillan <daniel.santillan@eox.at>
2025-10-27 16:16:25 +0100

    fix: converting render feature into full ol feature when using vector tiles

--- elements/drawtools/src/methods/draw/create-select-handler.js
+++ elements/drawtools/src/methods/draw/create-select-handler.js
@@ -2,6 +2,9 @@
  * Factory function to create a select event handler
  * @param {import("../../main").EOxDrawTools} EoxDrawTool - The drawing tool instance.
  */
+import GeoJSON from 'ol/format/GeoJSON';
+
+
 const createSelectHandler = (EoxDrawTool) => {
   /**
    * Copy the selected feature to the draw layer
@@ -13,6 +16,23 @@ const createSelectHandler = (EoxDrawTool) => {
     if (e?.detail.id !== "SelectLayerClickInteraction" || !e.detail.feature) {
       return;
     }
+    // Vector tiles geometries do not have the getCoordinates method
+    // only dispatch event if the feature has the getCoordinates method
+    if (typeof e.detail.feature.getGeometry().getCoordinates !== "function") {
+      // try to use the internal flatCoordinates_ and type_ to build a new feature
+      const geom = e.detail.feature.getGeometry();
+      const geojsonFormat = new GeoJSON();
+      const featureGeoJSON = {
+        type: "Feature",
+        geometry: {
+          type: geom.getType(),
+          coordinates: geom.flatCoordinates_,
+        },
+        properties: e.detail.feature.getProperties(),
+      };
+      const newFeature = geojsonFormat.readFeature(featureGeoJSON);
+      e.detail.feature = newFeature;
+    }    
     EoxDrawTool.drawLayer.getSource().addFeature(e.detail.feature);
     EoxDrawTool.eoxMap.dispatchEvent(
       new CustomEvent("addfeatures", { detail: e.detail }),
