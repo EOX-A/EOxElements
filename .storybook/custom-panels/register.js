9fbbde8f8cabf98c1ea93c2fd4ccffa58b9a3b4a
silvester-pari <silvester.pari@eox.at>
2025-10-29 14:38:26 +0100

    feat(docs): render story description in panels

---
+++ .storybook/custom-panels/register.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { addons, types } from "storybook/manager-api";
+import { DescriptionPanel } from "./DescriptionPanel.js";
+
+addons.register("eox/storybook-custom-panels", () => {
+  // Register the tool
+  addons.add("eox/storybook-custom-panels/description", {
+    type: types.PANEL,
+    title: "Description",
+    match: ({ tabId, viewMode }) => !tabId && viewMode === "story",
+    render: ({ active }) => <DescriptionPanel active={active} />,
+  });
+});
