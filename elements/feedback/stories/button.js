e341c774154384960d5a2c5bc875143ab278f9d2
silvester-pari <silvester.pari@eox.at>
2025-11-10 16:08:30 +0100

    chore(docs): eox-feedback  stories & description refactor

--- elements/feedback/stories/button.js
+++ elements/feedback/stories/button.js
@@ -1,13 +1,17 @@
 import { html } from "lit";
 
-export const Button = {
-  args: {},
-  render: () => html`
+export default {
+  args: {
+    endpoint: "/fake/endpoint",
+    position: "bottom-left",
+    // Example event handler as stringified function
+    click: "(e) => console.log('Feedback button clicked', e)",
+  },
+  render: (args) => html`
     <eox-feedback-button
-      endpoint="/fake/endpoint"
-      position="bottom-left"
+      endpoint="${args.endpoint}"
+      position="${args.position}"
+      @click="${eval(args.click)}"
     ></eox-feedback-button>
   `,
 };
-
-export default Button;
--- elements/feedback/stories/primary.js
+++ elements/feedback/stories/primary.js
@@ -1,15 +1,22 @@
 import { html } from "lit";
 
-export const Primary = {
-  args: {},
-  render: () => html`
-    <div style="height: 250px;">
+export default {
+  args: {
+    endpoint: "/fake/endpoint",
+    style: "position: relative !important; height: 250px;",
+    unstyled: undefined,
+    // Example event handler as stringified function
+    submit: "(e) => console.log('Feedback submitted', e.detail)",
+    close: "(e) => console.log('Feedback closed')",
+  },
+  render: (args, storyAdditionalComponents = {}) => html`
+    <div style="${args.style}">
       <eox-feedback
-        endpoint="/fake/endpoint"
-        style="position: relative !important;"
+        endpoint="${args.endpoint}"
+        ?unstyled="${args.unstyled}"
+        @submit="${eval(args.submit)}"
+        @close="${eval(args.close)}"
       ></eox-feedback>
     </div>
   `,
 };
-
-export default Primary;
