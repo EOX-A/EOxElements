04ac6e6d5ced147e48abf484766b73f23bd0493a
Silvester <silvester.pari@eox.at>
2025-11-10 14:30:52 +0100

    fix: only render individual featured sections if value length > 0 (#1930)
    * fix: only render individual featured sections if value length is larger than 0
    
    * chore: test

--- elements/stacinfo/test/cases/index.js
+++ elements/stacinfo/test/cases/index.js
@@ -1,5 +1,6 @@
 export { default as LoadStacinfoTest } from "./load-stacinfo";
 export { default as CreateListTest } from "./create-list";
+export { default as CreateFeaturedTest } from "./create-featured";
 export { default as SinglePropertyTest } from "./single-property";
 export { default as NoHtmlRenderTest } from "./no-html-render";
 export { default as htmlRenderTest } from "./html-render";
