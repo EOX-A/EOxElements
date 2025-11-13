3bbfe8350e34a76ef8c5952043cefe9acc7786f9
santilland <daniel.santillan@eox.at>
2025-11-13 13:58:06 +0100

    fix: using toFeature to convert vector tile feature

--- elements/drawtools/src/methods/draw/create-select-handler.js
+++ elements/drawtools/src/methods/draw/create-select-handler.js
@@ -2,7 +2,7 @@
  * Factory function to create a select event handler
  * @param {import("../../main").EOxDrawTools} EoxDrawTool - The drawing tool instance.
  */
-import GeoJSON from 'ol/format/GeoJSON';
+import {toFeature} from 'ol/render/Feature';
 
 
 const createSelectHandler = (EoxDrawTool) => {
@@ -19,19 +19,7 @@ const createSelectHandler = (EoxDrawTool) => {
     // Vector tiles geometries do not have the getCoordinates method
     // only dispatch event if the feature has the getCoordinates method
     if (typeof e.detail.feature.getGeometry().getCoordinates !== "function") {
-      // try to use the internal flatCoordinates_ and type_ to build a new feature
-      const geom = e.detail.feature.getGeometry();
-      const geojsonFormat = new GeoJSON();
-      const featureGeoJSON = {
-        type: "Feature",
-        geometry: {
-          type: geom.getType(),
-          coordinates: geom.flatCoordinates_,
-        },
-        properties: e.detail.feature.getProperties(),
-      };
-      const newFeature = geojsonFormat.readFeature(featureGeoJSON);
-      e.detail.feature = newFeature;
+      e.detail.feature = toFeature(e.detail.feature);
     }    
     EoxDrawTool.drawLayer.getSource().addFeature(e.detail.feature);
     EoxDrawTool.eoxMap.dispatchEvent(
