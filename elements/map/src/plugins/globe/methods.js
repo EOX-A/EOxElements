476e64e197e69998c3091be4a95865384e93be98
Mussab Abdalla <baloola-mu@hotmail.com>
2025-12-12 16:07:15 +0100

    feat: globe view (#1881)
    * initial commit
    
    * feat: canvas tiles
    
    * chore: update bg color
    
    * feat: mapPool logic
    
    * chore: cleanup
    
    * chore: more cleanup
    
    * chore: more cleanup
    
    * Comment out zoom level properties in globe.jschore: remove zoom constraint from example
    
    Comment out minzoom and maxzoom properties for osm-base layer.
    
    * making wms globe compatible ( setSetate )
    
    * fix tile size compatibility
    
    * initial dimension switch
    
    * re-projecting from-to globe
    
    * adding 2d/3d switch story
    
    * fix: ts errors
    
    * fix: globe lonlat getters
    
    * feat: listen for layer updates
    
    * types fix
    
    * handle layer groups update
    
    * WIP: hybrid
    
    * chore: fix url
    
    * adding GeoZarr source
    
    * feat: split up layers (WIP)
    
    * chore: reduce timeouts, trying to fix rendering artefacts
    
    * layer change adaptation
    
    * chore: major refactoring
    
    * wms rendering in openglobus
    
    * chore: formatting
    
    * chore: remove geozarr references
    
    * fix: typing/linting
    
    * add vector layer
    
    * formatting
    
    * test fixing
    
    * cleanup
    
    * fix linting
    
    * remove duplicated functions/files
    
    * move enableGlobus & disableGlobus from main.js
    
    * ignore openglobus dependency in cypress test
    
    * cleanup
    
    * remove switch dimensions story
    
    * types fixing
    
    * undo changes in projection story
    
    * format fix
    
    * remove extra arguments
    
    * add initial globe test
    
    * remove layer control fix
    
    * more globe tests
    
    * reordering functions, remove duplications, & un-used functions/classes
    
    * edit some globe configs
    
    * formatting fix
    
    ---------
    
    Co-authored-by: silvester-pari <silvester.pari@eox.at>
    Co-authored-by: A-Behairi <behairiahmed@gmail.com>

---
+++ elements/map/src/plugins/globe/methods.js
@@ -0,0 +1,157 @@
+import { createXYZ } from "ol/tilegrid";
+import { getFlatLayersArray } from "../../helpers/layer.js";
+
+/** @typedef {import("./types").ExtendedOLLayer} ExtendedOLLayer */
+
+export const createMapPool = (maxMaps, EOxMap, target) => {
+  // Get the serializable definitions of all layers on the main map.
+  const allMainLayers = getFlatLayersArray(EOxMap.map.getLayers().getArray());
+  const layerDefs = allMainLayers
+    .map((l) => l.get("_jsonDefinition"))
+    .filter(Boolean);
+
+  return Array.from({ length: maxMaps }, () => {
+    const tileMap = /** @type {import("../../main").EOxMap} **/ (
+      document.createElement("eox-map")
+    );
+    Object.assign(tileMap.style, {
+      width: `256px`,
+      height: `256px`,
+      position: "absolute",
+      top: `-9999px`,
+      left: `-9999px`,
+    });
+
+    // Initialize the tileMap with its own instances of all layers
+    Object.assign(tileMap, {
+      projection: "EPSG:3857",
+      layers: layerDefs,
+    });
+
+    target.appendChild(tileMap);
+    return {
+      tileMap,
+      tileQueue: [],
+      loadNextTile() {
+        const job = this.tileQueue[0];
+        requestTileFromMap(tileMap, job.layerId, job, this);
+      },
+    };
+  });
+};
+
+async function requestTileFromMap(tileMap, layerId, job, mapPoolMap) {
+  const map = tileMap.map;
+
+  // Wait for the map to be mounted before proceeding
+  if (!map) {
+    tileMap.addEventListener(
+      "mapmounted",
+      () => requestTileFromMap(tileMap, layerId, job, mapPoolMap),
+      { once: true },
+    );
+    return;
+  }
+
+  // Instead of re-creating the layer, make sure only the target layer is visible
+  map.getLayers().forEach((l) => {
+    l.setVisible(l.get("id") === layerId);
+  });
+
+  const tileGrid = createXYZ();
+  const extent = tileGrid.getTileCoordExtent([
+    job.tile.z,
+    job.tile.x,
+    job.tile.y,
+  ]);
+
+  map.getView().fit(extent, {
+    callback: () => {
+      map.once("rendercomplete", function () {
+        const mapCanvas = document.createElement("canvas");
+        const size = map.getSize();
+        mapCanvas.width = size[0];
+        mapCanvas.height = size[1];
+        const mapContext = mapCanvas.getContext("2d");
+        Array.prototype.forEach.call(
+          map
+            .getViewport()
+            .querySelectorAll(".ol-layer canvas, canvas.ol-layer"),
+          function (canvas) {
+            if (canvas.width > 0) {
+              const opacity =
+                canvas.parentNode.style.opacity || canvas.style.opacity;
+              mapContext.globalAlpha = opacity === "" ? 1 : Number(opacity);
+              let matrix;
+              const transform = canvas.style.transform;
+              if (transform) {
+                matrix = transform
+                  .match(/^matrix\(([^(]*)\)$/)[1]
+                  .split(",")
+                  .map(Number);
+              } else {
+                matrix = [
+                  parseFloat(canvas.style.width) / canvas.width,
+                  0,
+                  0,
+                  parseFloat(canvas.style.height) / canvas.height,
+                  0,
+                  0,
+                ];
+              }
+              CanvasRenderingContext2D.prototype.setTransform.apply(
+                mapContext,
+                matrix,
+              );
+              const backgroundColor = canvas.parentNode.style.backgroundColor;
+              if (backgroundColor) {
+                mapContext.fillStyle = backgroundColor;
+                mapContext.fillRect(0, 0, canvas.width, canvas.height);
+              }
+              mapContext.drawImage(canvas, 0, 0);
+            }
+          },
+        );
+        mapContext.globalAlpha = 1;
+        mapContext.setTransform(1, 0, 0, 1, 0, 0);
+        job.callback(mapCanvas);
+        mapPoolMap.tileQueue.shift();
+        if (mapPoolMap.tileQueue.length) {
+          mapPoolMap.loadNextTile();
+        }
+      });
+      map.renderSync();
+    },
+  });
+}
+
+export function distributeTileToIdealMap(mapPool, layerId, tile, callback) {
+  const { x, y, z } = tile;
+  const job = {
+    layerId, // The ID of the layer to render
+    tile,
+    callback,
+  };
+  let minDistance = 0;
+  let idealMapObj = null;
+
+  for (let i = 0; i < mapPool.length; i++) {
+    const mapObj = mapPool[i];
+    if (!mapObj.tileQueue.length) {
+      idealMapObj = mapObj;
+      break;
+    }
+
+    const lastQueuedJob = mapObj.tileQueue[mapObj.tileQueue.length - 1];
+    const distance =
+      Math.abs(lastQueuedJob.tile.x - x) +
+      Math.abs(lastQueuedJob.tile.y - y) +
+      Math.abs(lastQueuedJob.tile.z - z);
+    if (minDistance === 0 || distance < minDistance) {
+      minDistance = distance;
+      idealMapObj = mapObj;
+    }
+  }
+  idealMapObj.tileQueue.push(job);
+  return idealMapObj;
+}
