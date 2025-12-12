0c030fa1d641aa29afd013d8a341865b8eac3d1f
baloola <baloola-mu@hotmail.com>
2025-12-12 12:49:07 +0100

    format fix

--- elements/map/src/types.ts
+++ elements/map/src/types.ts
@@ -243,8 +243,8 @@ declare global {
     eoxMapAdvancedOlLayers: OLAdvancedLayers;
     eoxMapAdvancedOlSources: OLAdvancedSources;
     eoxMapGlobe: {
-      create: typeof import("./plugins/globe/index").create,
-      refresh: typeof import("./plugins/globe/openglobus").refreshGlobe,
+      create: typeof import("./plugins/globe/index").create;
+      refresh: typeof import("./plugins/globe/openglobus").refreshGlobe;
     };
   }
 }
