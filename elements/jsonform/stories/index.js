ab3a66580050c95c249dfd7e61e102ab02853280
Silvester <silvester.pari@eox.at>
2025-12-16 21:14:34 +0100

    feat(style): flex layout (#2031)

--- elements/jsonform/stories/index.js
+++ elements/jsonform/stories/index.js
@@ -18,3 +18,4 @@ export { default as ValidationStory } from "./validation"; // Validate input
 export { default as CodeStory } from "./code"; // Show code editor as input
 export { default as OptionalPropertiesStory } from "./optional-properties"; // Hide optional properties
 export { default as ShowOptInPropertiesStory } from "./show-opt-in-properties"; // Show opt-in properties
+export { default as FlexLayoutStory } from "./flex-layout"; // Flex layout story
