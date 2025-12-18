e0699b7106872885e864f8976945fdeaba67bb0e
srijitcoder <srijitsm123@gmail.com>
2025-12-18 18:27:27 +0530

    chore: dummy test added to ttimecontrol so that test pipeline dont fail

--- elements/timecontrol/test/general.cy.js
+++ elements/timecontrol/test/general.cy.js
@@ -1,12 +1,14 @@
+// Importing necessary modules, test cases, and enums
+import "../src/main";
+
 // Test suite for TimeControl
 describe("TimeControl", () => {
   // Set up before each test
   beforeEach(() => {
-    cy.mount("<mock-map></mock-map>").as("mock-map");
-    cy.mount(`<eox-timecontrol for="mock-map"></eox-timecontrol>`).as(
-      "eox-timecontrol",
-    );
+    cy.mount(`<eox-timecontrol></eox-timecontrol>`).as("eox-timecontrol");
   });
 
   // TODO: More test need to be added
+  // Sample test added so that test pipeline doesn't break due to no tests in timecontrol
+  it("loads the timecontrol", () => cy.get("eox-timecontrol").shadow());
 });
