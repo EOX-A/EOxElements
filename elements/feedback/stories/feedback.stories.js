8b4961aab7af7558e80f4eb8f14fae751994e80d
Silvester <silvester.pari@eox.at>
2025-12-23 12:28:48 +0100

    feat: support custom feedback forms (#2057)
    * feat: support custom feedback forms
    
    * chore(deps): update package lock
    
    * feat: support `schema` also on button
    
    * chore: formatting
    
    * chore: fix typing
    
    * chore: fix linting

--- elements/feedback/stories/feedback.stories.js
+++ elements/feedback/stories/feedback.stories.js
@@ -2,7 +2,7 @@
  * Stories for eox-feedback component showcasing various configurations.
  * These stories provide visual representations and usage examples for different scenarios.
  */
-import { PrimaryStory, ButtonStory } from "./index";
+import { PrimaryStory, ButtonStory, CustomFormStory } from "./index";
 
 export default {
   title: "Elements/eox-feedback",
@@ -20,3 +20,13 @@ export const Primary = PrimaryStory;
  * Can be `top-left`, `top-right`, `bottom-left`, or `bottom-right`.
  */
 export const Button = ButtonStory;
+
+/**
+ * Custom form schema for eox-feedback.
+ * By passing a JSON schema to the `schema` property, the form can be fully customized.
+ * This functionality is powered by [eox-jsonform](https://github.com/EOX-A/EOxElements/tree/main/elements/jsonform).
+ *
+ * Fields can be hidden by setting `options: { hidden: true }` in the schema property.
+ * Values can be passed programmatically by setting the `default` property in the schema.
+ */
+export const CustomForm = CustomFormStory;
