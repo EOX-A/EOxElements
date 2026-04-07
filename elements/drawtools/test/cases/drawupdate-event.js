7d74f9ecf49f79e0634910aac43f81d1c8857690
silvester-pari <silvester.pari@eox.at>
2026-04-07 20:18:21 +0200

    chore: linting/typing

--- elements/drawtools/test/cases/drawupdate-event.js
+++ elements/drawtools/test/cases/drawupdate-event.js
@@ -8,24 +8,29 @@ const drawUpdateEventTest = () => {
     const drawtools = $el[0];
     let count = 0;
     drawtools.addEventListener("drawupdate", () => count++);
-    
+
     cy.get("mock-map").then(($mapEl) => {
       const map = $mapEl[0];
-      
+
       map.dispatchEvent(new CustomEvent("addfeatures"));
     });
-    
+
     // Wait for the async setTimeout in emitDrawnFeaturesMethod
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(50).then(() => {
       expect(count).to.eq(1, "should emit only once for one addfeatures event");
-      
+
       cy.get("mock-map").then(($mapEl) => {
         $mapEl[0].dispatchEvent(new CustomEvent("addfeatures"));
       });
     });
-    
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(50).then(() => {
-      expect(count).to.eq(2, "should emit exactly twice for two addfeatures events");
+      expect(count).to.eq(
+        2,
+        "should emit exactly twice for two addfeatures events",
+      );
     });
   });
 };
