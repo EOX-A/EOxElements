9bd12d7e7501debd49f5f10a6d81ce612530fd48
Silvester <silvester.pari@eox.at>
2026-06-18 14:33:11 +0200

    feat: A2UI virtual components (#2411)
    * feat: virtual sub-components
    
    * chore: export transformers index
    
    * fix: support children references instead of objects

--- a2ui/src/index.js
+++ a2ui/src/index.js
@@ -2,4 +2,5 @@ export { EOxA2uiElement } from "./eox-a2ui-element.js";
 export { EOxA2uiWrapper } from "./eox-a2ui-wrapper.js";
 export { eoxCatalog } from "./eox-catalog.js";
 export { combinedCatalog, mergeCatalogs } from "./eox-catalog-helper.js";
+export * from "./transformers/index.js";
 export * from "./generated-catalog.js";
