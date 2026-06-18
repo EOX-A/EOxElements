4e1f559fd364e3ca558f18929d2f5849fdd2de83
Silvester <silvester.pari@eox.at>
2026-06-18 18:18:58 +0200

    feat: A2UI support (#2395)
    * feat: a2ui support
    
    * chore: build a2ui on docs build and e2e test
    
    * chore: remove unnecessary story location
    
    * feat: support recursive children rendering and incremental message
     streaming
    
    * feat: A2UI virtual components (#2411)
    
    * feat: virtual sub-components
    
    * chore: export transformers index
    
    * fix: support children references instead of objects
    
    * Update a2ui/src/eox-a2ui-wrapper.js
    
    Co-authored-by: Ahmed Behairi <behairiahmed@gmail.com>
    
    * chore: formatting
    
    ---------
    
    Co-authored-by: Ahmed Behairi <behairiahmed@gmail.com>

---
+++ a2ui/src/constants.js
@@ -0,0 +1,2 @@
+// Centralized version configuration for the A2UI spec/protocol compatibility
+export const A2UI_SPEC_VERSION = "0_9";
---
+++ a2ui/src/eox-catalog.js
@@ -0,0 +1,11 @@
+import { Catalog } from "@a2ui/web_core/v0_9";
+import { eoxComponents, version } from "./generated-catalog.js";
+import { A2UI_SPEC_VERSION } from "./constants.js";
+import { virtualComponents } from "./transformers/virtual-components.js";
+
+export const urlVersion = A2UI_SPEC_VERSION;
+
+export const eoxCatalog = new Catalog(
+  `https://eox-a.github.io/EOxElements/a2ui/${urlVersion}/eox_catalog.json`,
+  [...eoxComponents, ...virtualComponents],
+);
